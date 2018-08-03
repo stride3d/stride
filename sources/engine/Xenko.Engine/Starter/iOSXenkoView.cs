50b8a6a5f0829af2695061a8a64cfd022e55b64a
Simba Mupfunya <solomupfunya@gmail.com>
2018-08-03 14:31:35 +0200

    Interstitial ads black screen issue (#22)
    When implementing full page or interstitial ads such as google admob a black screen appears on any iOS device after the ad is loaded. This is a known problem with OpenTK-1.1 and the solution is to override WillMoveToWindow in iPhoneOSGameView so as to not suspend the window which causes the problem.

--- sources/engine/Xenko.Engine/Starter/iOSXenkoView.cs
+++ sources/engine/Xenko.Engine/Starter/iOSXenkoView.cs
@@ -100,6 +100,18 @@ namespace Xenko.Starter
 
             isRunning = false;
         }
+        
+        /// <summary>
+        /// When implementing full page or interstitial ads such as google admob a black screen appears on any iOS device after the ad is loaded.
+        /// This is a known problem with OpenTK-1.1 when the game is to be placed into suspended mode.
+        /// The solution is to override WillMoveToWindow and only invoke it when the window object is defined.
+        /// </summary>
+        /// <param name="window"></param>
+        public override void WillMoveToWindow(UIWindow window)
+        {
+            if (window != null)
+                base.WillMoveToWindow (window);
+        }
 
         [Export("drawFrame")]
         void DrawFrame()
