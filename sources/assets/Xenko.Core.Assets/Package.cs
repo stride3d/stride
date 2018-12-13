0f827539dd1be57298fe85e5d8fd9eec308686c5
Virgile Bello <virgile@xenko.com>
2018-12-13 12:03:26 +0900

    [Assets] Improve package upgrade when there is no ProjectReference

--- sources/assets/Xenko.Core.Assets/Package.cs
+++ sources/assets/Xenko.Core.Assets/Package.cs
@@ -1268,25 +1268,25 @@ namespace Xenko.Core.Assets
                             var projectLocation = (UFile)(string)profile.ProjectReferences[0].Location;
                             assetFile.FilePath = UPath.Combine(assetFile.OriginalFilePath.GetFullDirectory(), (UFile)(projectLocation.GetFullPathWithoutExtension() + PackageFileExtension));
                             asset.Meta.Name = projectLocation.GetFileNameWithoutExtension();
-
-                            for (int i = 0; i < profile.AssetFolders.Count; ++i)
-                            {
-                                var assetPath = UPath.Combine(assetFile.OriginalFilePath.GetFullDirectory(), (UDirectory)(string)profile.AssetFolders[i].Path);
-                                assetPath = assetPath.MakeRelative(assetFile.FilePath.GetFullDirectory());
-                                profile.AssetFolders[i].Path = (string)assetPath;
-                            }
-
-                            for (int i = 0; i < profile.ResourceFolders.Count; ++i)
-                            {
-                                var resourcePath = UPath.Combine(assetFile.OriginalFilePath.GetFullDirectory(), (UDirectory)(string)profile.ResourceFolders[i]);
-                                resourcePath = resourcePath.MakeRelative(assetFile.FilePath.GetFullDirectory());
-                                profile.ResourceFolders[i] = (string)resourcePath;
-                            }
-
-                            asset.AssetFolders = profile.AssetFolders;
-                            asset.ResourceFolders = profile.ResourceFolders;
-                            asset.OutputGroupDirectories = profile.OutputGroupDirectories;
                         }
+
+                        for (int i = 0; i < profile.AssetFolders.Count; ++i)
+                        {
+                            var assetPath = UPath.Combine(assetFile.OriginalFilePath.GetFullDirectory(), (UDirectory)(string)profile.AssetFolders[i].Path);
+                            assetPath = assetPath.MakeRelative(assetFile.FilePath.GetFullDirectory());
+                            profile.AssetFolders[i].Path = (string)assetPath;
+                        }
+
+                        for (int i = 0; i < profile.ResourceFolders.Count; ++i)
+                        {
+                            var resourcePath = UPath.Combine(assetFile.OriginalFilePath.GetFullDirectory(), (UDirectory)(string)profile.ResourceFolders[i]);
+                            resourcePath = resourcePath.MakeRelative(assetFile.FilePath.GetFullDirectory());
+                            profile.ResourceFolders[i] = (string)resourcePath;
+                        }
+
+                        asset.AssetFolders = profile.AssetFolders;
+                        asset.ResourceFolders = profile.ResourceFolders;
+                        asset.OutputGroupDirectories = profile.OutputGroupDirectories;
                     }
 
                     asset.Profiles = DynamicYamlEmpty.Default;
