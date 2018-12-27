47a5731b347c98ef029170e10d433894ef80c6c5
Eideren <the.eideren@gmail.com>
2018-12-27 11:31:41 +0100

    [Core] ThreadPool: shutdown idle threads

--- sources/core/Xenko.Core/Threading/ThreadPool.cs
+++ sources/core/Xenko.Core/Threading/ThreadPool.cs
@@ -1,9 +1,10 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
 using Xenko.Core.Annotations;
 
 namespace Xenko.Core.Threading
@@ -19,109 +20,71 @@ namespace Xenko.Core.Threading
         public static readonly ThreadPool Instance = new ThreadPool();
 
         private readonly int maxThreadCount = Environment.ProcessorCount + 2;
-        //private readonly int maxThreadCount = Environment.ProcessorCount * 2;
-        private readonly List<Task> workers = new List<Task>();
         private readonly Queue<Action> workItems = new Queue<Action>();
         private readonly ManualResetEvent workAvailable = new ManualResetEvent(false);
 
-        private SpinLock spinLock = new SpinLock();
-        private int activeThreadCount;
+        private object _lock = new object();
+        volatile private int activeThreadCount;
+        volatile private int aliveThreadCount;
 
         public void QueueWorkItem([NotNull] [Pooled] Action workItem)
         {
-            var lockTaken = false;
-            try
+            lock (_lock)
             {
-                spinLock.Enter(ref lockTaken);
-
                 PooledDelegateHelper.AddReference(workItem);
                 workItems.Enqueue(workItem);
-
-                if (activeThreadCount + 1 >= workers.Count && workers.Count < maxThreadCount)
-                {
-                    var worker = Task.Factory.StartNew(ProcessWorkItems, workers.Count, TaskCreationOptions.LongRunning);
-                    workers.Add(worker);
-                    //Console.WriteLine($"Thread {workers.Count} added");
-                }
-
                 workAvailable.Set();
-            }
-            finally
-            {
-                if (lockTaken)
-                    spinLock.Exit(true);
+                if (activeThreadCount + 1 >= aliveThreadCount && aliveThreadCount < maxThreadCount)
+                {
+                    Interlocked.Increment(ref aliveThreadCount);
+                    Task.Factory.StartNew(ProcessWorkItems, TaskCreationOptions.LongRunning);
+                }
             }
         }
 
         private void ProcessWorkItems(object state)
         {
-            //var spinWait = new SpinWait();
-
-            while (true)
+            try
             {
-                Action workItem = null;
-
-                //while (!spinWait.NextSpinWillYield)
+                long lastWork = Stopwatch.GetTimestamp();
+                TimeSpan maxIdleTime = TimeSpan.FromSeconds(5);
+                do
                 {
-                    var lockTaken = false;
-                    try
-                    {
-                        spinLock.Enter(ref lockTaken);
+                    Action workItem = null;
 
+                    lock (_lock)
+                    {
                         if (workItems.Count > 0)
                         {
-                            try
-                            {
-                                workItem = workItems.Dequeue();
-                                //Interlocked.Increment(ref activeThreadCount);
-
-                                if (workItems.Count == 0)
-                                    workAvailable.Reset();
-                            }
-                            catch
-                            {
-                            }
+                            workItem = workItems.Dequeue();
+                            if (workItems.Count == 0)
+                                workAvailable.Reset();
                         }
-
-                        //if (workItems.Count > 0)
-                        //{
-                        //    // If we didn't consume the last work item, kick off another worker
-                        //    workAvailable.Set();
-                        //}
-                    }
-                    finally
-                    {
-                        if (lockTaken)
-                            spinLock.Exit(true);
                     }
 
                     if (workItem != null)
                     {
+                        Interlocked.Increment(ref activeThreadCount);
                         try
                         {
-                            Interlocked.Increment(ref activeThreadCount);
                             workItem.Invoke();
-
-                            //spinWait.Reset();
                         }
                         catch (Exception)
                         {
                             // Ignoring Exception
                         }
-                        finally
-                        {
-                            PooledDelegateHelper.Release(workItem);
-                            Interlocked.Decrement(ref activeThreadCount);
-                        }
+                        Interlocked.Decrement(ref activeThreadCount);
+                        PooledDelegateHelper.Release(workItem);
+                        lastWork = Stopwatch.GetTimestamp();
                     }
-                    else
-                    {
-                        //spinWait.SpinOnce();
-                    }
-                }
 
-                // Wait for another work item to be (potentially) available
-                workAvailable.WaitOne();
+                    // Wait for another work item to be (potentially) available
+                    workAvailable.WaitOne(maxIdleTime);
+                } while (Utilities.ConvertRawToTimestamp(Stopwatch.GetTimestamp() - lastWork) < maxIdleTime);
+            }
+            finally
+            {
+                Interlocked.Decrement(ref aliveThreadCount);
             }
         }
     }
