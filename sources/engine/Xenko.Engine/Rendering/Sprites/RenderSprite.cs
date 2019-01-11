da2f500d07f1eefd5afce3327e59b478b183d087
Devon Klompmaker <devon@antihero-studios.com>
2019-01-10 16:35:42 -0800

    Adding some simple caching to prevent needless calculations.

--- sources/engine/Xenko.Engine/Rendering/Sprites/RenderSprite.cs
+++ sources/engine/Xenko.Engine/Rendering/Sprites/RenderSprite.cs
@@ -8,6 +8,9 @@ namespace Xenko.Rendering.Sprites
 {
     public class RenderSprite : RenderObject
     {
+        private Matrix lastWorldMatrix;
+        private Vector2 lastHalfSpriteSize;
+
         public SpriteComponent SpriteComponent;
         public TransformComponent TransformComponent;
 
@@ -16,43 +19,49 @@ namespace Xenko.Rendering.Sprites
             var transform = TransformComponent;
             var currentSprite = SpriteComponent.CurrentSprite;
 
-            // TODO: Optimize this to cache calculations...
-
             // update the sprite bounding box
             Vector3 halfBoxSize;
             var halfSpriteSize = currentSprite?.Size / 2 ?? Vector2.Zero;
             var worldMatrix = TransformComponent.WorldMatrix;
-            var boxWorldPosition = worldMatrix.TranslationVector;
 
-            if (SpriteComponent.SpriteType == SpriteType.Billboard)
+            // Only calculate if we've changed...
+            if (lastWorldMatrix != worldMatrix || lastHalfSpriteSize != halfSpriteSize)
             {
-                // Make a gross estimation here as we don't have access to the camera view matrix
-                // TODO: move this code or grant camera view matrix access to this processor
-                var maxScale = Math.Max(worldMatrix.Row1.Length(), Math.Max(worldMatrix.Row2.Length(), worldMatrix.Row3.Length()));
-                halfBoxSize = maxScale * halfSpriteSize.Length() * Vector3.One;
+                var boxWorldPosition = worldMatrix.TranslationVector;
+
+                if (SpriteComponent.SpriteType == SpriteType.Billboard)
+                {
+                    // Make a gross estimation here as we don't have access to the camera view matrix
+                    // TODO: move this code or grant camera view matrix access to this processor
+                    var maxScale = Math.Max(worldMatrix.Row1.Length(), Math.Max(worldMatrix.Row2.Length(), worldMatrix.Row3.Length()));
+                    halfBoxSize = maxScale * halfSpriteSize.Length() * Vector3.One;
+                }
+                else
+                {
+                    halfBoxSize = new Vector3(
+                        Math.Abs(worldMatrix.M11 * halfSpriteSize.X + worldMatrix.M21 * halfSpriteSize.Y),
+                        Math.Abs(worldMatrix.M12 * halfSpriteSize.X + worldMatrix.M22 * halfSpriteSize.Y),
+                        Math.Abs(worldMatrix.M13 * halfSpriteSize.X + worldMatrix.M23 * halfSpriteSize.Y));
+                }
+
+                // Get mins & maxes...
+                var minX = boxWorldPosition.X - halfBoxSize.X;
+                var minY = boxWorldPosition.Y - halfBoxSize.Y;
+                var maxX = boxWorldPosition.X + halfBoxSize.X;
+                var maxY = boxWorldPosition.Y + halfBoxSize.Y;
+
+                // Calculate new size...
+                var width = maxX - minX;
+                var height = maxY - minY;
+
+                // Assignment...
+                BoundingBox.Center = transform.WorldMatrix.TranslationVector;
+                BoundingBox.Extent = new Vector3(width / 2, height / 2, 0);
+                RenderGroup = SpriteComponent.RenderGroup;
             }
-            else
-            {
-                halfBoxSize = new Vector3(
-                    Math.Abs(worldMatrix.M11 * halfSpriteSize.X + worldMatrix.M21 * halfSpriteSize.Y),
-                    Math.Abs(worldMatrix.M12 * halfSpriteSize.X + worldMatrix.M22 * halfSpriteSize.Y),
-                    Math.Abs(worldMatrix.M13 * halfSpriteSize.X + worldMatrix.M23 * halfSpriteSize.Y));
-            }
 
-            // Get mins & maxes...
-            var minX = boxWorldPosition.X - halfBoxSize.X;
-            var minY = boxWorldPosition.Y - halfBoxSize.Y;
-            var maxX = boxWorldPosition.X + halfBoxSize.X;
-            var maxY = boxWorldPosition.Y + halfBoxSize.Y;
-
-            // Calculate new size...
-            var width = maxX - minX;
-            var height = maxY - minY;
-
-            // Assignment...
-            BoundingBox.Center = transform.WorldMatrix.TranslationVector;
-            BoundingBox.Extent = new Vector3(width / 2, height / 2, 0);
-            RenderGroup = SpriteComponent.RenderGroup;
+            lastWorldMatrix = worldMatrix;
+            lastHalfSpriteSize = halfSpriteSize;
         }
     }
 }
