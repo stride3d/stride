b41748068a2b531254a5a155b4d2cb030f94c22f
Nicolas Musset <musset.nicolas@gmail.com>
2019-01-11 23:20:03 +0900

    [Localization] Update extraction script

--- sources/localization/extract_strings.bat
+++ sources/localization/extract_strings.bat
@@ -1,16 +1,17 @@
-@set SOURCES_DIR="%~dp0\.."
-@set OUTPUT_DIR=%SOURCES_DIR%\localization\
+@set SOURCES_DIR=%~dp0..
+@set OUTPUT_DIR=%SOURCES_DIR%\localization
+@set TOOL_DIR=%SOURCES_DIR%\tools\Xenko.Core.Translation.Extractor\bin\Debug\net472
 
-@cd "%OUTPUT_DIR%"
+@cd %OUTPUT_DIR%
 
 rem Xenko.Core.Presentation.pot
-..\..\Bin\Windows\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\presentation\Xenko.Core.Presentation --domain-name=Xenko.Core.Presentation --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
+%TOOL_DIR%\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\presentation\Xenko.Core.Presentation --domain-name=Xenko.Core.Presentation --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
 
 rem Xenko.Assets.Presentation.pot
-..\..\Bin\Windows\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.Assets.Presentation --domain-name=Xenko.Assets.Presentation --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
+%TOOL_DIR%\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.Assets.Presentation --domain-name=Xenko.Assets.Presentation --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
 
 rem Xenko.Core.Assets.Editor.pot
- ..\..\Bin\Windows\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.Core.Assets.Editor --domain-name=Xenko.Core.Assets.Editor --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
+%TOOL_DIR%\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.Core.Assets.Editor --domain-name=Xenko.Core.Assets.Editor --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
 
 rem Xenko.GameStudio.pot
- ..\..\Bin\Windows\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.GameStudio --domain-name=Xenko.GameStudio --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
+%TOOL_DIR%\Xenko.Core.Translation.Extractor.exe --directory=%SOURCES_DIR%\editor\Xenko.GameStudio --domain-name=Xenko.GameStudio --recursive --preserve-comments --exclude=*.Designer.cs --verbose *.xaml *.cs
