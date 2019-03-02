3608b0b557341ef1db46475d98302ffdd0697a28
dfkeenan <dfkeenan@gmail.com>
2019-03-03 00:19:17 +1000

    Change the ObjectToTypeName value converter to give prettier C# names.

--- sources/presentation/Xenko.Core.Presentation/ValueConverters/ObjectToTypeName.cs
+++ sources/presentation/Xenko.Core.Presentation/ValueConverters/ObjectToTypeName.cs
@@ -3,6 +3,9 @@
 using System;
 using System.Globalization;
 using Xenko.Core.Annotations;
+using System.CodeDom;
+using Microsoft.CSharp;
+using System.Text.RegularExpressions;
 
 namespace Xenko.Core.Presentation.ValueConverters
 {
@@ -19,11 +22,22 @@ namespace Xenko.Core.Presentation.ValueConverters
         /// </summary>
         public const string NullObjectType = "(None)";
 
+        private readonly CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+
         /// <inheritdoc/>
         [NotNull]
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value?.GetType().Name ?? NullObjectType;
+            if (value == null) return NullObjectType;
+
+            Type valueType = value.GetType();
+            var fullTypeName = codeProvider.GetTypeOutput(new CodeTypeReference(valueType));
+
+            //Change System.Nullable to "Type?" format
+            var typeName = Regex.Replace(fullTypeName, @"System\.Nullable<([^>]*)>", "$1?");
+
+            //Simplify name. i.e. remove namespaces etc.
+            return Regex.Replace(typeName, @"[^,<>]*\.", "");
         }
     }
 }
