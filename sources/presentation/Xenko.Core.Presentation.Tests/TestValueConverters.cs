9540610dc7d4c7b3cabe89cdbe92f9197c6b2a30
dfkeenan <dfkeenan@gmail.com>
2019-03-09 14:32:00 +1000

    Fixed typos in unit test comments.

--- sources/presentation/Xenko.Core.Presentation.Tests/TestValueConverters.cs
+++ sources/presentation/Xenko.Core.Presentation.Tests/TestValueConverters.cs
@@ -36,18 +36,18 @@ namespace Xenko.Core.Presentation.Tests
             var tests = new List<(Type Type, string Name)>()
             {
                 (typeof(int), "int"), //Simple type
-                (typeof(int?), "int?"), //Nullable Simple type
+                (typeof(int?), "int?"), //Nullable simple type
                 (typeof(TimeSpan), "TimeSpan"), //Type
                 (typeof(TimeSpan?), "TimeSpan?"), //Nullable type
-                (typeof(int[]), "int[]"), //Array of Simple type
-                (typeof(int?[]), "int?[]"), //Array of Nullable Simple type
-                (typeof(TimeSpan[]), "TimeSpan[]"), //Array of Type
-                (typeof(TimeSpan?[]), "TimeSpan?[]"), //Array of Nullable type
-                (typeof(TimeSpan[,,]), "TimeSpan[,,]"), //Multi-dimesional Array of Type
+                (typeof(int[]), "int[]"), //Array of simple type
+                (typeof(int?[]), "int?[]"), //Array of nullable simple type
+                (typeof(TimeSpan[]), "TimeSpan[]"), //Array of type
+                (typeof(TimeSpan?[]), "TimeSpan?[]"), //Array of nullable type
+                (typeof(TimeSpan[,,]), "TimeSpan[,,]"), //Multi-dimesional array of type
                 (typeof(Dictionary<string, FactAttribute>), "Dictionary<string, FactAttribute>"), //Generic type
-                (typeof((string, FactAttribute)), "(string, FactAttribute)"), //Tuple Types
-                (typeof((string, FactAttribute)?), "(string, FactAttribute)?"), //Nullable Tuple Types
-                (typeof(Dictionary<string, GenericStruct<int?>>), "Dictionary<string, GenericStruct<int?>>"), //Nested Grneric Type
+                (typeof((string, FactAttribute)), "(string, FactAttribute)"), //Tuple types
+                (typeof((string, FactAttribute)?), "(string, FactAttribute)?"), //Nullable tuple types
+                (typeof(Dictionary<string, GenericStruct<int?>>), "Dictionary<string, GenericStruct<int?>>"), //Nested generic type
                 (typeof((GenericStruct<int?>?, double)), "(GenericStruct<int?>?, double)"), //Crazy type
             };
 
