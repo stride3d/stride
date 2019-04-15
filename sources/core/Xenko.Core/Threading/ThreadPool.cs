f7eeb20efc6272d0f2d8bba938809420b1704c74
phr00t <phr00t@gmail.com>
2019-04-14 22:50:42 -0400

    significant, engine-wide performance improvement with better threadpool system

--- sources/core/Xenko.Core/Threading/ThreadPool.cs
+++ sources/core/Xenko.Core/Threading/ThreadPool.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
@@ -17,93 +18,38 @@ namespace Xenko.Core.Threading
     /// </remarks>
     internal class ThreadPool
     {
-        private const long MaxIdleTimeInTicks = 5 * TimeSpan.TicksPerSecond;
-
         public static readonly ThreadPool Instance = new ThreadPool();
 
         private readonly int maxThreadCount = Environment.ProcessorCount + 2;
-        private readonly Queue<Action> workItems = new Queue<Action>();
-        private readonly ManualResetEvent workAvailable = new ManualResetEvent(false);
-
-        private SpinLock spinLock = new SpinLock();
-        private int workingCount;
-        /// <summary> Usage only within <see cref="spinLock"/> </summary>
-        private int aliveCount;
+        private readonly ConcurrentQueue<Action> workItems = new ConcurrentQueue<Action>();
+        private EventWaitHandle waiter = new EventWaitHandle(false, EventResetMode.AutoReset);
+        private int poolCount;
 
         public void QueueWorkItem([NotNull] [Pooled] Action workItem)
         {
-            var lockTaken = false;
-            try
+            PooledDelegateHelper.AddReference(workItem);
+            workItems.Enqueue(workItem);
+            waiter.Set();
+            if (poolCount < maxThreadCount)
             {
-                spinLock.Enter(ref lockTaken);
-
-                PooledDelegateHelper.AddReference(workItem);
-                workItems.Enqueue(workItem);
-                workAvailable.Set();
-
-                var curWorkingCount = Interlocked.CompareExchange(ref workingCount, 0, 0);
-                if (curWorkingCount + 1 >= aliveCount && aliveCount < maxThreadCount)
-                {
-                    aliveCount++;
-                    Task.Factory.StartNew(ProcessWorkItems, TaskCreationOptions.LongRunning);
-                }
-            }
-            finally
-            {
-                if (lockTaken)
-                    spinLock.Exit(true);
+                poolCount++;
+                Task.Factory.StartNew(ProcessWorkItems, TaskCreationOptions.LongRunning);
             }
         }
 
         private void ProcessWorkItems(object state)
         {
-            long lastWork = Stopwatch.GetTimestamp();
-            TimeSpan maxIdleTime = TimeSpan.FromTicks(MaxIdleTimeInTicks);
             while(true)
             {
-                Action workItem = null;
-                var lockTaken = false;
-                bool idleForTooLong = Utilities.ConvertRawToTimestamp(Stopwatch.GetTimestamp() - lastWork) < maxIdleTime;
-                try
-                {
-                    spinLock.Enter(ref lockTaken);
-
-                    if (workItems.Count > 0)
-                    {
-                        workItem = workItems.Dequeue();
-                        if (workItems.Count == 0)
-                            workAvailable.Reset();
-                    }
-                    else if (idleForTooLong)
-                    {
-                        aliveCount--;
-                        return;
-                    }
-                }
-                finally
-                {
-                    if (lockTaken)
-                        spinLock.Exit(true);
-                }
-
-                if (workItem != null)
-                {
-                    Interlocked.Increment(ref workingCount);
-                    try
-                    {
-                        workItem.Invoke();
-                    }
-                    catch (Exception)
-                    {
-                        // Ignoring Exception
-                    }
-                    Interlocked.Decrement(ref workingCount);
+                if (workItems.TryDequeue(out Action workItem)) {
+                    workItem.Invoke();
                     PooledDelegateHelper.Release(workItem);
-                    lastWork = Stopwatch.GetTimestamp();
-                }
+                };
 
-                // Wait for another work item to be (potentially) available
-                workAvailable.WaitOne(maxIdleTime);
+                if (waiter.WaitOne(5000) == false) {
+                    poolCount--;
+                    return;
+                }
             }
         }
     }
