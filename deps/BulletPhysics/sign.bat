465a52cdee1581c8fef8ffeb6b400f243e0fa9b3
Eideren <the.eideren@gmail.com>
2019-05-01 11:26:55 +0200

    [Bullet] Update all platforms to latest libs

--- deps/BulletPhysics/sign.bat
+++ deps/BulletPhysics/sign.bat
@@ -1,9 +1,9 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
 
 ildasm /all /out:Windows\BulletSharp.NetStandard.il BulletSharp.NetStandard.dll
-ildasm /all /out:iOS\BulletSharp.NetStandard.il BulletSharp.NetStandard.dll
+ildasm /all /out:iOS\BulletSharp.NetStandard.il iOS\BulletSharp.NetStandard.dll
 
-@echo "Please patch Windows\BulletSharp.il and iOS\BulletSharp.il Xenko.Core.Mathematics reference with .publickeytoken = ( BA CA CC 89 C3 B6 D5 56 )"
+@echo "Please patch Windows\BulletSharp.NetStandard.il and iOS\BulletSharp.NetStandard.il Xenko.Core.Mathematics reference with .publickeytoken = ( BA CA CC 89 C3 B6 D5 56 )"
 pause
 
 mkdir Windows\Signed
