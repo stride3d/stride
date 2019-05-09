25fbb8c7ee83e4a8b3b0b86a82ffacdce2834780
phr00t <phr00t@gmail.com>
2019-05-09 16:46:02 -0400

    significantly improve editor performance
    editor's "game" was updating very slowly when not in focus (which is any time you are doing something outside of the game view) by default... this fixes that

--- sources/editor/Xenko.Assets.Presentation/AssetEditors/EntityHierarchyEditor/Game/EntityHierarchyEditorGame.cs
+++ sources/editor/Xenko.Assets.Presentation/AssetEditors/EntityHierarchyEditor/Game/EntityHierarchyEditorGame.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
 using System.Collections.Generic;
@@ -53,6 +53,9 @@ namespace Xenko.Assets.Presentation.AssetEditors.EntityHierarchyEditor.Game
             this.gameContentLoadedTaskSource = gameContentLoadedTaskSource;
             this.effectCompiler = effectCompiler;
             this.effectLogPath = effectLogPath;
+            this.TreatNotFocusedLikeMinimized = false;
+            this.GraphicsDeviceManager.SynchronizeWithVerticalRetrace = true;
+            this.WindowMinimumUpdateRate.MinimumElapsedTime = TimeSpan.Zero;
             CreateScenePipeline();
         }
 
