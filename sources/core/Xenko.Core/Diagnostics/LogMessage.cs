0417eecffe1fdc7c8af09857e3dba7ad11339958
phr00t <phr00t@gmail.com>
2019-05-15 12:09:28 -0400

    make an attempt to get line info for exception logging

--- sources/core/Xenko.Core/Diagnostics/LogMessage.cs
+++ sources/core/Xenko.Core/Diagnostics/LogMessage.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
+using System.Diagnostics;
 
 namespace Xenko.Core.Diagnostics
 {
@@ -42,6 +43,17 @@ namespace Xenko.Core.Diagnostics
         /// <param name="callerInfo">The caller info.</param>
         public LogMessage(string module, LogMessageType type, string text, Exception exception, CallerInfo callerInfo)
         {
+            try {
+                // Get stack trace for the exception with source file information
+                StackTrace st = new StackTrace(exception, true);
+                // Get the top stack frame
+                StackFrame frame = st.GetFrame(0);
+                // Get the line number from the stack frame
+                int line = frame.GetFileLineNumber();
+                // try to add it to the front of the text
+                text = "{line #" + line.ToString() + "}" + text;
+            } catch(Exception e) { /* couldn't get line info */ }
+
             Module = module;
             Type = type;
             Text = text;
