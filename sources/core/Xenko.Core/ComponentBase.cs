7db782aac418392952626e6ca921687f842742ed
tebjan <tebjan@vvvv.org>
2019-05-18 21:38:32 +0200

    added NameChanged event to ComponentBase

--- sources/core/Xenko.Core/ComponentBase.cs
+++ sources/core/Xenko.Core/ComponentBase.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System;
+
 namespace Xenko.Core
 {
     /// <summary>
@@ -77,11 +79,14 @@ namespace Xenko.Core
             }
         }
 
+        public event EventHandler<string> NameChanged;
+
         /// <summary>
         /// Called when <see cref="Name"/> property was changed.
         /// </summary>
         protected virtual void OnNameChanged()
         {
+            NameChanged?.Invoke(this, name);
         }
 
         public override string ToString()
