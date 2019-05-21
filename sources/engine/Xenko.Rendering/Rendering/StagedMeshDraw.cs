47b5709b200cfaaf3a607464ffc9b2bc60c185dd
phr00t <phr00t@gmail.com>
2019-05-21 19:07:44 -0400

    stagedmeshdraw synchronization and cache

--- sources/engine/Xenko.Rendering/Rendering/StagedMeshDraw.cs
+++ sources/engine/Xenko.Rendering/Rendering/StagedMeshDraw.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using Xenko.Graphics;
@@ -9,6 +10,7 @@ namespace Xenko.Rendering.Rendering {
         public Action<GraphicsDevice> performStage;
 
         private StagedMeshDraw() { }
+        private static object StagedLock = new object();
 
         /// <summary>
         /// Gets a MeshDraw that will be prepared when needed with the given index buffer & vertex buffer.
@@ -22,20 +24,33 @@ namespace Xenko.Rendering.Rendering {
             smdt.PrimitiveType = PrimitiveType.TriangleList;
             smdt.DrawCount = indexBuffer.Length;
             smdt.performStage = (GraphicsDevice graphicsDevice) => {
-                Xenko.Graphics.Buffer vbo, ibo;
-                vbo = Xenko.Graphics.Buffer.Vertex.New<T>(
-                    graphicsDevice,
-                    vertexBuffer,
-                    GraphicsResourceUsage.Immutable
-                );
-                ibo = Xenko.Graphics.Buffer.Index.New<uint>(
-                    graphicsDevice,
-                    indexBuffer
-                );
-                smdt.VertexBuffers = new[] {
-                    new VertexBufferBinding(vbo, vertexBufferLayout, smdt.DrawCount)
-                };
-                smdt.IndexBuffer = new IndexBufferBinding(ibo, true, smdt.DrawCount);
+                if (StagedMeshDrawTyped<T>.CachedBuffers.TryGetValue(vertexBuffer, out object[] bufferBindings)) {
+                    smdt.VertexBuffers = (VertexBufferBinding[])bufferBindings[0];
+                    smdt.IndexBuffer = (IndexBufferBinding)bufferBindings[1];
+                } else {
+                    Xenko.Graphics.Buffer vbo, ibo;
+                    lock (StagedLock) {
+                        vbo = Xenko.Graphics.Buffer.Vertex.New<T>(
+                            graphicsDevice,
+                            vertexBuffer,
+                            GraphicsResourceUsage.Immutable
+                        );
+                        ibo = Xenko.Graphics.Buffer.Index.New<uint>(
+                            graphicsDevice,
+                            indexBuffer
+                        );
+                    }
+                    object[] o = new object[2];
+                    VertexBufferBinding[] vbb = new[] {
+                        new VertexBufferBinding(vbo, vertexBufferLayout, smdt.DrawCount)
+                    };
+                    IndexBufferBinding ibb = new IndexBufferBinding(ibo, true, smdt.DrawCount);
+                    o[0] = vbb;
+                    o[1] = ibb;
+                    StagedMeshDrawTyped<T>.CachedBuffers.TryAdd(vertexBuffer, o);
+                    smdt.VertexBuffers = vbb;
+                    smdt.IndexBuffer = ibb;
+                }
             };
             return smdt;
         }
@@ -43,6 +58,7 @@ namespace Xenko.Rendering.Rendering {
         private class StagedMeshDrawTyped<T> : StagedMeshDraw where T : struct {
             private uint[] stagedIndexBuffer;
             private T[] stagedVertexBuffer;
+            public static ConcurrentDictionary<T[], object[]> CachedBuffers = new ConcurrentDictionary<T[], object[]>();
         }
     }
 }
