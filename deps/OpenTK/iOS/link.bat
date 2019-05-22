2079a316301cba76185cbeaf1b53fa2ddcc7b84a
Virgile Bello <virgile@xenko.com>
2019-05-22 17:55:45 +0200

    [Build] Bumped Cecil to NuGet 0.10.4 rather than custom one.

--- deps/OpenTK/iOS/link.bat
+++ deps/OpenTK/iOS/link.bat
@@ -1,5 +1,5 @@
 pushd ..\..\..\bin\iOS-OpenGLES
-..\..\deps\Mono.Cecil\monolinker -a Xenko.Engine.dll -a Xenko.Games.dll -a Xenko.Input.dll -a Xenko.Graphics.dll -p link OpenTK-1.1 -u copy -b true -d "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Xamarin.iOS\v1.0"
+..\..\deps\monolinker\monolinker -a Xenko.Engine.dll -a Xenko.Games.dll -a Xenko.Input.dll -a Xenko.Graphics.dll -p link OpenTK-1.1 -u copy -b true -d "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Xamarin.iOS\v1.0"
 popd
 
 copy ..\..\..\bin\iOS-OpenGLES\output\OpenTK-1.1.dll .
