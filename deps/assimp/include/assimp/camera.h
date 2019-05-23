9cdd43c0091991fca36f81842be0fe2259be60e7
tebjan <tebjan@vvvv.org>
2019-05-23 19:16:06 +0200

    [Assimp] Updated assimp to official 4.1.0 and updated references in projects (closes #458)

--- deps/assimp/include/assimp/camera.h
+++ deps/assimp/include/assimp/camera.h
@@ -3,7 +3,9 @@
 Open Asset Import Library (assimp)
 ---------------------------------------------------------------------------
 
-Copyright (c) 2006-2016, assimp team
+Copyright (c) 2006-2018, assimp team
+
+
 
 All rights reserved.
 
