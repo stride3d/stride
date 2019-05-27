79ef2b1bf199f84d73053bbd5fd4de85d92be6e2
Eideren <the.eideren@gmail.com>
2019-05-27 21:25:18 +0200

    [Physics] Rollback unused serialization workaround

--- sources/core/Xenko.Core.Serialization/Serialization/Contents/ReferenceSerializer.cs
+++ sources/core/Xenko.Core.Serialization/Serialization/Contents/ReferenceSerializer.cs
@@ -129,7 +129,7 @@ namespace Xenko.Core.Serialization.Contents
                 else
                 {
                     // No real case yet
-                    //throw new NotSupportedException();
+                    throw new NotSupportedException();
                 }
             }
         }
