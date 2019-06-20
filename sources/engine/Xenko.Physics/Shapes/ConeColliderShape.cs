eeb86e48cc54bd70baed9d16e5a3ddee737b5b54
Eideren <the.eideren@gmail.com>
2019-06-21 01:55:29 +0200

    [Physics] Provide read access to collider shape's data

--- sources/engine/Xenko.Physics/Shapes/ConeColliderShape.cs
+++ sources/engine/Xenko.Physics/Shapes/ConeColliderShape.cs
@@ -12,49 +12,56 @@ namespace Xenko.Physics
 {
     public class ConeColliderShape : ColliderShape
     {
+        public readonly float Height;
+        public readonly float Radius;
+        public readonly ShapeOrientation Orientation;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ConeColliderShape"/> class.
         /// </summary>
         /// <param name="orientation">Up axis.</param>
         /// <param name="radius">The radius of the cone</param>
         /// <param name="height">The height of the cone</param>
-        public ConeColliderShape(float height, float radius, ShapeOrientation orientation)
+        public ConeColliderShape(float heightParam, float radiusParam, ShapeOrientation orientationParam)
         {
             Type = ColliderShapeTypes.Cone;
             Is2D = false; //always false for cone
+            Height = heightParam;
+            Radius = radiusParam;
+            Orientation = orientationParam;
 
             Matrix rotation;
 
             cachedScaling = Vector3.One;
 
-            switch (orientation)
+            switch (Orientation)
             {
                 case ShapeOrientation.UpX:
-                    InternalShape = new BulletSharp.ConeShapeX(radius, height)
+                    InternalShape = new BulletSharp.ConeShapeX(Radius, Height)
                     {
                         LocalScaling = cachedScaling,
                     };
                     rotation = Matrix.RotationZ((float)Math.PI / 2.0f);
                     break;
                 case ShapeOrientation.UpY:
-                    InternalShape = new BulletSharp.ConeShape(radius, height)
+                    InternalShape = new BulletSharp.ConeShape(Radius, Height)
                     {
                         LocalScaling = cachedScaling,
                     };
                     rotation = Matrix.Identity;
                     break;
                 case ShapeOrientation.UpZ:
-                    InternalShape = new BulletSharp.ConeShapeZ(radius, height)
+                    InternalShape = new BulletSharp.ConeShapeZ(Radius, Height)
                     {
                         LocalScaling = cachedScaling,
                     };
                     rotation = Matrix.RotationX((float)Math.PI / 2.0f);
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(orientation));
+                    throw new ArgumentOutOfRangeException(nameof(Orientation));
             }
 
-            DebugPrimitiveMatrix = Matrix.Scaling(new Vector3(radius * 2, height, radius * 2) * DebugScaling) * rotation;
+            DebugPrimitiveMatrix = Matrix.Scaling(new Vector3(Radius * 2, Height, Radius * 2) * DebugScaling) * rotation;
         }
 
         public override MeshDraw CreateDebugPrimitive(GraphicsDevice device)
