c4cde4eb1eac47bfe859144cad5826456dffc0f8
Eideren <the.eideren@gmail.com>
2019-06-21 02:24:21 +0200

    [Physics] Split static mesh collider into new PR

--- sources/engine/Xenko.Assets/Physics/ColliderShapeFactories.cs
+++ sources/engine/Xenko.Assets/Physics/ColliderShapeFactories.cs
@@ -95,17 +95,4 @@ namespace Xenko.Assets.Physics
             return Create();
         }
     }
-
-    public class ColliderShapeStaticMeshFactory : AssetFactory<ColliderShapeAsset>
-    {
-        public static ColliderShapeAsset Create()
-        {
-            return new ColliderShapeAsset { ColliderShapes = { new StaticMeshColliderShapeDesc() } };
-        }
-
-        public override ColliderShapeAsset New()
-        {
-            return Create();
-        }
-    }
 }
