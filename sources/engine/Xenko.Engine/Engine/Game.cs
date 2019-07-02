67622f31cd9b80d011abd30b2bae0ab0abae9ded
Hyperpred <robertson.jarrett@gmail.com>
2019-07-02 05:01:39 -0700

    Remove cast to Game in AudioSystem (#472)

--- sources/engine/Xenko.Engine/Engine/Game.cs
+++ sources/engine/Xenko.Engine/Engine/Game.cs
@@ -30,7 +30,7 @@ namespace Xenko.Engine
     /// <summary>
     /// Main Game class system.
     /// </summary>
-    public class Game : GameBase, ISceneRendererContext
+    public class Game : GameBase, ISceneRendererContext, IGameSettingsService
     {
         /// <summary>
         /// Static event that will be fired when a game is initialized
@@ -343,6 +343,8 @@ namespace Xenko.Engine
                         if (!deviceManager.ShaderProfile.HasValue)
                             deviceManager.ShaderProfile = renderingSettings.DefaultGraphicsProfile;
                     }
+
+                    Services.AddService<IGameSettingsService>(this);
                 }
 
                 // Load several default settings
