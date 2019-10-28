9c1976f0d0af1368d90beca2b0279fee06ca27af
Virgile Bello <virgile@xenko.com>
2019-10-28 13:14:29 +0100

    [RoslynPad] Updated to latest version and added checkout/build scripts

---
+++ deps/RoslynPad/build.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+set ROSLYNPAD=%~dp0..\..\externals\RoslynPad
+
+pushd %ROSLYNPAD%\src
+
+msbuild /nologo /restore /p:Configuration=Release RoslynPad.sln
+if ERRORLEVEL 1 echo "Cannot build RoslynPad" && pause
+
+popd
+
+xcopy /Y /Q %ROSLYNPAD%\src\RoslynPad.Editor.Windows\bin\Release\net462\RoslynPad*.* net462 > nul
+if ERRORLEVEL 1  echo "Cannot copy RoslynPad net462 files" && pause
+
+xcopy /Y /Q %ROSLYNPAD%\src\RoslynPad.Editor.Windows\bin\Release\netcoreapp3.0\RoslynPad*.* netcoreapp3.0 > nul
+if ERRORLEVEL 1  echo "Cannot copy RoslynPad netcoreapp3.0 files" && pause
+
+echo RoslynPad build completed successfully
--- sources/editor/Xenko.Assets.Presentation/AssetEditors/ScriptEditor/ClassificationHighlightColorsDark.cs
+++ sources/editor/Xenko.Assets.Presentation/AssetEditors/ScriptEditor/ClassificationHighlightColorsDark.cs
@@ -18,63 +18,16 @@ namespace Xenko.Assets.Presentation.AssetEditors.ScriptEditor
         public static readonly Color TypeColor = Color.FromRgb(78, 201, 176);
         public static readonly Color KeywordColor = Color.FromRgb(86, 156, 214);
 
-        private readonly ImmutableDictionary<string, HighlightingColor> _map;
-
         public ClassificationHighlightColorsDark()
         {
-            this.DefaultBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(220, 220, 220)) };
-            this.TypeBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(78, 201, 176)) };
+            this.DefaultBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(DefaultColor) };
+            this.TypeBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(TypeColor) };
             this.CommentBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(87, 166, 74)) };
             this.XmlCommentBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(87, 166, 74)) };
             this.KeywordBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(KeywordColor) };
             this.PreprocessorKeywordBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(155, 155, 155)) };
             this.StringBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(Color.FromRgb(214, 157, 133)) };
-
-            // ClassificationHighlightColors ctor is hardcoded so we can't override any color, create our own dictionary
-            // (need to submit PR so that dictionary creation is done in GetOrCreateMap instead)
-            _map = new Dictionary<string, HighlightingColor>
-            {
-                [ClassificationTypeNames.ClassName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.StructName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.InterfaceName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.DelegateName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.EnumName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.ModuleName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.TypeParameterName] = AsFrozen(TypeBrush),
-                [ClassificationTypeNames.MethodName] = AsFrozen(MethodBrush),
-                [ClassificationTypeNames.Comment] = AsFrozen(CommentBrush),
-                [ClassificationTypeNames.StaticSymbol] = AsFrozen(StaticSymbolBrush),
-                [ClassificationTypeNames.XmlDocCommentAttributeName] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentAttributeQuotes] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentAttributeValue] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentCDataSection] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentComment] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentDelimiter] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentEntityReference] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentName] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentProcessingInstruction] = AsFrozen(XmlCommentBrush),
-                [ClassificationTypeNames.XmlDocCommentText] = AsFrozen(CommentBrush),
-                [ClassificationTypeNames.Keyword] = AsFrozen(KeywordBrush),
-                [ClassificationTypeNames.ControlKeyword] = AsFrozen(KeywordBrush),
-                [ClassificationTypeNames.PreprocessorKeyword] = AsFrozen(PreprocessorKeywordBrush),
-                [ClassificationTypeNames.StringLiteral] = AsFrozen(StringBrush),
-                [ClassificationTypeNames.VerbatimStringLiteral] = AsFrozen(StringBrush),
-                [BraceMatchingClassificationTypeName] = AsFrozen(BraceMatchingBrush)
-            }.ToImmutableDictionary();
-        }
-
-        protected override ImmutableDictionary<string, HighlightingColor> GetOrCreateMap()
-        {
-            return _map;
-        }
-
-        private static HighlightingColor AsFrozen(HighlightingColor color)
-        {
-            if (!color.IsFrozen)
-            {
-                color.Freeze();
-            }
-            return color;
+            this.MethodBrush = new HighlightingColor { Foreground = new SimpleHighlightingBrush(TypeColor) };
         }
     }
 }
