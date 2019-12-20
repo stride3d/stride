1b4e95387fb0ddc50595661e34dba78816fd1230
Virgile Bello <virgile@xenko.com>
2019-12-20 13:55:23 +0100

    [Video] Video wouldn't load if not packed in the bundle. D3D video now uses the resolved URL rather than resolving again.

--- sources/engine/Xenko.Video/VideoInstance.cs
+++ sources/engine/Xenko.Video/VideoInstance.cs
@@ -471,20 +471,20 @@ namespace Xenko.Video
                 var fileProvider = source.FileProvider;
 
                 if (!fileProvider.ContentIndexMap.TryGetValue(dataUrl, out ObjectId objectId) ||
-                    !fileProvider.ObjectDatabase.BundleBackend.TryGetObjectLocation(objectId, out url, out startPosition, out end))
+                    !fileProvider.ObjectDatabase.TryGetObjectLocation(objectId, out url, out startPosition, out end))
                 {
                     throw new InvalidOperationException("Video files needs to be stored on the virtual file system in a non-compressed form.");
                 }
+
+                // Initialize media
+                InitializeMedia(url, startPosition, end - startPosition);
+
+                // Set playback properties
+                UpdateAudioVolume();
+                UpdateLoopingSettings();
+                UpdatePlayRangeSettings();
+                UpdateSpeedSettings();
             }
-
-            // Initialize media
-            InitializeMedia(url, startPosition, end - startPosition);
-
-            // Set playback properties
-            UpdateAudioVolume();
-            UpdateLoopingSettings();
-            UpdatePlayRangeSettings();
-            UpdateSpeedSettings();
 
             // Do not play and pause the new video. The new video always starts in the 'Stopped' state
             // It is responsibility of the user the revert play state after changing the source video.
