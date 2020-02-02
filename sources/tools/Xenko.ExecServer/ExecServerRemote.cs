34d62843f778b3350503ca25b85b94cfc5848742
Anon <makotech222@gmail.com>
2020-02-02 11:18:49 -0600

    Replace WCF with ServiceWire in ExecServer

--- sources/tools/Xenko.ExecServer/ExecServerRemote.cs
+++ sources/tools/Xenko.ExecServer/ExecServerRemote.cs
@@ -7,16 +7,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.ServiceModel;
 using System.Threading;
 using Xenko.Core.VisualStudio;
+using static Xenko.ExecServer.AppDomainShadow;
+using static Xenko.ExecServer.ExecServerApp;
 
 namespace Xenko.ExecServer
 {
     /// <summary>
-    /// Remote implementation of the server
+    /// Remote implementation of the ServiceWire server
     /// </summary>
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode=ConcurrencyMode.Multiple)]
     internal class ExecServerRemote : IExecServerRemote
     {
         public const int BusyReturnCode = -8000;
@@ -65,7 +65,7 @@ namespace Xenko.ExecServer
         {
         }
 
-        public int Run(string currentDirectory, Dictionary<string, string> environmentVariables, string[] args, bool shadowCache, int? debuggerProcessId)
+        public int Run(string currentDirectory, Dictionary<string, string> environmentVariables, string[] args, bool shadowCache, int? debuggerProcessId, IServerLogger logger)
         {
             bool lockTaken = false;
             try
@@ -90,8 +90,7 @@ namespace Xenko.ExecServer
                     }
                 }
 
-                var logger = OperationContext.Current.GetCallbackChannel<IServerLogger>();
-                var result = shadowManager.Run(currentDirectory, environmentVariables, args, shadowCache, logger);
+                var result = shadowManager.Run(currentDirectory, environmentVariables, args, shadowCache,logger);
                 return result;
             }
             finally
