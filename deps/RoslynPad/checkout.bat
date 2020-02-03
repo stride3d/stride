83090d55695a5c0300e8ac75e8f13e6aba75fc31
tebjan <tebjan@vvvv.org>
2020-02-03 22:24:17 +0100

    [Assets] updated RoslyPad and Scripting to use Microsoft.CodeAnalysis 3.4.0
    # Conflicts:
    #	deps/RoslynPad/checkout.bat
    #	deps/RoslynPad/net462/RoslynPad.Editor.Windows.dll
    #	deps/RoslynPad/net462/RoslynPad.Editor.Windows.pdb
    #	deps/RoslynPad/net462/RoslynPad.Roslyn.Windows.dll
    #	deps/RoslynPad/net462/RoslynPad.Roslyn.Windows.pdb
    #	deps/RoslynPad/net462/RoslynPad.Roslyn.dll
    #	deps/RoslynPad/net462/RoslynPad.Roslyn.pdb

--- deps/RoslynPad/checkout.bat
+++ deps/RoslynPad/checkout.bat
@@ -8,6 +8,6 @@ IF NOT ERRORLEVEL 0 (
 ) 
 %GIT_CMD% clone --recursive https://github.com/aelij/RoslynPad -b master ../../externals/RoslynPad
 pushd ..\..\externals\RoslynPad
-%GIT_CMD% checkout c2f1c48
+%GIT_CMD% checkout 0d576ca5d18d67d447bce5d078f0a568a69a677d
 popd
 if ERRORLEVEL 1 echo "Could not checkout RoslynPad" && pause
