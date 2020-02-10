19547f9fdd60980f9257637e8700fdbd33fd0518
Virgile Bello <virgile@xenko.com>
2020-02-10 15:50:52 +0900

    [Editor] Fix copyright header in new files

--- sources/editor/Xenko.Core.Assets.Editor/View/Behaviors/TextBoxVectorPropertyValueValidationBehavior.cs
+++ sources/editor/Xenko.Core.Assets.Editor/View/Behaviors/TextBoxVectorPropertyValueValidationBehavior.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Copyright (c) Xenko contributors (https://xenko.com)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System.Linq;
 using System.Windows;
