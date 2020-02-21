061bc564bb354fb9d8f688e8cddd08a1d574c972
Basewq <Basewq@users.noreply.github.com>
2020-02-22 11:26:35 +1300

    [Core] Promote Matrix.Multiply remarks to summary tags so it's visible in Intellisense

--- sources/core/Xenko.Core.Mathematics/Matrix.cs
+++ sources/core/Xenko.Core.Mathematics/Matrix.cs
@@ -956,14 +956,12 @@ namespace Xenko.Core.Mathematics
 
         /// <summary>
         /// Determines the product of two matrices.
+        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
+        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
         /// </summary>
         /// <param name="left">The first matrix to multiply.</param>
         /// <param name="right">The second matrix to multiply.</param>
         /// <param name="result">The product of the two matrices.</param>
-        /// <remarks>
-        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
-        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
-        /// </remarks>
         public static void MultiplyTo(ref Matrix left, ref Matrix right, out Matrix result)
         {
             result.M11 = (left.M11 * right.M11) + (left.M12 * right.M21) + (left.M13 * right.M31) + (left.M14 * right.M41);
@@ -986,14 +984,12 @@ namespace Xenko.Core.Mathematics
 
         /// <summary>
         /// Determines the product of two matrices.
+        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
+        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
         /// </summary>
         /// <param name="left">The first matrix to multiply.</param>
         /// <param name="right">The second matrix to multiply.</param>
         /// <param name="result">The product of the two matrices.</param>
-        /// <remarks>
-        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
-        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
-        /// </remarks>
         public static void Multiply(ref Matrix left, ref Matrix right, out Matrix result)
         {
             result.M11 = (left.M11 * right.M11) + (left.M12 * right.M21) + (left.M13 * right.M31) + (left.M14 * right.M41);
@@ -1016,14 +1012,12 @@ namespace Xenko.Core.Mathematics
 
         /// <summary>
         /// Determines the product of two matrices.
+        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
+        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
         /// </summary>
         /// <param name="left">The first matrix to multiply.</param>
         /// <param name="right">The second matrix to multiply.</param>
         /// <param name="result">The product of the two matrices.</param>
-        /// <remarks>
-        /// Variables passed as <paramref name="left"/> or <paramref name="right"/> must not be used as the out parameter
-        /// <paramref name="result"/>, because <paramref name="result"/> is calculated in-place.
-        /// </remarks>
         public static void MultiplyRef(ref Matrix left, ref Matrix right, ref Matrix result)
         {
             result.M11 = (left.M11 * right.M11) + (left.M12 * right.M21) + (left.M13 * right.M31) + (left.M14 * right.M41);
