067d9b788393a1bcdcbf95d27ec37ca66a8571ea
Anon <makotech222@gmail.com>
2020-03-04 09:20:19 -0600

    Temporary commit to help debug.

--- sources/buildengine/Xenko.Core.BuildEngine.Common/XenkoServiceWireSerializer.cs
+++ sources/buildengine/Xenko.Core.BuildEngine.Common/XenkoServiceWireSerializer.cs
@@ -3,7 +3,9 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Xml;
+using Newtonsoft.Json;
 using ServiceWire;
 using Xenko.Core.Serialization;
 using Xenko.Core.Serialization.Contents;
@@ -14,6 +16,7 @@ namespace Xenko.Core.BuildEngine
     {
         public T Deserialize<T>(byte[] bytes)
         {
+            if (null == bytes || bytes.Length == 0) return default(T);
             var reader = new BinarySerializationReader(new MemoryStream(bytes));
             reader.Context.SerializerSelector = SerializerSelector.AssetWithReuse;
             reader.Context.Set(ContentSerializerContext.SerializeAttachedReferenceProperty, ContentSerializerContext.AttachedReferenceSerialization.AsSerializableVersion);
@@ -24,11 +27,20 @@ namespace Xenko.Core.BuildEngine
 
         public object Deserialize(byte[] bytes, string typeConfigName)
         {
-            throw new NotImplementedException();
+            if (null == typeConfigName) throw new ArgumentNullException(nameof(typeConfigName));
+            var type = typeConfigName.ToType();
+            if (null == typeConfigName || null == bytes || bytes.Length == 0) return type.GetDefault();
+            var reader = new BinarySerializationReader(new MemoryStream(bytes));
+            reader.Context.SerializerSelector = SerializerSelector.AssetWithReuse;
+            reader.Context.Set(ContentSerializerContext.SerializeAttachedReferenceProperty, ContentSerializerContext.AttachedReferenceSerialization.AsSerializableVersion);
+            object command = null;
+            reader.SerializeExtended(ref command, ArchiveMode.Deserialize, null);
+            return command;
         }
 
         public byte[] Serialize<T>(T obj)
         {
+            if (null == obj) return null;
             var memoryStream = new MemoryStream();
             var writer = new BinarySerializationWriter(memoryStream);
             writer.Context.SerializerSelector = SerializerSelector.AssetWithReuse;
@@ -40,7 +52,54 @@ namespace Xenko.Core.BuildEngine
 
         public byte[] Serialize(object obj, string typeConfigName)
         {
-            throw new NotImplementedException();
+            if (null == obj) return null;
+            var type = typeConfigName.ToType();
+            var memoryStream = new MemoryStream();
+            var writer = new BinarySerializationWriter(memoryStream);
+            writer.Context.SerializerSelector = SerializerSelector.AssetWithReuse;
+            writer.Context.Set(ContentSerializerContext.SerializeAttachedReferenceProperty, ContentSerializerContext.AttachedReferenceSerialization.AsSerializableVersion);
+            writer.SerializeExtended(ref obj, ArchiveMode.Serialize);
+
+            return memoryStream.ToArray();
+        }
+    }
+
+    public class NewtonsoftSerializer : ISerializer
+    {
+        private JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+        };
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            if (null == bytes || bytes.Length == 0) return default(T);
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+
+        public object Deserialize(byte[] bytes, string typeConfigName)
+        {
+            if (null == typeConfigName) throw new ArgumentNullException(nameof(typeConfigName));
+            var type = typeConfigName.ToType();
+            if (null == typeConfigName || null == bytes || bytes.Length == 0) return type.GetDefault();
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject(json, type, settings);
+        }
+
+        public byte[] Serialize<T>(T obj)
+        {
+            if (null == obj) return null;
+            var json = JsonConvert.SerializeObject(obj, settings);
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        public byte[] Serialize(object obj, string typeConfigName)
+        {
+            if (null == obj) return null;
+            var type = typeConfigName.ToType();
+            var json = JsonConvert.SerializeObject(obj, type, settings);
+            return Encoding.UTF8.GetBytes(json);
         }
     }
 }
---
+++ sources/core/Xenko.Core/Serialization/Serializers/ServiceWireSerializer.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ServiceWire;
+
+namespace Xenko.Core.Serialization.Serializers
+{
+    [DataSerializerGlobal(typeof(ServiceWireSerializer))]
+    public class ServiceWireSerializer : DataSerializer<ServiceSyncInfo>
+    {
+        public override void PreSerialize(ref ServiceSyncInfo obj, ArchiveMode mode, SerializationStream stream)
+        {
+            if (mode == ArchiveMode.Deserialize)
+            {
+                if (obj == null)
+                    obj = new ServiceSyncInfo();
+            }
+        }
+
+        public override void Serialize(ref ServiceSyncInfo info, ArchiveMode mode, SerializationStream stream)
+        {
+            if (mode == ArchiveMode.Serialize)
+            {
+                stream.Write(info.CompressionThreshold);
+                stream.Write(info.ServiceKeyIndex);
+                stream.Write(info.UseCompression);
+                stream.Write(info.MethodInfos.Length);
+                foreach (var method in info.MethodInfos)
+                {
+                    stream.Write(method.MethodIdent);
+                    stream.Write(method.MethodName);
+                    stream.Write(method.MethodReturnType);
+                    stream.Write(method.ParameterTypes.Length);
+                    foreach (var paramType in method.ParameterTypes)
+                    {
+                        stream.Write(paramType);
+                    }
+                }
+            }
+            else if (mode == ArchiveMode.Deserialize)
+            {
+                info.CompressionThreshold = stream.ReadInt32();
+                info.ServiceKeyIndex = stream.ReadInt32();
+                info.UseCompression = stream.ReadBoolean();
+                var length = stream.ReadInt32();
+                info.MethodInfos = new MethodSyncInfo[length];
+                for (int i = 0; i < length; i++)
+                {
+                    info.MethodInfos[i].MethodIdent = stream.ReadInt32();
+                    info.MethodInfos[i].MethodName = stream.ReadString();
+                    info.MethodInfos[i].MethodReturnType = stream.ReadString();
+                    var paramLength = stream.ReadInt32();
+                    info.MethodInfos[i].ParameterTypes = new string[paramLength];
+                    for (int j = 0; j < length; j++)
+                    {
+                        info.MethodInfos[i].ParameterTypes[j] = stream.ReadString();
+                    }
+                }
+            }
+        }
+    }
+}
