f80f2d7723f4cdbe5a3ee10c0a21bb1786a49b8c
EternalTamago <13661631+EternalTamago@users.noreply.github.com>
2020-03-14 21:59:02 +0900

    Change CustomHeightScale to use fraction

--- sources/engine/Xenko.Physics/CustomHeightScaleCalculator.cs
+++ sources/engine/Xenko.Physics/CustomHeightScaleCalculator.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Xenko contributors (https://xenko.com)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using Xenko.Core;
+using Xenko.Core.Mathematics;
 
 namespace Xenko.Physics
 {
@@ -9,8 +10,11 @@ namespace Xenko.Physics
     public class CustomHeightScaleCalculator : IHeightScaleCalculator
     {
         [DataMember(10)]
-        public float CustomHeightScale { get; set; } = 1f;
+        public float Numerator { get; set; } = 1;
 
-        public float Calculate(IHeightStickParameters heightDescription) => CustomHeightScale;
+        [DataMember(20)]
+        public float Denominator { get; set; } = 255;
+
+        public float Calculate(IHeightStickParameters heightDescription) => MathUtil.IsZero(Denominator) ? 0 : (Numerator / Denominator);
     }
 }
