sierra-zero/FocusEngine

[Editor] Add Theme (color scheme) config option

sources/editor/Xenko.Core.Assets.Editor/Settings/ThemesSettings.cs
@@ -1,0 +1,29 @@
+// Copyright (c) Xenko contributors (https://xenko.com)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using Xenko.Core.Assets.Editor.Settings;
+using Xenko.Core.Settings;
+using Xenko.Core.Translation;
+
+namespace Xenko.Core.Presentation.Themes
+{
+    public static class ThemesSettings
+    {
+        // Categories
+        public static readonly string Themes = Tr._p("Settings", "Themes");
+
+        static ThemesSettings()
+        {
+            ThemeName = new SettingsKey<ThemeType>("Themes/ThemeName", EditorSettings.SettingsContainer, ThemeType.ExpressionDark)
+            {
+                DisplayName = $"{Themes}/{Tr._p("Settings", "Theme Name")}"
+            };
+        }
+
+        public static SettingsKey<ThemeType> ThemeName { get; }
+
+        public static void Initialize()
+        {
+            ThemeController.CurrentTheme = ThemeName.GetValue();
+        }
+    }
+}

sources/presentation/Xenko.Core.Presentation/Themes/ThemeResourceDictionary.cs
@@ -1,0 +1,52 @@
+// Copyright (c) Xenko contributors (https://xenko.com)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using System;
+using System.Windows;
+
+namespace Xenko.Core.Presentation.Themes
+{
+    public class ThemeResourceDictionary : ResourceDictionary
+    {
+        private Uri expressionDarkSource;
+        private Uri darkSteelSource;
+
+        public Uri ExpressionDarkSource
+        {
+            get => expressionDarkSource;
+            set => SetValue(ref expressionDarkSource,  value);
+        }
+
+        public Uri DarkSteelSource
+        {
+            get => darkSteelSource;
+            set => SetValue(ref darkSteelSource, value);
+        }
+
+        public void UpdateSource(ThemeType themeType)
+        {
+            switch (themeType)
+            {
+                case ThemeType.ExpressionDark:
+                    if (ExpressionDarkSource != null)
+                        Source = ExpressionDarkSource;
+                    break;
+
+                case ThemeType.DarkSteel:
+                    if (DarkSteelSource != null)
+                        Source = DarkSteelSource;
+                    break;
+            }
+        }
+
+        private void SetValue(ref Uri sourceBackingField, Uri value)
+        {
+            sourceBackingField = value;
+            UpdateSource(ThemeController.CurrentTheme);
+        }
+
+        protected override void OnGettingValue(object key, ref object value, out bool canCache)
+        {
+            base.OnGettingValue(key, ref value, out canCache);
+        }
+    }
+}

stride3d/stride

sources/editor/Stride.Core.Assets.Editor/Settings/ThemesSettings.cs
@@ -1,0 +1,29 @@
+// Copyright (c) Stride contributors (https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using Stride.Core.Assets.Editor.Settings;
+using Stride.Core.Settings;
+using Stride.Core.Translation;
+
+namespace Stride.Core.Presentation.Themes
+{
+    public static class ThemesSettings
+    {
+        // Categories
+        public static readonly string Themes = Tr._p("Settings", "Themes");
+
+        static ThemesSettings()
+        {
+            ThemeName = new SettingsKey<ThemeType>("Themes/ThemeName", EditorSettings.SettingsContainer, ThemeType.ExpressionDark)
+            {
+                DisplayName = $"{Themes}/{Tr._p("Settings", "Theme Name")}"
+            };
+        }
+
+        public static SettingsKey<ThemeType> ThemeName { get; }
+
+        public static void Initialize()
+        {
+            ThemeController.CurrentTheme = ThemeName.GetValue();
+        }
+    }
+}

sources/presentation/Stride.Core.Presentation/Themes/ThemeResourceDictionary.cs
@@ -1,0 +1,49 @@
+// Copyright (c) Stride contributors (https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using System;
+using System.Windows;
+
+namespace Stride.Core.Presentation.Themes
+{
+    public class ThemeResourceDictionary : ResourceDictionary
+    {
+        private Uri expressionDarkSource;
+        private Uri darkSteelSource;
+
+        // New themes are added here as new properties.
+
+        public Uri ExpressionDarkSource
+        {
+            get => expressionDarkSource;
+            set => SetValue(ref expressionDarkSource,  value);
+        }
+
+        public Uri DarkSteelSource
+        {
+            get => darkSteelSource;
+            set => SetValue(ref darkSteelSource, value);
+        }
+
+        public void UpdateSource(ThemeType themeType)
+        {
+            switch (themeType)
+            {
+                case ThemeType.ExpressionDark:
+                    if (ExpressionDarkSource != null)
+                        Source = ExpressionDarkSource;
+                    break;
+
+                case ThemeType.DarkSteel:
+                    if (DarkSteelSource != null)
+                        Source = DarkSteelSource;
+                    break;
+            }
+        }
+
+        private void SetValue(ref Uri sourceBackingField, Uri value)
+        {
+            sourceBackingField = value;
+            UpdateSource(ThemeController.CurrentTheme);
+        }
+    }
+}