0c720bdf243d7dc256b3eb003135aef394388e31
Aggror <aggror.jorn@gmail.com>
2020-05-04 23:40:42 +0200

    Improved properties tutorial

--- samples/Tutorials/CSharpBeginner/CSharpBeginner/CSharpBeginner.Game/Code/PropertiesDemo.cs
+++ samples/Tutorials/CSharpBeginner/CSharpBeginner/CSharpBeginner.Game/Code/PropertiesDemo.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Stride.Core;
+using Stride.Core.Annotations;
 using Stride.Core.Mathematics;
 using Stride.Engine;
 
@@ -12,40 +13,65 @@ namespace CSharpBeginner.Code
     /// </summary>
     public class PropertiesDemo : SyncScript
     {
-        public bool SomeBoolean = true;
-        public float SomeFloat = 5.6f;
+        // By default public properties are ordered alphabetically
+        public string SomeString = "Hello Stride";
         public int SomeInteger = 10;
-        public string SomeString = "Hello world";
-        public Color SomeColor = Color.Red;
-        public Vector2 SomeVector2 = new Vector2(1, 2);
+
+        // Use DataMember followed by a number to order properties by number
+        [DataMember(5)]
+        public bool SomeBoolean = true;
+
+        //Instead of using the name of the variable, you can also define your own text
+        [DataMember(4, "My custom text")]
+        public float SomeFloat = 5.6f;
+
+        public Vector2 SomeVector2; // Is the same as = new Vector2(0, 0);
         public Vector3 SomeVector3 = new Vector3(1, 2, 3);
-        public Vector4 SomeVector4 = new Vector4(1, 2, 3, 4);
+        public Vector4 SomeVector4 = new Vector4(5); //All 4 float value get the value of 5
+        public Color SomeColor = Color.Red;
 
-        // We can reference other entities to our script by using the Entity class
-        public Entity SomeEntity;
+        // Turns a float in to a range slider
+        // Dragging the slider uses the smallstep value
+        // Clicking the slider uses the bigstep value
+        [DataMemberRange(1, 100, 0.1, 1, 3)]
+        public float RangedFloat = 10.0f;
 
-        // If we want a list of ojects like strings, integers or even Entities, we have to create the new List right away
+        // If we want a list of ojects like strings, Entities or specific components, we have to create the new List right away
         public List<string> StringList = new List<string>();
         public List<Entity> EntityList = new List<Entity>();
+        public List<CameraComponent> CameraList = new List<CameraComponent>();
 
         // If we dont want a public property to be visible in the editor we can use '[DataMemberIgnore]'
         [DataMemberIgnore]
-        public string SomeHiddenProperty = "HiddenInEditor";
+        public string HideMe;
+
+        //Enums can be used for dropdowns
+        public CharacterType Character;
+        public enum CharacterType
+        {
+            Warrior,
+            Archer,
+            Mage
+        }
+
+        /// <userdoc> This is a super long description. Use it to help the people that use your components, understand what this propery does.
+        /// The text above is displayed in game studio and also shows up when you hover over the property
+        public string Explanation;
 
         public override void Update()
         {
             var x = 400;
-            DebugText.Print("Integer: " + SomeInteger, new Int2(x, 200));
-            DebugText.Print("Float: " + SomeFloat, new Int2(x, 220));
-            DebugText.Print("Boolean: " + SomeBoolean, new Int2(x, 240));
-            DebugText.Print("String: " + SomeString, new Int2(x, 260));
-            DebugText.Print("Vector2: " + SomeVector2, new Int2(x, 280));
-            DebugText.Print("Vector3: " + SomeVector3, new Int2(x, 300));
-            DebugText.Print("Vector4: " + SomeVector4, new Int2(x, 320));
-            DebugText.Print("Color: " + SomeColor, new Int2(x, 340));
-            DebugText.Print("Entity: " + SomeEntity.Name, new Int2(x, 360));
-            DebugText.Print("String list count: " + StringList.Count, new Int2(x, 380));
-            DebugText.Print("Entity list count: " + EntityList.Count, new Int2(x, 400));
+            DebugText.Print("Integer: " + SomeInteger,  new Int2(x, 200));
+            DebugText.Print("Float: " + SomeFloat,      new Int2(x, 220));
+            DebugText.Print("Boolean: " + SomeBoolean,  new Int2(x, 240));
+            DebugText.Print("String: " + SomeString,    new Int2(x, 260));
+            DebugText.Print("Vector2: " + SomeVector2,  new Int2(x, 280));
+            DebugText.Print("Vector3: " + SomeVector3,  new Int2(x, 300));
+            DebugText.Print("Vector4: " + SomeVector4,  new Int2(x, 320));
+            DebugText.Print("Color: " + SomeColor,      new Int2(x, 340));
+            DebugText.Print("String list count: " + StringList.Count, new Int2(x, 360));
+            DebugText.Print("Entity list count: " + EntityList.Count, new Int2(x, 380));
+            DebugText.Print("Camera list count: " + CameraList.Count, new Int2(x, 400));
         }
     }
 }
