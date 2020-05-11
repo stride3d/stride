7b4ea922a3a5c4fb6aa2fceab7d049bd3e9807e9
Basewq <Basewq@users.noreply.github.com>
2020-05-11 14:52:11 +1200

    [Samples] Minor typo fixes

--- samples/Tutorials/CSharpBeginner/CSharpBeginner/CSharpBeginner.Game/Code/KeyboardInputDemo.cs
+++ samples/Tutorials/CSharpBeginner/CSharpBeginner/CSharpBeginner.Game/Code/KeyboardInputDemo.cs
@@ -21,7 +21,7 @@ namespace CSharpBeginner.Code
             if (Input.HasKeyboard)
             {
                 // Key down is used for when a key is being held down.
-                DebugText.Print("Hold the 1 key down to rotate the blue theapot", new Int2(340, 500));
+                DebugText.Print("Hold the 1 key down to rotate the blue teapot", new Int2(340, 500));
                 if (Input.IsKeyDown(Keys.D1))
                 {
                     var deltaTime = (float)Game.UpdateTime.Elapsed.TotalSeconds;
@@ -29,14 +29,14 @@ namespace CSharpBeginner.Code
                 }
 
                 // Use 'IsKeyPressed' for a single key press event. 
-                DebugText.Print("Press F to rotate the yellow theapot (and to pay respects)", new Int2(340, 520));
+                DebugText.Print("Press F to rotate the yellow teapot (and to pay respects)", new Int2(340, 520));
                 if (Input.IsKeyPressed(Keys.F))
                 {
                     YellowTeapot.Transform.Rotation *= Quaternion.RotationY(-0.4f);
                 }
 
                 // 'IsKeyReleased' is used for when you want to know when a key is released after being either held down or pressed. 
-                DebugText.Print("Press and release the Space bar to rotate the green theapot", new Int2(340, 540));
+                DebugText.Print("Press and release the Space bar to rotate the green teapot", new Int2(340, 540));
                 if (Input.IsKeyReleased(Keys.Space))
                 {
                     GreenTeapot.Transform.Rotation *= Quaternion.RotationY(0.6f);
