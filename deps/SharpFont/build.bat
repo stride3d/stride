581d4e173cec4936c7eec78daee15e718884df00
Virgile Bello <virgile@xenko.com>
2020-05-12 15:32:28 +0200

    [Graphics] SharpFont: switch to nuget and .NET Standard 2.0

--- deps/SharpFont/build.bat
+++
@@ -1,5 +0,0 @@
-msbuild ..\..\externals\SharpFont\Source\SharpFont.sln /p:Configuration=Release
-
-xcopy /Y /S ..\..\externals\SharpFont\Binaries\SharpFont\Portable\Release\* Portable\
-xcopy /Y /S ..\..\externals\SharpFont\Binaries\SharpFont\iOS\Release\* iOS\
-xcopy /Y /S ..\..\externals\SharpFont\Binaries\SharpFont\Linux\Release\* Linux\
