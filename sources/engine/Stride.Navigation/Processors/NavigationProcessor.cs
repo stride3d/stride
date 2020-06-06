09adafb606b0c9d5e964f224535fb954c0b6b5f3
tebjan <tebjan@vvvv.org>
2020-06-06 21:39:50 +0200

    [Navigation] Fixed copy & paste error

--- sources/engine/Stride.Navigation/Processors/NavigationProcessor.cs
+++ sources/engine/Stride.Navigation/Processors/NavigationProcessor.cs
@@ -48,7 +48,7 @@ namespace Stride.Navigation.Processors
         {
             if (gameSystemCollection != null)
             {
-                gameSystemCollection.CollectionChanged += GameSystemsOnCollectionChanged;
+                gameSystemCollection.CollectionChanged -= GameSystemsOnCollectionChanged;
             }
 
             if (dynamicNavigationMeshSystem != null)
