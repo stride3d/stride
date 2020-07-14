921e6af011dbe349c6f54aaf85ee218eca5de95e
Virgile Bello <virgile@xenko.com>
2020-07-14 15:08:26 +0200

    [Voxels] Updated copyright headers

--- sources/engine/Stride.Voxels/Voxels/Voxelization/VoxelStorage/VoxelStorers/VoxelStorerClipmap.cs
+++ sources/engine/Stride.Voxels/Voxels/Voxelization/VoxelStorage/VoxelStorers/VoxelStorerClipmap.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Stride contributors (https://stride3d.net) and Sean Boettger <sean@whypenguins.com>
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using System;
 using System.Collections.Generic;
 using Stride.Core.Mathematics;
 using Stride.Core;
