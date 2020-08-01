ac74ee0e59eef7f41efd1138e51c13941a554341
Johan Gustafsson <johan@tinyrocket.se>
2020-08-01 14:50:52 +0200

    Fixed missing usings

--- sources/engine/Stride.Engine/Animations/AnimationCurveEvaluatorDirectGroup.cs
+++ sources/engine/Stride.Engine/Animations/AnimationCurveEvaluatorDirectGroup.cs
@@ -2,6 +2,7 @@
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 #pragma warning disable SA1402 // File may only contain a single class
 using System;
+using Stride.Core;
 using Stride.Core.Collections;
 using Stride.Core.Mathematics;
 using Stride.Updater;
