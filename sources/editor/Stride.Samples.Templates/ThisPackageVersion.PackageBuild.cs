5a872a79f8cd42e4ed1aceaba72f6932899713bb
Virgile Bello <virgile@xenko.com>
2020-09-08 10:43:47 +0200

    [Build] Samples package now has a different version when doing local build (otherwise it was conflicting)

---
+++ sources/editor/Stride.Samples.Templates/ThisPackageVersion.PackageBuild.cs
@@ -0,0 +1,10 @@
+#if STRIDE_PACKAGE_BUILD
+namespace Stride.Samples.Templates
+{
+    static class ThisPackageVersion
+    {
+        // we version this package manually because most of the time the samples are big and don't need to be updated
+        public static string Current = "4.0.0.1-beta05";
+    }
+}
+#endif
