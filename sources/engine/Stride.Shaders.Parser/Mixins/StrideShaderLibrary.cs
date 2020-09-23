05f68f448bf0768f8e1ac5a2c1867ee65debe76d
Elias Holzer <elias@vvvv.org>
2020-09-23 17:13:38 +0200

    [Shaders] Removed unnecessary distinct/clone call - the minimal context is already a hash set and the contained items don't have the equality overwritten

--- sources/engine/Stride.Shaders.Parser/Mixins/StrideShaderLibrary.cs
+++ sources/engine/Stride.Shaders.Parser/Mixins/StrideShaderLibrary.cs
@@ -197,7 +197,6 @@ namespace Stride.Shaders.Parser.Mixins
                     {
                         LoadNecessaryShaders(mixinInfo, macros, macrosString);
                     }
-                    mixinInfo.MinimalContext = new HashSet<ModuleMixinInfo>(mixinInfo.MinimalContext.Distinct());
                 }
             }
 
