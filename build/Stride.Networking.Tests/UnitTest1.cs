d37b02512d610cb87b1598d41734aca8b7869eb9
Ty Alex <spinnerbowl@hotmail.com>
2020-10-08 09:52:21 -0400

    Added test prodject for networking

---
+++ build/Stride.Networking.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+using Telepathy;
+using NUnit.Framework;
+using Stride.Networking;
+namespace Stride.Networking.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string s = "T";
+            var server = MainTransportTCP.CreateServer(80);
+            var client = MainTransportTCP.ConnectAndCreateClient("localhost", 80);
+            client.Send(new UTF8Encoding(true).GetBytes(s));
+            Message message;
+            while (true)
+            {
+                client.Send(new UTF8Encoding(true).GetBytes(s));
+                while (server.GetNextMessage(out message))
+                {
+                    switch (message.eventType)
+                    {
+                        case EventType.Connected:
+                            break;
+                        case EventType.Data:
+                            Assert.That(new UTF8Encoding(true).GetString(message.data), Is.EqualTo(s));
+                            return;
+                        case EventType.Disconnected:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
