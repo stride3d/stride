e031e471dd41ccaa653edeac6f2767c8a4320ce0
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-11-08 11:20:24 +0000

    [Editor] Fix command adding in GameSettingsNodeUpdater

--- sources/editor/Stride.Assets.Presentation/NodePresenters/Updaters/GameSettingsAssetNodeUpdater.cs
+++ sources/editor/Stride.Assets.Presentation/NodePresenters/Updaters/GameSettingsAssetNodeUpdater.cs
@@ -27,7 +27,8 @@ namespace Stride.Assets.Presentation.NodePresenters.Updaters
                     item.Order = node.Order + item.Index.Int;
                     item.AttachedProperties.Add(CategoryData.Key, true);
                 }
-                node.Commands.Add(new AddNewItemCommand());
+                if (!node.Commands.Any(cmd => cmd.Name == AddNewItemCommand.CommandName))
+                    node.Commands.Add(new AddNewItemCommand());
             }
 
             if (typeof(Configuration).IsAssignableFrom(node.Type))
