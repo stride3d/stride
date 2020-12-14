672c9dced451de420ede6fa773e418d351ed92db
Elias Holzer <elias@vvvv.org>
2020-12-14 20:57:58 +0100

    [Games] Adds Alt+Enter (keypad) to fullscreen toggle shortcuts.

--- sources/engine/Stride.Games/SDL/GameFormSDL.cs
+++ sources/engine/Stride.Games/SDL/GameFormSDL.cs
@@ -139,7 +139,9 @@ namespace Stride.Games
 
         private void GameFormSDL_KeyDownActions(SDL.SDL_KeyboardEvent e)
         {
-            if ((e.keysym.sym == SDL.SDL_Keycode.SDLK_RETURN) && ((e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != 0))
+            var altReturn = (e.keysym.sym == SDL.SDL_Keycode.SDLK_RETURN) && ((e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != 0);
+            var altEnter = (e.keysym.sym == SDL.SDL_Keycode.SDLK_KP_ENTER) && ((e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != 0);
+            if (altReturn || altEnter)
             {
                 FullscreenToggle?.Invoke(this, EventArgs.Empty);
             }
