607319f0cc4fc41bed653b0dc7365f16d913cb38
Virgile Bello <virgile@xenko.com>
2021-02-10 16:35:06 +0900

    [GameStudio] Tests: fix build

--- sources/editor/Stride.GameStudio.Tests/Module.cs
+++ sources/editor/Stride.GameStudio.Tests/Module.cs
@@ -12,7 +12,7 @@ namespace Stride.GameStudio.Tests
 {
     public class Module
     {
-        [ModuleInitializer]
+        [Core.ModuleInitializer]
         internal static void Initialize()
         {
             PackageSessionPublicHelper.FindAndSetMSBuildVersion();
