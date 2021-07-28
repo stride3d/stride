d9fdc6f1a70a742a81afb93fab71192f27c1b671
Youness KAFIA <kafia.youness@hotmail.fr>
2021-07-28 15:50:24 +0200

    Correction from source

--- sources/core/Stride.Core.Mathematics/Quaternion.cs
+++ sources/core/Stride.Core.Mathematics/Quaternion.cs
@@ -459,7 +459,7 @@ namespace Stride.Core.Mathematics
             Multiply(ref left, ref right, out result);
             return result;
         }
-        
+
         /// <summary>
         /// Reverses the direction of a given quaternion.
         /// </summary>
@@ -977,7 +977,7 @@ namespace Stride.Core.Mathematics
             var halfRoll = roll * 0.5f;
             var halfPitch = pitch * 0.5f;
             var halfYaw = yaw * 0.5f;
-            
+
             var sinRoll = (float)Math.Sin(halfRoll);
             var cosRoll = (float)Math.Cos(halfRoll);
             var sinPitch = (float)Math.Sin(halfPitch);
@@ -1049,35 +1049,71 @@ namespace Stride.Core.Mathematics
         }
 
         /// <summary>
-        /// Computes a quaternion corresponding to the rotation looking from the position<paramref name="eye"/> to the position <paramref name="target"/>.
+        /// Computes a quaternion corresponding to the rotation from a direction <paramref name="direction"/> and the up vector <paramref name="target"/>.
         /// </summary>
-        /// <param name="eye">The position of the eye.</param>
-        /// <param name="target">The target to look at.</param>
+        /// <param name="direction">The direction to look at.</param>
+        /// <param name="up">The up vector.</param>
         /// <returns>The resulting quaternion of the rotation from the .</returns>
-        public static Quaternion LookAtRH(Vector3 eye, Vector3 target)
+        public static Quaternion LookAt(Vector3 direction, Vector3 up)
         {
-            Vector3 forwardVector = Vector3.Normalize(target - eye);
-            Vector3 axis = Vector3.Cross(-Vector3.UnitZ, forwardVector);
-            float dot = Vector3.Dot(-Vector3.UnitZ, forwardVector);
+            // Taken from http://answers.unity.com/answers/467979/view.html
+            direction.Normalize();
 
-            return Normalize(new Quaternion(axis, dot + 1));
-        }
-        /// <summary>
-        /// Computes a quaternion corresponding to the rotation looking from the position<paramref name="eye"/> to the position <paramref name="target"/>.
-        /// </summary>
-        /// <param name="eye">The position of the eye.</param>
-        /// <param name="target">The target to look at.</param>
-        /// <param name="result">The resulting direction</param>
-        public static void LookAtRH(Vector3 eye, Vector3 target, out Quaternion result)
-        {
-            Vector3 forwardVector = Vector3.Normalize(target - eye);
-            Vector3 axis = Vector3.Cross(-Vector3.UnitZ, forwardVector);
-            float dot = Vector3.Dot(-Vector3.UnitZ, forwardVector);
+            Vector3 vector = Vector3.Normalize(direction);
+            Vector3 vector2 = Vector3.Normalize(Vector3.Cross(up, vector));
+            Vector3 vector3 = Vector3.Cross(vector, vector2);
+            var m00 = vector2.X;
+            var m01 = vector2.Y;
+            var m02 = vector2.Z;
+            var m10 = vector3.X;
+            var m11 = vector3.Y;
+            var m12 = vector3.Z;
+            var m20 = vector.X;
+            var m21 = vector.Y;
+            var m22 = vector.Z;
 
-            result.X = axis.X;
-            result.Y = axis.Y;
-            result.Z = axis.Z;
-            result.W = dot + 1;
+
+            float num8 = m00 + m11 + m22;
+            var quaternion = new Quaternion();
+            if (num8 > 0f)
+            {
+                var num = (float)Math.Sqrt(num8 + 1f);
+                quaternion.W = num * 0.5f;
+                num = 0.5f / num;
+                quaternion.X = (m12 - m21) * num;
+                quaternion.Y = (m20 - m02) * num;
+                quaternion.Z = (m01 - m10) * num;
+                return quaternion;
+            }
+            if ((m00 >= m11) && (m00 >= m22))
+            {
+                var num7 = (float)Math.Sqrt(1f + m00 - m11 - m22);
+                var num4 = 0.5f / num7;
+                quaternion.X = 0.5f * num7;
+                quaternion.Y = (m01 + m10) * num4;
+                quaternion.Z = (m02 + m20) * num4;
+                quaternion.W = (m12 - m21) * num4;
+                return quaternion;
+            }
+            if (m11 > m22)
+            {
+                var num6 = (float)Math.Sqrt(1f + m11 - m00 - m22);
+                var num3 = 0.5f / num6;
+                quaternion.X = (m10 + m01) * num3;
+                quaternion.Y = 0.5f * num6;
+                quaternion.Z = (m21 + m12) * num3;
+                quaternion.W = (m20 - m02) * num3;
+                return quaternion;
+            }
+            var num5 = (float)Math.Sqrt(1f + m22 - m00 - m11);
+            var num2 = 0.5f / num5;
+            quaternion.X = (m20 + m02) * num2;
+            quaternion.Y = (m21 + m12) * num2;
+            quaternion.Z = 0.5f * num5;
+            quaternion.W = (m01 - m10) * num2;
+            return quaternion;
+
+
         }
 
         /// <summary>
