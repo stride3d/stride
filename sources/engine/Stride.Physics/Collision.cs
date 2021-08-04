ccd29c6fe752eb86c5d87a118fe7425818ae75dd
Eideren <contact@eideren.com>
2021-08-04 13:44:04 +0200

    [Physics] Improve collision and contact harvesting

--- sources/engine/Stride.Physics/Collision.cs
+++ sources/engine/Stride.Physics/Collision.cs
@@ -5,58 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Stride.Core.MicroThreading;
-using Stride.Core.Threading;
 using Stride.Engine;
 
 namespace Stride.Physics
 {
-    public class Collision
+    /// <summary>
+    /// A pair of component colliding with each other.
+    /// Pair of [b,a] is considered equal to [a,b].
+    /// </summary>
+    public readonly struct Collision : IEquatable<Collision>
     {
-        private static readonly Queue<Channel<ContactPoint>> ChannelsPool = new Queue<Channel<ContactPoint>>();
-        private bool destroyed;
+        public readonly PhysicsComponent ColliderA;
 
-        internal Collision()
-        {
-        }
-
-        public void Initialize(PhysicsComponent colliderA, PhysicsComponent colliderB)
-        {
-            ColliderA = colliderA;
-            ColliderB = colliderB;
-
-            NewContactChannel = ChannelsPool.Count == 0 ? new Channel<ContactPoint> { Preference = ChannelPreference.PreferSender } : ChannelsPool.Dequeue();
-            ContactUpdateChannel = ChannelsPool.Count == 0 ? new Channel<ContactPoint> { Preference = ChannelPreference.PreferSender } : ChannelsPool.Dequeue();
-            ContactEndedChannel = ChannelsPool.Count == 0 ? new Channel<ContactPoint> { Preference = ChannelPreference.PreferSender } : ChannelsPool.Dequeue();
-        }
-
-        internal void Destroy()
-        {
-            // Because we raise CollisionEnded for removed components right before
-            // Simulation.BeginContactTesting() cleans up the ended collisions
-            // we need to retain the collider information for an additional frame.
-            // The collision has been removed from PhysicsComponent.Collisions
-            // and the user should not store the collision object between frames.
-            if (!HasEndedFromComponentRemoval)
-            {
-                ColliderA = null;
-                ColliderB = null;
-            }
-            NewContactChannel.Reset();
-            ContactUpdateChannel.Reset();
-            ContactEndedChannel.Reset();
-            ChannelsPool.Enqueue(NewContactChannel);
-            ChannelsPool.Enqueue(ContactUpdateChannel);
-            ChannelsPool.Enqueue(ContactEndedChannel);
-            Contacts.Clear();
-
-            destroyed = true;
-        }
-
-        public PhysicsComponent ColliderA { get; private set; }
-
-        public PhysicsComponent ColliderB { get; private set; }
-
-        public HashSet<ContactPoint> Contacts = new HashSet<ContactPoint>(ContactPointEqualityComparer.Default);
+        public readonly PhysicsComponent ColliderB;
 
         /// <summary>
         /// True if the collision has ended because one of the colliders has been removed,
@@ -67,70 +28,54 @@ namespace Stride.Physics
         /// If true, it is not safe to invoke further actions on the colliders.
         /// Only use colliders information to identify the entity that has been removed.
         /// </remarks>
-        public bool HasEndedFromComponentRemoval { get; internal set; }
+        public bool HasEndedFromComponentRemoval => ColliderA.Simulation.EndedFromComponentRemoval.Contains(this);
 
-        internal Channel<ContactPoint> NewContactChannel;
+        public HashSet<ContactPoint> Contacts => ColliderA.Simulation.LatestContactPointsFor(this);
 
-        public ChannelMicroThreadAwaiter<ContactPoint> NewContact()
+        internal Collision(PhysicsComponent a, PhysicsComponent b)
         {
-            if (destroyed)
-                throw new InvalidOperationException("The collision object has been destroyed.");
-
-            return NewContactChannel.Receive();
+            ColliderA = a;
+            ColliderB = b;
         }
 
-        internal Channel<ContactPoint> ContactUpdateChannel;
-
-        public ChannelMicroThreadAwaiter<ContactPoint> ContactUpdate()
+        /// <summary>
+        /// The returned collection contains the previous contacts, new contacts are under <see cref="Contacts"/>
+        /// </summary>
+        public ChannelMicroThreadAwaiter<HashSet<ContactPoint>> ContactChanged()
         {
-            if (destroyed)
-                throw new InvalidOperationException("The collision object has been destroyed.");
-
-            return ContactUpdateChannel.Receive();
-        }
-
-        internal Channel<ContactPoint> ContactEndedChannel;
-
-        public ChannelMicroThreadAwaiter<ContactPoint> ContactEnded()
-        {
-            if (destroyed)
-                throw new InvalidOperationException("The collision object has been destroyed.");
-
-            return ContactEndedChannel.Receive();
+            return ColliderA.Simulation.ContactChanged(this);
         }
 
         public async Task Ended()
         {
-            if (destroyed)
+            if (ColliderA.Simulation.CurrentCollisions.Contains(this) == false)
                 throw new InvalidOperationException("The collision object has been destroyed.");
 
             Collision endCollision;
             do
             {
                 endCollision = await ColliderA.CollisionEnded();
-            }
-            while (!endCollision.Equals(this));
+            } while (!endCollision.Equals(this));
         }
 
         public override bool Equals(object obj)
         {
-            var other = (Collision)obj;
-            return other != null && ((other.ColliderA == ColliderA && other.ColliderB == ColliderB) || (other.ColliderB == ColliderA && other.ColliderA == ColliderB));
+            return obj is Collision other && Equals(other);
+        }
+
+        public bool Equals(Collision other)
+        {
+            return (Equals(ColliderA, other.ColliderA) && Equals(ColliderB, other.ColliderB)) 
+                   || (Equals(ColliderB, other.ColliderA) && Equals(ColliderA, other.ColliderB));
         }
 
         public override int GetHashCode()
         {
-            unchecked
-            {
-                var result = ColliderA?.GetHashCode() ?? 0;
-                result = (result * 397) ^ (ColliderB?.GetHashCode() ?? 0);
-                return result;
-            }
-        }
-
-        internal bool InternalEquals(PhysicsComponent a, PhysicsComponent b)
-        {
-            return (ColliderA == a && ColliderB == b) || (ColliderB == a && ColliderA == b);
+            int aH = ColliderA.GetHashCode();
+            int bH = ColliderB.GetHashCode();
+            // This ensures that a pair of components will return the same hash regardless
+            // of if they are setup as [b,a] or [a,b]
+            return aH > bH ? HashCode.Combine(aH, bH) : HashCode.Combine(bH, aH);
         }
     }
 }
