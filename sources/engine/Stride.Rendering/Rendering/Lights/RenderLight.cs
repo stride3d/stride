fad253b38e2af76c1fb20529f0b01b9c12b01d09
Eideren <contact@eideren.com>
2022-01-14 09:22:43 +0100

    Revert "[Lighting] Expose RenderLight bounding boxes (#1275)" (#1278)
    This reverts commit 4cd4ff31504502f081c6f6530d81544a14e2e789.

--- sources/engine/Stride.Rendering/Rendering/Lights/RenderLight.cs
+++ sources/engine/Stride.Rendering/Rendering/Lights/RenderLight.cs
@@ -32,12 +32,12 @@ namespace Stride.Rendering.Lights
         /// <summary>
         /// The bounding box of this light in WS after the <see cref="LightProcessor"/> has been applied (readonly field).
         /// </summary>
-        public BoundingBox BoundingBox { get; internal set; }
+        internal BoundingBox BoundingBox;
 
         /// <summary>
         /// The bounding box extents of this light in WS after the <see cref="LightProcessor"/> has been applied (readonly field).
         /// </summary>
-        public BoundingBoxExt BoundingBoxExt { get; internal set; }
+        internal BoundingBoxExt BoundingBoxExt;
 
         /// <summary>
         /// The determines whether this instance has a valid bounding box (readonly field).
