974f4316d4f210cc5ac6452739500259bc144850
Aggror <aggror.jorn@gmail.com>
2022-01-23 04:19:56 +0100

    raycast wip

--- samples/Tutorials/CSharpIntermediate/CSharpIntermediate/Code/3 - Raycasting/RaycastDemo.cs
+++ samples/Tutorials/CSharpIntermediate/CSharpIntermediate/Code/3 - Raycasting/RaycastDemo.cs
@@ -9,10 +9,10 @@ namespace CSharpIntermediate.Code
     {
         public float RotationSpeed = 0.4f;
         public float MaxDistance = 4.0f;
-        Entity barrel;
-        Entity laser;
-        Entity hitPointVisualiser;
-        Simulation simulation;
+        private Entity barrel;
+        private Entity laser;
+        private Entity hitPointVisualiser;
+        private Simulation simulation;
 
         public override void Start()
         {
@@ -30,20 +30,20 @@ namespace CSharpIntermediate.Code
             hitPointVisualiser.Transform.Position = barrel.Transform.Position + new Vector3(0, 0, MaxDistance);
             var raycastEndWorldPosition = hitPointVisualiser.Transform.WorldMatrix.TranslationVector;
             var barrelWorldPosition = barrel.Transform.WorldMatrix.TranslationVector;
-            var result = simulation.Raycast(barrelWorldPosition, raycastEndWorldPosition);
+            simulation.Raycast(barrelWorldPosition, raycastEndWorldPosition, out HitResult hitResult);
 
-            if (result.Succeeded)
+            if (hitResult.Succeeded)
             {
-                var length = Vector3.Distance(barrelWorldPosition, result.Point);
+                var length = Vector3.Distance(barrelWorldPosition, hitResult.Point);
                 laser.Transform.Scale = new Vector3(0.01f, length, 1);
 
                 // Update the position of the hit point visualiser
-                hitPointVisualiser.Transform.WorldMatrix.TranslationVector = result.Point;
+                hitPointVisualiser.Transform.WorldMatrix.TranslationVector = hitResult.Point;
                 hitPointVisualiser.Transform.UpdateWorldMatrix();
 
-                DebugText.Print("Hit collider", new Int2(500, 200));
-                DebugText.Print("Raycast hit point: " + result.Point.ToString(), new Int2(500, 220));
-                DebugText.Print("Raycast hit entity : " + result.Collider.Entity.Name, new Int2(500, 240));
+                DebugText.Print("Hit a collider", new Int2(500, 200));
+                DebugText.Print("Raycast hit point: " + hitResult.Point.ToString(), new Int2(500, 220));
+                DebugText.Print("Raycast hit entity : " + hitResult.Collider.Entity.Name, new Int2(500, 240));
             }
             else
             {
