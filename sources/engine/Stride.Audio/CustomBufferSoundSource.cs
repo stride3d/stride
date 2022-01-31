5b648b4bf15a649b0a34d814988988538d59d565
tebjan <tebjan@vvvv.org>
2022-01-31 22:22:42 +0100

    Adds ICustomBufferAudioSource to implement custom audio sources

--- sources/engine/Stride.Audio/CustomBufferSoundSource.cs
+++ sources/engine/Stride.Audio/CustomBufferSoundSource.cs
@@ -9,53 +9,46 @@ using Stride.Media;
 
 namespace Stride.Audio
 {
-    public abstract class StreamedBufferSoundSourceBase : DynamicSoundSource
+    public interface ICustomBufferAudioSource
     {
-        public float SpeedFactor { get; set; } = 1f;
-        public int Channels { get; protected set; }
-        public int SampleRate { get; protected set; }
-        public MediaType MediaType => MediaType.Audio;
-        public TimeSpan MediaDuration { get; protected set; }
-        public bool IsDisposed => isDisposed;
+        bool ComputeAudioData(AudioDataBuffer storageBufferToFill, out bool endOfStream);
 
-        public StreamedBufferSoundSourceBase(SoundInstance soundInstance, int numberOfBuffers, int maxBufferSizeBytes)
-            : base(soundInstance, numberOfBuffers, maxBufferSizeBytes)
-        { }
-
-        /// <summary>
-        /// The media scheduler will check this field to determine whether he can stop waiting for the extractors getting ready
-        /// </summary>
-        protected volatile bool seekRequestCompleted = true;
-        public bool SeekRequestCompleted()
-        {
-            return seekRequestCompleted;
-        }
-
-        protected volatile bool isEof;
-        public bool ReachedEndOfMedia()
-        {
-            return isEof;
-        }
-
-        public abstract void Seek(TimeSpan mediaTime);
+        int Blocks { get; }
+        int BlockSize { get; }
+        int NativeBlockSize { get; }
+        int Channels { get; }
+        int SampleRate { get; }
     }
 
-    //The audio buffer is extracted by an external, unknown API (MediaCodec, FFMPEG, ...), and then feed into this class
-    public partial class StreamedBufferSoundSource : StreamedBufferSoundSourceBase, IMediaExtractor
+    public class AudioDataBuffer
+    {
+        public readonly int MaxBufferSizeBytes;
+
+        public byte[] Data;
+
+        public int CountDataBytes = 0;
+        public TimeSpan PresentationTime;
+
+        public AudioDataBuffer(int maxBufferSizeBytes)
+        {
+            MaxBufferSizeBytes = maxBufferSizeBytes;
+            Data = new byte[MaxBufferSizeBytes];
+        }
+    }
+
+    //The audio buffer is created by a callback
+    public class CustomBufferSoundSource : StreamedBufferSoundSourceBase
     {
         /// <summary>
         /// Specifies how much data we wait to have extracted before we send the storage buffer to the audio buffer
         /// </summary>
-        private const int MinBufferSizeBytesBeforeFlushingStorageBuffer = 28000;
-        private const int MaxBufferSizeBytes = 64000;
-        private const int NumberOfBuffers = 4;
+        private int minBufferSizeBytesBeforeFlushingStorageBuffer = 28000;
+        private int numberOfBuffers = 1;
 
         private readonly object objLock = new object();
 
-        private readonly MediaSynchronizer mediaSynchronizer;
-        private readonly string mediaDataUrl;
-        private readonly long startPosition;
-        private readonly long length;
+        ICustomBufferAudioSource audioSource;
+
         private TimeSpan mediaCurrentTimeMax;
         private TimeSpan mediaCurrentTime;
         private TimeSpan commandSeekTime;
@@ -68,7 +61,7 @@ namespace Stride.Audio
         /// <summary>
         /// Temporary buffers for accumulating the data we're extracting before sending them to the AudioLayer
         /// </summary>
-        private AudioDataStorageBuffer storageBuffer = new AudioDataStorageBuffer();
+        private AudioDataBuffer storageBuffer;
 
         private bool beginningOfStream;
 
@@ -77,24 +70,17 @@ namespace Stride.Audio
 
         private DateTime lastLoopTime = DateTime.Now;
 
-        private class AudioDataStorageBuffer
+        public override int MaxNumberOfBuffers => audioSource.Blocks;
+
+        public CustomBufferSoundSource(SoundInstanceStreamedBuffer instance, ICustomBufferAudioSource customBufferAudioSource)
+            : base(instance, customBufferAudioSource.Blocks, customBufferAudioSource.NativeBlockSize)
         {
-            public byte[] Data = new byte[MaxBufferSizeBytes];
-
-            public int CountDataBytes = 0;
-            public TimeSpan PresentationTime;
-        }
-
-        public override int MaxNumberOfBuffers => NumberOfBuffers;
-
-        public StreamedBufferSoundSource(SoundInstanceStreamedBuffer instance, MediaSynchronizer synchronizer, string mediaDataUrl, long startPosition, long length)
-            : base(instance, NumberOfBuffers, MaxBufferSizeBytes)
-        {
-            mediaSynchronizer = synchronizer;
-            this.mediaDataUrl = mediaDataUrl;
-            this.startPosition = startPosition;
-            this.length = length;
-
+            audioSource = customBufferAudioSource;
+            numberOfBuffers = audioSource.Blocks;
+            minBufferSizeBytesBeforeFlushingStorageBuffer = audioSource.BlockSize;
+            storageBuffer = new AudioDataBuffer(audioSource.BlockSize);
+            Channels = audioSource.Channels;
+            SampleRate = audioSource.SampleRate;
             NewSources.Add(this);
         }
 
@@ -130,27 +116,6 @@ namespace Stride.Audio
             }
         }
 
-        /// <inheritdoc/>
-        public override PlayRange PlayRange
-        {
-            get
-            {
-                lock (objLock)
-                {
-                    return playRange;
-                }
-            }
-            set
-            {
-                lock (objLock)
-                {
-                    playRange = value;
-                }
-
-                base.PlayRange = value;
-            }
-        }
-
         public override void Seek(TimeSpan mediaTime)
         {
             seekRequestCompleted = false;
@@ -160,14 +125,10 @@ namespace Stride.Audio
 
         protected override void InitializeInternal()
         {
-            InitializeMediaExtractor(mediaDataUrl, startPosition, length);
-
             byteRatePerSecond = SampleRate * (Channels * 2); //2 = bit depth (Pcm16bit)
 
             base.InitializeInternal();
         }
-
-        partial void InitializeMediaExtractor(string mediaDataUrl, long startPosition, long length);
 
         protected override void PrepareInternal()
         {
@@ -208,18 +169,15 @@ namespace Stride.Audio
 
             //Seek
             AudioLayer.SourceFlushBuffers(soundInstance.Source);
-            SeekInternalImpl(commandSeekTime);
         }
-
-        partial void SeekInternalImpl(TimeSpan seekTimeUs);
 
         protected override void ExtractAndFillData()
         {
             //Try to extract some new audio data
-            if (ExtractSomeAudioData(out var endOfFile))
+            if (audioSource.ComputeAudioData(storageBuffer, out var endOfStream))
             {
                 //Can we flush the storage buffer?
-                if (storageBuffer.CountDataBytes >= MinBufferSizeBytesBeforeFlushingStorageBuffer)
+                if (storageBuffer.CountDataBytes >= minBufferSizeBytesBeforeFlushingStorageBuffer)
                 {
                     var bufferType = AudioLayer.BufferType.None;
 
@@ -233,22 +191,17 @@ namespace Stride.Audio
                     SendExtractedAudioDataToAudioBuffer(bufferType);
                     storageBuffer.CountDataBytes = 0;
 
-                    if (mediaSynchronizer.IsWaitingForSynchronization())
-                        seekRequestCompleted = true;
                 }
             }
 
-            isEof = endOfFile;  //setting this bool to true will let the media scheduler know when the audio media is done
+            isEof = endOfStream;  //setting this bool to true will let the media scheduler know when the audio media is done
         }
 
         protected override void DisposeInternal()
         {
             base.DisposeInternal();
 
-            ReleaseMediaInternal();
         }
-
-        partial void ReleaseMediaInternal();
 
         private unsafe void SendExtractedAudioDataToAudioBuffer(AudioLayer.BufferType bufferType)
         {
@@ -268,7 +221,7 @@ namespace Stride.Audio
                 var bufferDuration = TimeSpan.FromSeconds(storageBuffer.CountDataBytes / byteRatePerSecond);
 
                 //compute an estimate of the time left before this new buffer can be played
-                var playTimeLeft = TimeSpan.FromSeconds((NumberOfBuffers - freeBuffers.Count) * countAverageBytesPerBuffer / byteRatePerSecond);
+                var playTimeLeft = TimeSpan.FromSeconds((numberOfBuffers - freeBuffers.Count) * countAverageBytesPerBuffer / byteRatePerSecond);
 
                 //Normally: if the buffer are continuous: bufferStartingTimeUs should be very close from the previous MediaPresentationTimeUsMax value
                 //This could help us debug in the case of the audio timeFrame is incorrect
