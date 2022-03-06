1b46e82129c582da723b2367541e700d40cb462e
Eideren <contact@eideren.com>
2022-03-06 16:09:00 +0100

    [Math] Optimize Matrix mult and invert (#1301)

--- sources/engine/Stride.Engine/Engine/Processors/TransformProcessor.cs
+++ sources/engine/Stride.Engine/Engine/Processors/TransformProcessor.cs
@@ -95,7 +95,7 @@ namespace Stride.Engine.Processors
 
         internal void UpdateTransformations(FastCollection<TransformComponent> transformationComponents)
         {
-            Dispatcher.ForEach(transformationComponents, UpdateTransformationAndChildren);
+            Dispatcher.ForEach(transformationComponents, UpdateTransformationsRecursive);
 
             // Re-update model node links to avoid one frame delay compared reference model (ideally entity should be sorted to avoid this in future).
             if (ModelNodeLinkProcessor != null)
@@ -105,36 +105,18 @@ namespace Stride.Engine.Processors
                 {
                     modelNodeLinkComponents.Add(modelNodeLink.Entity.Transform);
                 }
-                Dispatcher.ForEach(modelNodeLinkComponents, UpdateTransformationAndChildren);
+                Dispatcher.ForEach(modelNodeLinkComponents, UpdateTransformationsRecursive);
             }
         }
 
-        private static void UpdateTransformationAndChildren(TransformComponent transformation)
+        private static void UpdateTransformationsRecursive(TransformComponent transform)
         {
-            UpdateTransformation(transformation);
-
-            // Recurse
-            if (transformation.Children.Count > 0)
-                UpdateTransformationsRecursive(transformation.Children);
-        }
-
-        private static void UpdateTransformationsRecursive(FastCollection<TransformComponent> transformationComponents)
-        {
-            foreach (var transformation in transformationComponents)
-            {
-                UpdateTransformation(transformation);
-
-                // Recurse
-                if (transformation.Children.Count > 0)
-                    UpdateTransformationsRecursive(transformation.Children);
-            }
-        }
-
-        private static void UpdateTransformation(TransformComponent transform)
-        {
-            // Update transform
             transform.UpdateLocalMatrix();
             transform.UpdateWorldMatrixInternal(false);
+            foreach (var child in transform.Children)
+            {
+                UpdateTransformationsRecursive(child);
+            }
         }
 
         /// <summary>
