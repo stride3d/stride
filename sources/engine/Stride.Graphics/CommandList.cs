933d47f4a527ce5d7c88e26d47d1c7716cda8d8e
James Rinker <james_rinker@hotmail.com>
2022-03-28 11:33:39 -0700

    From PR: Restore CommandList.scissorsDirty and suppress the warning since it's used conditionally.

--- sources/engine/Stride.Graphics/CommandList.cs
+++ sources/engine/Stride.Graphics/CommandList.cs
@@ -21,9 +21,14 @@ namespace Stride.Graphics
         private int boundScissorCount;
         private readonly Rectangle[] scissors = new Rectangle[MaxViewportAndScissorRectangleCount];
 
+#pragma warning disable 414 // The field 'CommandList.scissorsDirty' is assigned but its value is never used
+        // This field is used in CommandList.Direct3D12.cs and CommandList.Vulkan.cs
+        private bool scissorsDirty = false;
+#pragma warning restore 414
+
         private Texture depthStencilBuffer;
 
-        private readonly Texture[] renderTargets = new Texture[MaxRenderTargetCount];
+        private Texture[] renderTargets = new Texture[MaxRenderTargetCount];
         private int renderTargetCount;
 
         /// <summary>
@@ -73,6 +78,7 @@ namespace Stride.Graphics
                 viewports[i] = new Viewport();
 
             // Setup empty scissors
+            scissorsDirty = true;
             for (int i = 0; i < scissors.Length; i++)
                 scissors[i] = new Rectangle();
 
@@ -142,6 +148,7 @@ namespace Stride.Graphics
         /// <param name="rectangle">The scissor rectangle.</param>
         public void SetScissorRectangle(Rectangle rectangle)
         {
+            scissorsDirty = true;
             boundScissorCount = 1;
             scissors[0] = rectangle;
             SetScissorRectangleImpl(ref rectangle);
@@ -163,6 +170,7 @@ namespace Stride.Graphics
         /// <param name="scissorRectangles">The set of scissor rectangles to bind.</param>
         public void SetScissorRectangles(int scissorCount, Rectangle[] scissorRectangles)
         {
+            scissorsDirty = true;
             boundScissorCount = scissorCount;
             for (int i = 0; i < scissorCount; ++i)
             {
