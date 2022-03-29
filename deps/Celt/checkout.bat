67bc5e8003956a9b9b3c99d4f1e2ce1dbfdfdb1d
IceReaper <andremohren@gmail.com>
2022-03-29 18:33:23 +0200

    Fixed Celt repo url.

--- deps/Celt/checkout.bat
+++ deps/Celt/checkout.bat
@@ -3,5 +3,5 @@ IF NOT ERRORLEVEL 0 (
   ECHO "Could not find git.exe"
   EXIT /B %ERRORLEVEL%
 ) 
-%GIT_CMD% clone https://git.xiph.org/opus.git -b v1.1.3 ..\..\externals\Celt
+%GIT_CMD% clone https://gitlab.xiph.org/xiph/opus.git -b v1.1.3 ..\..\externals\Celt
 if NOT ERRORLEVEL 0 pause
