1d94ac09e3905f5e42225de7f5f8fdee93f750f4
Sebastian Gregor <gregor@vvvv.org>
2022-03-31 17:18:28 +0200

    Added abstractions IIntersectableWithRay (implemented by BoundingBox, BoundingSphere and Plane) and IIntersectableWithPlane (implemented by BoundingBox and BoundingSphere) (#1395)

---
+++ sources/core/Stride.Core.Mathematics/IIntersectableWithPlane.cs
@@ -0,0 +1,15 @@
+namespace Stride.Core.Mathematics
+{
+    /// <summary>
+    /// Allows to determine intersections with a <see cref="Stride.Core.Mathematics.Plane"/>.
+    /// </summary>
+    public interface IIntersectableWithPlane
+    {
+        /// <summary>
+        /// Determines if there is an intersection between the current object and a <see cref="Stride.Core.Mathematics.Plane"/>.
+        /// </summary>
+        /// <param name="plane">The plane to test.</param>
+        /// <returns>Whether the two objects intersected.</returns>
+        public PlaneIntersectionType Intersects(ref Plane plane);
+    }
+}
