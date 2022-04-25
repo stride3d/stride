56c4982f94238650e6afeaf100bc21af2f0fb508
Youness KAFIA <kafia.youness@hotmail.fr>
2022-04-25 12:56:30 +0200

    Correction Texture2dms

--- src/SDSLParser/Program.cs
+++ src/SDSLParser/Program.cs
@@ -12,7 +12,7 @@ var tokens = StrideGrammar.HlslGrammar();
 // var tmp = new Grammar("something", Terminals.Set("a").Not());
 var s = new Stopwatch();
 s.Start();
-var match = tokens.Match("float4x3");
+var match = tokens.Match("Texture2DMS");
 // var res = SDSLPParser.Parse("My_Var");
 s.Stop();
 
