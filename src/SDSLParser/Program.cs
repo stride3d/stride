3e3b92446fa040dcd1792e5e1ae49116f81e5379
Youness KAFIA <kafia.youness@hotmail.fr>
2022-04-28 15:31:40 +0200

    Expression and directive corrections

--- src/SDSLParser/Program.cs
+++ src/SDSLParser/Program.cs
@@ -4,17 +4,18 @@ using Stride.Shader.Parser;
 using System.Diagnostics;
 
 
-var shaderf = File.ReadAllText("./SDSL/Directive.sdsl");
+var shaderf = File.ReadAllText("./SDSL/Expressions.sdsl");
 
 // var parser = new SDSLGrammar();
 var parser = StrideGrammar.New("sum");
-var tokens = StrideGrammar.HlslGrammar("directives");
+var tokens = StrideGrammar.HlslGrammar("expression");
 var s = new Stopwatch();
+var match = tokens.Match("(8)");
 s.Start();
-var match = tokens.Match(shaderf);
+match = tokens.Match(shaderf);
 s.Stop();
 
-Console.WriteLine(match.ErrorMessage);
+Console.WriteLine(match.ErrorMessage[..(Math.Min(300,match.ErrorMessage.Length))]);
 match.Matches.ForEach(x => Console.WriteLine(x.Name + " : " + x.Value));
 Console.WriteLine($"parsing time : {s.Elapsed}");
 Console.Write("");
