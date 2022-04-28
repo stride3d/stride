e31e3855ef4e30c385d61c694cef3ea46c206df6
Youness KAFIA <kafia.youness@hotmail.fr>
2022-04-29 01:33:41 +0200

    Solved weird issue for expressions

--- src/SDSLParser/Program.cs
+++ src/SDSLParser/Program.cs
@@ -7,15 +7,15 @@ using System.Diagnostics;
 var shaderf = File.ReadAllText("./SDSL/Expressions.sdsl");
 
 // var parser = new SDSLGrammar();
-var parser = StrideGrammar.New("sum");
+var parser = StrideGrammar.New("expr");
 var tokens = StrideGrammar.HlslGrammar("expression");
 var s = new Stopwatch();
 var match = tokens.Match("(8)");
 s.Start();
-match = tokens.Match(shaderf);
+match = tokens.Match("3 \n*3*3");
 s.Stop();
 
-Console.WriteLine(match.ErrorMessage[..(Math.Min(300,match.ErrorMessage.Length))]);
+Console.WriteLine(match.ErrorMessage[..Math.Min(1000,match.ErrorMessage.Length)]);
 match.Matches.ForEach(x => Console.WriteLine(x.Name + " : " + x.Value));
 Console.WriteLine($"parsing time : {s.Elapsed}");
 Console.Write("");
