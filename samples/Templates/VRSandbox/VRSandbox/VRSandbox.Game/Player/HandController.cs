0322e32399695e2eb6dc27dcd6d51e40d914f15e
James Rinker <james_rinker@hotmail.com>
2022-04-29 17:04:42 -0700

    Convert existing examples to use a common base package.
    Also correct a couple of build errors.

--- samples/Templates/VRSandbox/VRSandbox/VRSandbox.Game/Player/HandController.cs
+++ samples/Templates/VRSandbox/VRSandbox/VRSandbox.Game/Player/HandController.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
 using System.Linq;
@@ -114,8 +114,8 @@ namespace VRSandbox.Player
             while (enumerator.MoveNext())
             {
                 var collision = enumerator.Current;
-                var entityA = collision?.ColliderA?.Entity;
-                var entityB = collision?.ColliderB?.Entity;
+                var entityA = collision.ColliderA?.Entity;
+                var entityB = collision.ColliderB?.Entity;
 
                 var otherEntity = (entityA == Entity) ? entityB : entityA;
 
