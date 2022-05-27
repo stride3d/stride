368099c70ad67464396f22a5bca9cac7c3ff92d9
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-05-28 00:25:28 +0200

    Update on shader mixin

--- src/Stride.Shader.Parsing/ShaderMixer.cs
+++ src/Stride.Shader.Parsing/ShaderMixer.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spv.Generator;
 
 namespace Stride.Shader.Parsing;
 
-public class SDSLMixer
+public class ShaderMixer : Module
 {
     //Dictionary<string, int> 
     List<ShaderMixin> Mixins { get; set; } = new();
-    
+
+    public ShaderMixer(uint version) : base(version)
+    {
+    }
+
 }
--- src/Stride.Shader.Parsing/ShaderMixin.cs
+++ src/Stride.Shader.Parsing/ShaderMixin.cs
@@ -10,7 +10,61 @@ namespace Stride.Shader.Parsing;
 public class ShaderMixin
 {
     public string Code { get; set; }
-    public string MixinName { get; set; }
-    public ShaderToken AST { get; set; }
+    public string MixinName { get => AST != null ? AST.Name : string.Empty; }
+    public ShaderProgram? AST { get; set; }
+    Eto.Parse.Grammar EntryPointMatcher = new Eto.Parse.Grammar(
+        Eto.Parse.Terminals.Literal("PSMain")
+        | "VSMain"
+        | "GSMain"
+        | "HSMain"
+        | "DSMain"
+        | "CSMain");
+
+    SDSLParser Parser { get; set; } = new();
+
+    public ShaderMixin(string code)
+    {
+        Code = code;
+    }
+
+    public void Parse()
+    {
+        AST = (ShaderProgram)Parser.Parse(Code);
+    }
+
+    public IEnumerable<ShaderValueDeclaration> GetStreamValues()
+    {
+        if (AST is not null)
+            return
+                AST.Body
+                .Where(x => x is ShaderValueDeclaration)
+                .Cast<ShaderValueDeclaration>()
+                .Where(x => x.IsStream);
+        else
+            throw new Exception("AST is null");
+    }
+    public IEnumerable<ShaderMethod> GetEntryPoints()
+    {
+        if (AST is not null)
+            return
+                AST.Body
+                .Where(x => x is ShaderMethod)
+                .Cast<ShaderMethod>()
+                .Where(x => EntryPointMatcher.Match(x.Name).Success);
+        else
+            throw new Exception("AST is null");
+    }
+    public IEnumerable<ShaderMethod> GetMethods()
+    {
+        if (AST is not null)
+            return
+                AST.Body
+                .Where(x => x is ShaderMethod)
+                .Cast<ShaderMethod>()
+                .Where(x => !EntryPointMatcher.Match(x.Name).Success);
+        else
+            throw new Exception("AST is null");
+    }
+
 
 }
