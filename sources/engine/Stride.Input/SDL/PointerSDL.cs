a0247e4bc114b29a110fff44d3c2ad49504a8f28
Elias Holzer <elias@vvvv.org>
2022-07-20 13:53:15 +0200

    Fixes key not found exception when working with multiple devices
    See initial bug report here: https://discourse.vvvv.org/t/multiple-multitouch-screens-crashes-on-fast-touches/20683/5

--- sources/engine/Stride.Input/SDL/PointerSDL.cs
+++ sources/engine/Stride.Input/SDL/PointerSDL.cs
@@ -18,7 +18,7 @@ namespace Stride.Input
     internal class PointerSDL : PointerDeviceBase, IDisposable
     {
         private readonly Window uiControl;
-        private readonly Dictionary<long, int> touchFingerIndexMap = new Dictionary<long, int>();
+        private readonly Dictionary<(long touchId, long finderId), int> touchFingerIndexMap = new Dictionary<(long touchId, long finderId), int>();
         private int touchCounter;
 
         public PointerSDL(InputSourceSDL source, Window uiControl)
@@ -59,25 +59,24 @@ namespace Stride.Input
             SetSurfaceSize(new Vector2(uiControl.ClientSize.Width, uiControl.ClientSize.Height));
         }
 
-        // TODO Code from PointeriOS with slight modifications, consider creating and referencing a utility class
-        private int GetFingerId(long touchId, PointerEventType type)
+        private int GetFingerId(long touchId, long fingerId, PointerEventType type)
         {
             // Assign finger index (starting at 0) to touch ID
             int touchFingerIndex = 0;
+            var key = (touchId, fingerId);
             if (type == PointerEventType.Pressed)
             {
                 touchFingerIndex = touchCounter++;
-                touchFingerIndexMap.Add(touchId, touchFingerIndex);
+                touchFingerIndexMap[key] = touchFingerIndex;
             }
             else
             {
-                touchFingerIndex = touchFingerIndexMap[touchId];
+                touchFingerIndexMap.TryGetValue(key, out touchFingerIndex);
             }
 
             // Remove index
-            if (type == PointerEventType.Released)
+            if (type == PointerEventType.Released && touchFingerIndexMap.Remove(key))
             {
-                touchFingerIndexMap.Remove(touchId);
                 touchCounter = 0; // Reset touch counter
 
                 // Recalculate next finger index
@@ -94,7 +93,7 @@ namespace Stride.Input
         private void HandleFingerEvent(SDL.SDL_TouchFingerEvent e, PointerEventType type)
         {
             var newPosition = new Vector2(e.x, e.y);
-            var id = GetFingerId(e.fingerId, type);
+            var id = GetFingerId(e.touchId, e.fingerId, type);
             PointerState.PointerInputEvents.Add(new PointerDeviceState.InputEvent { Type = type, Position = newPosition, Id = id });
         }
 
