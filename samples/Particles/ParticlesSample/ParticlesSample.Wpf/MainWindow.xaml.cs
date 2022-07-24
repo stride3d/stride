d474c9e6219e6dbae8c41f634831c0c21b6502cc
Van Stonecypher <van@unnamed.net>
2022-07-24 13:30:00 -0500

    Initial stab at using GameEngineHost.

--- samples/Particles/ParticlesSample/ParticlesSample.Wpf/MainWindow.xaml.cs
+++ samples/Particles/ParticlesSample/ParticlesSample.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +9,14 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Stride.Core.Presentation.Controls;
+using Stride.Engine;
+using Stride.Games;
 
 namespace ParticlesSample
 {
@@ -23,6 +28,40 @@ namespace ParticlesSample
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            Unloaded += MainWindow_Unloaded;
         }
+
+        private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_game is not null)
+            {
+                _game.Dispose();
+                _game = null;
+            }
+
+            if (_sdlWindow is not null)
+            {
+                _sdlWindow.Dispose();
+                _sdlWindow = null;
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var host = Window.GetWindow(this);
+            WindowInteropHelper wih = new(host);
+            IntPtr hwnd = wih.Handle;
+            Stride.Graphics.SDL.Window sdlWindow = new("Embedded Stride Window", hwnd);
+            contentCtrl.Content = new GameEngineHost(sdlWindow.Handle);
+
+            GameContextSDL context = new(sdlWindow);
+            _game = new Game();
+            _game.Run(context);
+        }
+
+        private Game? _game;
+        Stride.Graphics.SDL.Window? _sdlWindow;
     }
 }
