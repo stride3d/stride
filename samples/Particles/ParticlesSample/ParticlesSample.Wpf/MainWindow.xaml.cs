010d16dba3db143953f224ea9c5177c1f801aaa8
Van Stonecypher <van@unnamed.net>
2022-07-24 13:31:24 -0500

    Made sure _sdlWindow was actually set.

--- samples/Particles/ParticlesSample/ParticlesSample.Wpf/MainWindow.xaml.cs
+++ samples/Particles/ParticlesSample/ParticlesSample.Wpf/MainWindow.xaml.cs
@@ -33,6 +33,19 @@ namespace ParticlesSample
             Unloaded += MainWindow_Unloaded;
         }
 
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var host = Window.GetWindow(this);
+            WindowInteropHelper wih = new(host);
+            IntPtr hwnd = wih.Handle;
+            _sdlWindow = new("Embedded Stride Window", hwnd);
+            contentCtrl.Content = new GameEngineHost(_sdlWindow.Handle);
+
+            GameContextSDL context = new(_sdlWindow);
+            _game = new Game();
+            _game.Run(context);
+        }
+
         private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
         {
             if (_game is not null)
@@ -46,19 +59,6 @@ namespace ParticlesSample
                 _sdlWindow.Dispose();
                 _sdlWindow = null;
             }
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            var host = Window.GetWindow(this);
-            WindowInteropHelper wih = new(host);
-            IntPtr hwnd = wih.Handle;
-            Stride.Graphics.SDL.Window sdlWindow = new("Embedded Stride Window", hwnd);
-            contentCtrl.Content = new GameEngineHost(sdlWindow.Handle);
-
-            GameContextSDL context = new(sdlWindow);
-            _game = new Game();
-            _game.Run(context);
         }
 
         private Game? _game;
