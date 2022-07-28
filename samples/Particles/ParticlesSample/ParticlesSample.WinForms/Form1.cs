2bfa8d0bca69d5d0c04031b5998bfec4d38f735c
Van Stonecypher <van@unnamed.net>
2022-07-28 15:16:49 -0500

    Removed child window creation as it is not needed. Removed NativeMethods.

--- samples/Particles/ParticlesSample/ParticlesSample.WinForms/Form1.cs
+++ samples/Particles/ParticlesSample/ParticlesSample.WinForms/Form1.cs
@@ -10,30 +10,8 @@ namespace ParticlesSample
         {
             InitializeComponent();
 
-            // Create a child window in which to host the game
-            var className = GetType().Name;
-            var wndClass = new ParticlesSample.NativeMethods.WndClassEx();
-            wndClass.cbSize = (uint)Marshal.SizeOf(wndClass);
-            wndClass.hInstance = NativeMethods.GetModuleHandle(null);
-            wndClass.lpfnWndProc = NativeMethods.DefaultWindowProc;
-            wndClass.lpszClassName = className;
-            // If this is not null, the cursor is drawn whenever the mouse is moved.
-            wndClass.hCursor = IntPtr.Zero;
-            NativeMethods.RegisterClassEx(ref wndClass);
-
-            var childHandle = NativeMethods.CreateWindowEx(
-                0,
-                className,
-                "",
-                NativeMethods.WS_CHILD | NativeMethods.WS_VISIBLE,
-                0,
-                0,
-                (int)Width, (int)Height,
-                panel1.Handle,
-                IntPtr.Zero, IntPtr.Zero, 0);
-
             // Create SDL window using child
-            var _sdlWindow = new Stride.Graphics.SDL.Window("Embedded Stride Window", childHandle);
+            var _sdlWindow = new Stride.Graphics.SDL.Window("Embedded Stride Window", panel1.Handle);
             var context = new GameContextSDL(_sdlWindow);
 
             // Start the game
