1f969b648826849024b06973d6c65e055dddbd37
Aggror <aggror.jorn@gmail.com>
2022-09-14 17:28:31 +0200

    almost fixed, except loop oddity

--- sources/engine/Stride.Engine/Engine/Splines/Models/Mesh/SplineMeshBox.cs
+++ sources/engine/Stride.Engine/Engine/Splines/Models/Mesh/SplineMeshBox.cs
@@ -16,6 +16,16 @@ namespace Stride.Engine.Splines.Models
         /// </summary>
         public bool CloseEnd;
 
+        private VertexPositionNormalTexture[] vertices;
+        private int[] indices;
+        private readonly Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.UnitY, //Down
+            -Vector3.UnitX, //Right
+            Vector3.UnitY, //Up
+            Vector3.UnitX // Left
+        };
+
         protected override GeometricMeshData<VertexPositionNormalTexture> CreatePrimitiveMeshData()
         {
             var splinePointCount = bezierPoints.Length;
@@ -23,16 +33,17 @@ namespace Stride.Engine.Splines.Models
             var indicesCount = (splinePointCount - 1) * 24;
             if (Loop)
             {
-                vertexCount += 4;
-                indicesCount += 24;
+                //vertexCount += 4;
+                //indicesCount += 24;
             }
             else if (CloseEnd)
             {
+                vertexCount += 8;
                 indicesCount += 12;
             }
 
-            var vertices = new VertexPositionNormalTexture[vertexCount];
-            var indices = new int[indicesCount];
+            vertices = new VertexPositionNormalTexture[vertexCount];
+            indices = new int[indicesCount];
 
             var halfWidth = Scale.X / 2;
             var halfHeigth = new Vector3(0, Scale.Y / 2, 0);
@@ -48,132 +59,137 @@ namespace Stride.Engine.Splines.Models
                 forward.Normalize();
                 var right = Vector3.Cross(forward, Vector3.UnitY) * halfWidth;
                 var left = -right;
-
-                // Todo: deal with up normal and rotation
-                var upNormal = Vector3.UnitY;
-                var leftNormal = Vector3.UnitX;
                 float textureY;
 
                 // Create vertices
-                var a = left - halfHeigth;
-                var b = right - halfHeigth;
-                var c = right + halfHeigth;
-                var d = left + halfHeigth;
-
-                for (int k = 0; k < 4; k++)
+                var sides = new Vector3[4]
                 {
+                    left - halfHeigth, //Bottom left
+                    right - halfHeigth, //Bottom right
+                    right + halfHeigth, //Top right
+                    left + halfHeigth // Top Lef
+                };
 
-                    if (i == 0)
+                if (i == 0) //First vertexes
+                {             
+                    // Loop over each side in following order: Bottom, Right, Top, Left
+                    for (int side = 0; side < sides.Length; side++)
                     {
-                        vertices[verticesIndex] = new VertexPositionNormalTexture(startPoint.Position + a, -upNormal, new Vector2(0, 0));
-                        vertices[verticesIndex + 1] = new VertexPositionNormalTexture(startPoint.Position + b, -leftNormal, new Vector2(1, 0));
-                        vertices[verticesIndex + 2] = new VertexPositionNormalTexture(startPoint.Position + c, upNormal, new Vector2(0, 0));
-                        vertices[verticesIndex + 3] = new VertexPositionNormalTexture(startPoint.Position + d, leftNormal, new Vector2(1, 0));
-                        verticesIndex += 4;
-                    }
-
-                    if (i == splinePointCount - 2 && Loop) //If Loop is enabled, then the target node is the first node in the entire spline
-                    {
-                        splineDistance += Vector3.Distance(startPoint.Position, bezierPoints[0].Position);
-                        textureY = splineDistance / UvScale.Y;
-                        vertices[verticesIndex] = new VertexPositionNormalTexture(vertices[0].Position, -upNormal, new Vector2(0, textureY));
-                        vertices[verticesIndex + 1] = new VertexPositionNormalTexture(vertices[1].Position, -leftNormal, new Vector2(1, textureY));
-                        vertices[verticesIndex + 2] = new VertexPositionNormalTexture(vertices[2].Position, upNormal, new Vector2(1, textureY));
-                        vertices[verticesIndex + 3] = new VertexPositionNormalTexture(vertices[3].Position, leftNormal, new Vector2(1, textureY));
-                    }
-                    else
-                    {
-                        splineDistance += targetPoint.DistanceToPreviousPoint;
-                        textureY = splineDistance / UvScale.Y;
-                        vertices[verticesIndex] = new VertexPositionNormalTexture(targetPoint.Position + a, -upNormal, new Vector2(0, textureY));
-                        vertices[verticesIndex + 1] = new VertexPositionNormalTexture(targetPoint.Position + b, -leftNormal, new Vector2(1, textureY));
-                        vertices[verticesIndex + 2] = new VertexPositionNormalTexture(targetPoint.Position + c, upNormal, new Vector2(1, textureY));
-                        vertices[verticesIndex + 3] = new VertexPositionNormalTexture(targetPoint.Position + d, leftNormal, new Vector2(1, textureY));
-                        verticesIndex += 4;
+                        CreateVertex(verticesIndex + 0, startPoint.Position + sides[side], normals[side], new Vector2(0, 0));
+                        CreateVertex(verticesIndex + 1, startPoint.Position + sides[(side + 1) % 4], normals[side], new Vector2(1, 0));
+                        verticesIndex += 2;
                     }
                 }
+
+                if (i == splinePointCount - 2 && Loop) //If Loop is enabled, then the target node is the first node in the entire spline
+                {
+                    splineDistance += Vector3.Distance(startPoint.Position, bezierPoints[0].Position);
+                    textureY = splineDistance / UvScale.Y;
+
+                    for (int side = 0; side < sides.Length; side++)
+                    {
+                        CreateVertex(verticesIndex + 0, vertices[side].Position, normals[side], new Vector2(0, textureY));
+                        CreateVertex(verticesIndex + 1, vertices[(side + 1) % 4].Position, normals[side], new Vector2(1, textureY));
+                        verticesIndex += 2;
+                    }
+                }
+                else
+                {
+                    splineDistance += targetPoint.DistanceToPreviousPoint;
+                    textureY = splineDistance / UvScale.Y;
+                    for (int side = 0; side < sides.Length; side++)
+                    {
+                        CreateVertex(verticesIndex + 0, targetPoint.Position + sides[side], normals[side], new Vector2(0, textureY));
+                        CreateVertex(verticesIndex + 1, targetPoint.Position + sides[(side + 1) % 4], normals[side], new Vector2(1, textureY));
+                        verticesIndex += 2;
+                    }
+                }
+
 
                 // Create indices
                 var indiceIndex = i * 24;
 
-                //A
+                //Bottom
                 indices[indiceIndex + 0] = 0 + triangleIndex;
                 indices[indiceIndex + 1] = 1 + triangleIndex;
-                indices[indiceIndex + 2] = 4 + triangleIndex;
+                indices[indiceIndex + 2] = 8 + triangleIndex;
 
                 indices[indiceIndex + 3] = 1 + triangleIndex;
-                indices[indiceIndex + 4] = 5 + triangleIndex;
-                indices[indiceIndex + 5] = 4 + triangleIndex;
+                indices[indiceIndex + 4] = 9 + triangleIndex;
+                indices[indiceIndex + 5] = 8 + triangleIndex;
 
-                //b
-                indices[indiceIndex + 6] = 1 + triangleIndex;
-                indices[indiceIndex + 7] = 2 + triangleIndex;
-                indices[indiceIndex + 8] = 5 + triangleIndex;
+                //Right
+                indices[indiceIndex + 6] = 2 + triangleIndex;
+                indices[indiceIndex + 7] = 3 + triangleIndex;
+                indices[indiceIndex + 8] = 10 + triangleIndex;
 
-                indices[indiceIndex + 9] = 2 + triangleIndex;
-                indices[indiceIndex + 10] = 6 + triangleIndex;
-                indices[indiceIndex + 11] = 5 + triangleIndex;
+                indices[indiceIndex + 9] = 3 + triangleIndex;
+                indices[indiceIndex + 10] = 11 + triangleIndex;
+                indices[indiceIndex + 11] = 10 + triangleIndex;
 
-                //c
-                indices[indiceIndex + 12] = 3 + triangleIndex;
-                indices[indiceIndex + 13] = 6 + triangleIndex;
-                indices[indiceIndex + 14] = 2 + triangleIndex;
+                //Top
+                indices[indiceIndex + 12] = 4 + triangleIndex;
+                indices[indiceIndex + 13] = 5 + triangleIndex;
+                indices[indiceIndex + 14] = 12 + triangleIndex;
 
-                indices[indiceIndex + 15] = 3 + triangleIndex;
-                indices[indiceIndex + 16] = 7 + triangleIndex;
-                indices[indiceIndex + 17] = 6 + triangleIndex;
+                indices[indiceIndex + 15] = 5 + triangleIndex;
+                indices[indiceIndex + 16] = 13 + triangleIndex;
+                indices[indiceIndex + 17] = 12 + triangleIndex;
 
-                //d
-                indices[indiceIndex + 18] = 3 + triangleIndex;
-                indices[indiceIndex + 19] = 0 + triangleIndex;
-                indices[indiceIndex + 20] = 7 + triangleIndex;
+                //Left
+                indices[indiceIndex + 18] = 6 + triangleIndex;
+                indices[indiceIndex + 19] = 7 + triangleIndex;
+                indices[indiceIndex + 20] = 15 + triangleIndex;
 
-                indices[indiceIndex + 21] = 0 + triangleIndex;
-                indices[indiceIndex + 22] = 4 + triangleIndex;
-                indices[indiceIndex + 23] = 7 + triangleIndex;
+                indices[indiceIndex + 21] = 6 + triangleIndex;
+                indices[indiceIndex + 22] = 15 + triangleIndex;
+                indices[indiceIndex + 23] = 14 + triangleIndex;
 
-                triangleIndex += 4;
+                triangleIndex += 8;
 
                 // If this was the last loop, we do 1 additional check for Closing of the sides or looping the geometry
                 if (i == splinePointCount - 2)
                 {
-                    if (Loop)
-                    {
+                    //if (Loop)
+                    //{
 
-                    }
-                    else if (CloseEnd)
-                    {
-                        var closeIndicesIndex = indicesCount - 12;
+                    //}
+                    //else if (CloseEnd)
+                    //{
+                    //    var closeIndicesIndex = indicesCount - 12;
 
-                        //Front
-                        indices[closeIndicesIndex + 0] = 0;
-                        indices[closeIndicesIndex + 1] = 3;
-                        indices[closeIndicesIndex + 2] = 1;
+                    //    //Front
+                    //    indices[closeIndicesIndex + 0] = 0;
+                    //    indices[closeIndicesIndex + 1] = 1;
+                    //    indices[closeIndicesIndex + 2] = 6;
 
-                        indices[closeIndicesIndex + 3] = 1;
-                        indices[closeIndicesIndex + 4] = 3;
-                        indices[closeIndicesIndex + 5] = 2;
-                        closeIndicesIndex += 6;
+                    //    indices[closeIndicesIndex + 3] = 1;
+                    //    indices[closeIndicesIndex + 4] = 3;
+                    //    indices[closeIndicesIndex + 5] = 6;
+                    //    closeIndicesIndex += 6;
 
-                        //Back
-                        var closeVerticesIndex = vertexCount - 4;
-                        indices[closeIndicesIndex + 0] = closeVerticesIndex;
-                        indices[closeIndicesIndex + 1] = closeVerticesIndex + 1;
-                        indices[closeIndicesIndex + 2] = closeVerticesIndex + 3;
+                    //    //Back
+                    //    var closeVerticesIndex = vertexCount - 4;
+                    //    indices[closeIndicesIndex + 0] = closeVerticesIndex;
+                    //    indices[closeIndicesIndex + 1] = closeVerticesIndex + 1;
+                    //    indices[closeIndicesIndex + 2] = closeVerticesIndex + 4;
 
-                        indices[closeIndicesIndex + 3] = closeVerticesIndex + 1;
-                        indices[closeIndicesIndex + 4] = closeVerticesIndex + 2;
-                        indices[closeIndicesIndex + 5] = closeVerticesIndex + 3;
-                    }
-
-
+                    //    indices[closeIndicesIndex + 3] = closeVerticesIndex + 1;
+                    //    indices[closeIndicesIndex + 4] = closeVerticesIndex + 2;
+                    //    indices[closeIndicesIndex + 5] = closeVerticesIndex + 3;
+                    //}
                 }
+
             }
 
             // Create the primitive object for further processing by the base class
             return new GeometricMeshData<VertexPositionNormalTexture>(vertices, indices, isLeftHanded: false);
+
         }
 
-
+        private void CreateVertex(int verticesIndex, Vector3 position, Vector3 normal, Vector2 texture)
+        {
+            vertices[verticesIndex] = new VertexPositionNormalTexture(position, normal, texture);
+        }
     }
 }
