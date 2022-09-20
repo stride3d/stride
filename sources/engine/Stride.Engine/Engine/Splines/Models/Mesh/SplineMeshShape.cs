b8b575be17240ee94958157a13ae82a769efbc85
Aggror <aggror.jorn@gmail.com>
2022-09-20 13:51:30 +0200

    feeling crap

--- sources/engine/Stride.Engine/Engine/Splines/Models/Mesh/SplineMeshShape.cs
+++ sources/engine/Stride.Engine/Engine/Splines/Models/Mesh/SplineMeshShape.cs
@@ -33,6 +33,7 @@ namespace Stride.Engine.Splines.Models
 
             var splinePointCount = bezierPoints.Length;
             var shapePointsCount = shapePoints.Length;
+
             var totalVertexCount = 4 * (shapePointsCount - 1) * (splinePointCount - 1);
             var totalIndicesCount = (totalVertexCount / 4) * 6;
 
@@ -44,49 +45,84 @@ namespace Stride.Engine.Splines.Models
 
             var verticesIndex = 0;
             Vector3 posA, posB, posC, posD;
+            float splineDistance = 0.0f;
 
-            for (int i = 0; i < splinePointCount; i++)
+            for (int i = 0; i < splinePointCount - 1; i++)
             {
                 var startPoint = bezierPoints[i];
-             
+                var targetPoint = bezierPoints[i + 1];
+                var splineForward = targetPoint.Position - startPoint.Position;
+
+                splineForward.Normalize();
+                var left = Vector3.Cross(splineForward, Vector3.UnitY);
+                var right = -left;  
+                float textureY;
+
                 for (int j = 0; j < shapePointsCount - 1; j++)
                 {
                     var startShapePoint = shapePoints[j];
                     var targetShapePoint = shapePoints[j + 1];
-                    var offset = targetShapePoint.Position - startShapePoint.Position;
-
-                    ////First vertexes
+                  
+                    var shapeForward = (targetShapePoint.Position - startShapePoint.Position);
+                    shapeForward.Normalize();
+                    var normal = Vector3.Cross(shapeForward, Vector3.UnitY);
+                    //First vertexes
                     if (j == 0)
                     {
+                        var temp = right;
+                        temp *= targetShapePoint.Position.X - startShapePoint.Position.X;
+                        temp.Y += targetShapePoint.Position.Y - startShapePoint.Position.Y;
                         posA = startPoint.Position;
-                        posB = startPoint.Position + offset;
-                        CreateVertex(verticesIndex++, posA, Vector3.UnitY, new Vector2(0, 0));
-                        CreateVertex(verticesIndex++, posB, Vector3.UnitY, new Vector2(0, 0));
+                        posB = startPoint.Position + temp;
+                        posC = targetPoint.Position;
+                        posD = targetPoint.Position + temp;
+
+                        textureY = splineDistance / UvScale.Y;
+                        CreateVertex(verticesIndex++, posA, normal, new Vector2(0, textureY));
+                        CreateVertex(verticesIndex++, posB, normal, new Vector2(1, textureY));
+                        CreateVertex(verticesIndex++, posC, normal, new Vector2(0, textureY));
+                        CreateVertex(verticesIndex++, posD, normal, new Vector2(1, textureY));
                     }
                     else
                     {
-                        posA = vertices[verticesIndex-1].Position;
-                        posB = vertices[verticesIndex-1].Position + offset;
-                        CreateVertex(verticesIndex++, posA, Vector3.UnitY, new Vector2(0, 0));
-                        CreateVertex(verticesIndex++, posB, Vector3.UnitY, new Vector2(0, 0));
+                        var temp = right;
+                        temp.X *= targetShapePoint.Position.X - startShapePoint.Position.X;
+                        temp.Y += targetShapePoint.Position.Y - startShapePoint.Position.Y;
+                        //right *= offset.X;
+                        posA = vertices[verticesIndex - 3].Position;
+                        posB = vertices[verticesIndex - 3].Position + temp;
+                        posC = vertices[verticesIndex - 1].Position;
+                        posD = vertices[verticesIndex - 1].Position + temp;
+                        splineDistance += targetPoint.DistanceToPreviousPoint;
+                        textureY = splineDistance / UvScale.Y;
+                        CreateVertex(verticesIndex++, posA, normal, new Vector2(0, textureY));
+                        CreateVertex(verticesIndex++, posB, normal, new Vector2(1, textureY));
+                        CreateVertex(verticesIndex++, posC, normal, new Vector2(0, textureY));
+                        CreateVertex(verticesIndex++, posD, normal, new Vector2(1, textureY));
                     }
+                }
+            }
 
-                    if (i < splinePointCount - 1)
+            for (int i = 0; i < splinePointCount - 1; i++)
+            {
+                for (int j = 0; j < shapePointsCount - 1; j++)
+                {
+                    //if (j > 0)
                     {
                         // Indices
                         var vertexIndex = i * verticesPerShapeCount * 2; //Huidige Spline iteratie, 6, 12, 18
-                        var vertexShapeIndex = j * 2;
+                        var vertexShapeIndex = j * 4;
 
                         var indiceIndex = i * indicesPerShapeCount;
                         var triangleIndex = j * 6;
 
                         indices[indiceIndex + triangleIndex + 0] = vertexIndex + vertexShapeIndex + 0;
-                        indices[indiceIndex + triangleIndex + 1] = vertexIndex + vertexShapeIndex + verticesPerShapeCount;
+                        indices[indiceIndex + triangleIndex + 1] = vertexIndex + vertexShapeIndex + 2;
                         indices[indiceIndex + triangleIndex + 2] = vertexIndex + vertexShapeIndex + 1;
 
                         indices[indiceIndex + triangleIndex + 3] = vertexIndex + vertexShapeIndex + 1;
-                        indices[indiceIndex + triangleIndex + 4] = vertexIndex + vertexShapeIndex + verticesPerShapeCount;
-                        indices[indiceIndex + triangleIndex + 5] = vertexIndex + vertexShapeIndex + verticesPerShapeCount + 1;
+                        indices[indiceIndex + triangleIndex + 4] = vertexIndex + vertexShapeIndex + 2;
+                        indices[indiceIndex + triangleIndex + 5] = vertexIndex + vertexShapeIndex + 3;
                     }
                 }
             }
