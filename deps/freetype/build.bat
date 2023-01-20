e27c568e42655851424d1336caad0cda3cdf8592
jklawreszuk <jk.lawreszuk@gmail.com>
2023-01-21 00:43:35 +0100

    Cleanup WinStore refs

--- deps/freetype/build.bat
+++ deps/freetype/build.bat
@@ -1,9 +1,6 @@
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
 msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=Windows;Platform=Win32
 msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=Windows;Platform=x64
-msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=WindowsStore;Platform=x64
-msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=WindowsStore;Platform=Win32
-msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=WindowsStore;Platform=ARM
 msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=Windows10;Platform=x64
 msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=Windows10;Platform=Win32
 msbuild ..\..\externals\freetype\builds\windows\vc2013\freetype.vcxproj /Property:Configuration=Release;StridePlatform=Windows10;Platform=ARM
@@ -12,5 +9,3 @@ xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\Windows\Release\*
 xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\Windows\Release\*.pdb Windows\
 xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\Windows10\Release\*.dll Windows10\
 xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\Windows10\Release\*.pdb Windows10\
-xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\WindowsStore\Release\*.dll WindowsStore\
-xcopy /Y /S ..\..\externals\freetype\builds\windows\vc2013\bin\WindowsStore\Release\*.pdb WindowsStore\
