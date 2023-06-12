21056cb0ea2cc2167e4e8b4bcef4eff09ebf3927
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2023-06-12 21:28:50 +0200

    Mixer + IO

--- src/SDSL/Mixer/Mixer.cs
+++ src/SDSL/Mixer/Mixer.cs
@@ -2,14 +2,14 @@ using SoftTouch.Spirv;
 
 namespace SDSL.Mixer;
 
-public class Mixer
+public partial struct Mixer
 {
     public Mixin Shader { get; private set; }
     public Mixer()
     {
         Shader = new();
     }
-    public Mixin Build() => Shader;
+    public Mixin Get() => Shader;
 
     public Mixer With(Mixin mixin)
     {
