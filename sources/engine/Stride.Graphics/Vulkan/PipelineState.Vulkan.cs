3f8b98ac8eb3ce91646c537d397e723989a47144
Mario Guerra <mgg7007@gmail.com>
2023-06-16 22:09:09 +0200

    Fix Vulkan uses of Utilities.CopyMemory
    * Also some minor cleanup

--- sources/engine/Stride.Graphics/Vulkan/PipelineState.Vulkan.cs
+++ sources/engine/Stride.Graphics/Vulkan/PipelineState.Vulkan.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Vortice.Vulkan;
 using static Vortice.Vulkan.Vulkan;
 using Stride.Shaders;
