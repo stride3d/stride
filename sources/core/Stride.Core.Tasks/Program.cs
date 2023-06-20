206d77aceb50622e2b9842e5e3ac40a0652be5a1
Eideren <contact@eideren.com>
2023-06-20 15:45:10 +0200

    Show useful error message and directions to users lacking requirements for Stride.Core.Tasks

--- sources/core/Stride.Core.Tasks/Program.cs
+++ sources/core/Stride.Core.Tasks/Program.cs
@@ -16,7 +16,20 @@ namespace Stride.Core.Tasks
     {
         public static int Main(string[] args)
         {
-            MSBuildLocator.RegisterDefaults();
+            try
+            {
+                MSBuildLocator.RegisterDefaults();
+            }
+            catch (InvalidOperationException e) when (e.Message.StartsWith("No instances of MSBuild could be detected."))
+            {
+                // When tasks running through build tools throw, it logs an obtuse 'The command [...]/Stride.Core.Tasks.exe [...] exited with code - x'
+                // message requiring the user to dig in the output to figure out what happened.
+                // The following ensures that a clear message is logged before the stuff above is shown.
+                // Do note that the 'error' word in the message is essential for it to be logged,
+                // direct any message about this peculiar 'feature' over to Microsoft, thanks !
+                Console.Error.WriteLine($@"error {typeof(Program).Namespace}: No supported instance of MSBuild could be detected, make sure you have .Net SDK {Environment.Version.Major} {Environment.Version.Minor} installed");
+                throw;
+            }
             return RealMain(args);
         }
 
