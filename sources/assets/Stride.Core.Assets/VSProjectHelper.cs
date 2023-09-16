6969cba2031a2214614c677748432d6ee5d6f4d5
Eideren <contact@eideren.com>
2023-09-16 15:47:11 +0200

    [Project] Show custom message for users with outdated dotnet version

--- sources/assets/Stride.Core.Assets/VSProjectHelper.cs
+++ sources/assets/Stride.Core.Assets/VSProjectHelper.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
@@ -297,12 +298,21 @@ namespace Stride.Core.Assets
 
             void ErrorRaised(object sender, Microsoft.Build.Framework.BuildErrorEventArgs e)
             {
-                var loggerResult = logger as LoggerResult;
-                if (loggerResult != null)
+                if (logger is LoggerResult loggerResult)
                 {
-                    loggerResult.Module = string.Format("{0}({1},{2})", e.File, e.LineNumber, e.ColumnNumber);
+                    loggerResult.Module = $"{e.File}({e.LineNumber},{e.ColumnNumber})";
                 }
-                logger.Error(e.Message);
+
+                if (e.Code == "NETSDK1045")
+                {
+                    var netVersion = Regex.Match(e.Message, @"\.(NET|net) ?(\d+\.\d+)");
+                    if (netVersion.Success)
+                        logger.Error($"{e.Code}: this project requires {netVersion} SDK, please go to https://dotnet.microsoft.com/download and download {netVersion} SDK.");
+                    else
+                        logger.Error($"{e.Code}: {e.Message}");
+                }
+                else
+                    logger.Error(e.Message);
             }
         }
 
