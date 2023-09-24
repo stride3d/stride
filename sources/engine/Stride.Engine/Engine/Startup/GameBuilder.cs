d2f83d88f9d6db2737e4f5d72ba95dad65960b49
Raphael Schmitz <raphaelschmitz00@gmail.com>
2023-09-25 00:55:59 +0200

    rework

--- sources/engine/Stride.Engine/Engine/Startup/GameBuilder.cs
+++ sources/engine/Stride.Engine/Engine/Startup/GameBuilder.cs
@@ -1,18 +1,87 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 
 namespace Stride.Engine.Startup;
 
-public class GameBuilder
+public interface IGameBuilder
 {
-    public GameBuilder(ServiceCollection serviceCollection)
+    public ConfigurationManager Configuration { get; }
+    public IServiceCollection Services { get; }
+    public ILoggingBuilder Logging { get; }
+    public IAppEnvironment Environment { get; }
+
+    IStrideApplication Build();
+}
+
+internal class GameBuilder : IGameBuilder
+{
+    public GameBuilder()
     {
-        ServiceCollection = serviceCollection;
+        Configuration = new ConfigurationManager();
+
+        Environment = new AppEnvironment { ApplicationName = Assembly.GetEntryAssembly()?.GetName().Name ?? "", EnvironmentName = GetEnvironment(), ContentRootPath = AppContext.BaseDirectory };
+        
+        var physicalFileProvider = new PhysicalFileProvider(Environment.ContentRootPath);
+        Environment.ContentRootFileProvider = physicalFileProvider;
+        Configuration.SetFileProvider(physicalFileProvider);
+
+        // As an example of how ASP handles appsettings.Development.json
+        // overriding configuration values from appsettings.json
+        Configuration
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{Environment.EnvironmentName}.json", optional: true);
+
+        Services = new ServiceCollection();
+
+
+        Services.AddSingleton(Environment);
+        Services.AddSingleton(_ => Configuration);
+
+        Services.AddLogging();
+        Logging = new LoggingBuilder(Services);
+    }
+    
+    public ConfigurationManager Configuration { get; }
+    public IServiceCollection Services { get; }
+    public ILoggingBuilder Logging { get; }
+    public IAppEnvironment Environment { get; }
+    
+    private static string GetEnvironment()
+    {
+#if DEBUG
+        return Environments.Development;
+#elif STAGING
+        return Environments.Staging;
+#endif
+        return Environments.Production;
     }
 
-    public ServiceCollection ServiceCollection { get; }
+    public IStrideApplication Build() =>
+        new StrideApplication(
+            Configuration,
+            Environment,
+            Services.BuildServiceProvider()
+        );
 
-    public GameRunner Build() => new(ServiceCollection);
+    ///<remarks>
+    /// Is private in the library, so I can't just use that one.
+    /// However, this is all it was.
+    /// </remarks>
+    private sealed class LoggingBuilder : ILoggingBuilder
+    {
+        public LoggingBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+
+        public IServiceCollection Services { get; }
+    }
 }
--- sources/engine/Stride.Engine/Engine/Startup/StrideApplication.cs
+++ sources/engine/Stride.Engine/Engine/Startup/StrideApplication.cs
@@ -1,22 +1,68 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Stride.Games;
 
 namespace Stride.Engine.Startup;
 
-public class StrideApplication
+public interface IStrideApplication : IDisposable, IAsyncDisposable
 {
-    /// <summary>
-    /// Creates a GameBuilder with the default functionality added.
-    /// </summary>
-    public static GameBuilder CreateBuilder()
+    ConfigurationManager Configuration { get; }
+    IAppEnvironment Environment { get; }
+    ServiceProvider Services { get; }
+    ILogger Logger { get; }
+
+    void Run();
+}
+
+public class StrideApplication : IStrideApplication
+{
+    public StrideApplication(
+        ConfigurationManager configuration,
+        IAppEnvironment environment,
+        ServiceProvider services
+    )
     {
-        var serviceCollection = new ServiceCollection();
-        var gameBuilder = new GameBuilder(serviceCollection);
+        Configuration = configuration;
+        Environment = environment;
+        Services = services;
+        Logger = 
+            Services
+                .GetRequiredService<ILoggerFactory>()
+                .CreateLogger(Environment.ApplicationName ?? nameof(StrideApplication));
 
-        gameBuilder.ServiceCollection.AddSingleton<Game>();
+    }
 
-        return gameBuilder;
+
+    public ConfigurationManager Configuration { get; }
+    public IAppEnvironment Environment { get; }
+    public ServiceProvider Services { get; }
+    public ILogger Logger { get; }
+
+    public static IGameBuilder CreateBuilder() => new GameBuilder();
+
+    public void Run()
+    {
+        using (var game = new Game())
+        {
+            game.Run();
+        }
+    }
+
+
+    public void Dispose()
+    {
+        Services.Dispose();
+    }
+
+
+    public async ValueTask DisposeAsync()
+    {
+        await Services.DisposeAsync();
     }
 }
