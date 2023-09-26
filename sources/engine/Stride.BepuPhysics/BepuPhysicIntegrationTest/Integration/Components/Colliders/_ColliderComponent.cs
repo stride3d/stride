21b402225dfcf777f6516076ccd45d70a3a3f50f
Doprez <73259914+Doprez@users.noreply.github.com>
2023-09-25 19:43:42 -0600

    changed to EntityComponent

--- sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Colliders/_ColliderComponent.cs
+++ sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Colliders/_ColliderComponent.cs
@@ -10,16 +10,10 @@ namespace BepuPhysicIntegrationTest.Integration.Components.Colliders
     [DataContract]
     [DefaultEntityComponentProcessor(typeof(ColliderProcessor), ExecutionMode = ExecutionMode.Runtime)]
     [ComponentCategory("Bepu - Colliders")]
-    public abstract class ColliderComponent : StartupScript
+    public abstract class ColliderComponent : EntityComponent
     {
         public float Mass { get; set; } = 1f;
 
         internal ContainerComponent Container => Entity.GetInMeOrParents<ContainerComponent>();
-        
-        public override void Start()
-        {
-            base.Start();
-        }
-
     }
 }
