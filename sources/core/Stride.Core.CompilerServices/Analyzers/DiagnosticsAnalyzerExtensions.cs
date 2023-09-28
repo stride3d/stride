5014834e7c357fc7f4a3a3e796caed54fa8f0fa2
IXLLEGACYIXL <ixllegacy123@outlook.com>
2023-09-28 21:34:57 +0200

    empty line removed

--- sources/core/Stride.Core.CompilerServices/Analyzers/DiagnosticsAnalyzerExtensions.cs
+++ sources/core/Stride.Core.CompilerServices/Analyzers/DiagnosticsAnalyzerExtensions.cs
@@ -4,7 +4,6 @@ public static class DiagnosticsAnalyzerExtensions
 {
     public static void ReportDiagnostics(this DiagnosticAnalyzer analyzer, DiagnosticDescriptor rule, SymbolAnalysisContext context, INamedTypeSymbol dataMemberAttribute, ISymbol symbol)
     {
-
         var identifier = symbol.Locations;
         foreach (var location in identifier)
         {
