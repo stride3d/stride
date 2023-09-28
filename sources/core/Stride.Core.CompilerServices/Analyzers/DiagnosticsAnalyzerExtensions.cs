e469a08fc853d54a56e1e7175b174db791d0d5d0
IXLLEGACYIXL <ixllegacy123@outlook.com>
2023-09-28 20:38:44 +0200

    format

--- sources/core/Stride.Core.CompilerServices/Analyzers/DiagnosticsAnalyzerExtensions.cs
+++ sources/core/Stride.Core.CompilerServices/Analyzers/DiagnosticsAnalyzerExtensions.cs
@@ -2,14 +2,14 @@
 namespace Stride.Core.CompilerServices.Analyzers;
 public static class DiagnosticsAnalyzerExtensions
 {
-    public static void ReportDiagnostics(this DiagnosticAnalyzer analyzer,DiagnosticDescriptor rule,SymbolAnalysisContext context, INamedTypeSymbol dataMemberAttribute, ISymbol symbol)
+    public static void ReportDiagnostics(this DiagnosticAnalyzer analyzer, DiagnosticDescriptor rule, SymbolAnalysisContext context, INamedTypeSymbol dataMemberAttribute, ISymbol symbol)
     {
 
-            var identifier = symbol.Locations;
-            foreach (var location in identifier)
-            {
-                var diagnostic = Diagnostic.Create(rule, location, symbol.Name);
-                context.ReportDiagnostic(diagnostic);
-            }
+        var identifier = symbol.Locations;
+        foreach (var location in identifier)
+        {
+            var diagnostic = Diagnostic.Create(rule, location, symbol.Name);
+            context.ReportDiagnostic(diagnostic);
+        }
     }
 }
