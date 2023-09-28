b90f87c075230877d7f1d650fd5fda58050746c6
IXLLEGACYIXL <ixllegacy123@outlook.com>
2023-09-28 21:35:16 +0200

    expand references

--- sources/core/Stride.Core.CompilerServices/Common/WellKnownReferences.cs
+++ sources/core/Stride.Core.CompilerServices/Common/WellKnownReferences.cs
@@ -10,9 +10,21 @@ internal class WellKnownReferences
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataMemberAttribute");
     }
+    public static INamedTypeSymbol? DataContractAttribute(Compilation compilation)
+    {
+        return compilation.GetTypeByMetadataName("Stride.Core.DataContractAttribute");
+    }
+    public static INamedTypeSymbol? DataMemberIgnoreAttribute(Compilation compilation)
+    {
+        return compilation.GetTypeByMetadataName("Stride.Core.DataMemberIgnoreAttribute");
+    }
     public static INamedTypeSymbol? DataMemberMode(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataMemberMode");
+    }    
+    public static INamedTypeSymbol? DataMemberUpdatableAttribute(Compilation compilation)
+    {
+        return compilation.GetTypeByMetadataName("Stride.Updater.DataMemberUpdatableAttribute");
     }
     public static bool HasAttribute(ISymbol symbol, INamedTypeSymbol attribute)
     {
