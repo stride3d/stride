6913e70c763eadda9ca2f12cd582622fc1de62b8
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-08 11:20:44 +0200

    [Editor] Create projects from Avalonia template

---
+++ sources/xplat-editor/Stride.GameStudio.Avalonia.Desktop/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+using Avalonia;
+
+namespace Stride.GameStudio.Avalonia.Desktop;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+
+}
---
+++ sources/xplat-editor/Stride.GameStudio.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
