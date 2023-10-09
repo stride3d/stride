7101d0612e3af2a028a099742d657e52e7ed9abb
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-09 20:59:53 +0200

    [Editor] Add placeholders

---
+++ sources/xplat-editor/Stride.GameStudio.Avalonia/Views/SolutionExplorerView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class SolutionExplorerView : UserControl
+{
+    public SolutionExplorerView()
+    {
+        InitializeComponent();
+    }
+}
