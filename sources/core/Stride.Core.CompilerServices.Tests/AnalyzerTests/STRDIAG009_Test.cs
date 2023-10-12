c35959ec550be14eed2417bd86e477ab627deb8e
IXLLEGACYIXL <ixllegacy123@outlook.com>
2023-10-13 00:09:15 +0200

    add strdiag009 tests

---
+++ sources/core/Stride.Core.CompilerServices.Tests/AnalyzerTests/STRDIAG009_Test.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.CompilerServices.Analyzers;
+using Xunit;
+
+namespace Stride.Core.CompilerServices.Tests.AnalyzerTests;
+public class STRDIAG009_Test
+{
+    private static string[] Types = new string[]
+    {
+        "string",
+        "int",
+        "float",
+        "double",
+    };
+    [Fact]
+    public void No_Error_On_Immutable_Types()
+    {
+        foreach(var type in Types)
+        {
+            string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, $"[DataMember] public System.Collections.Generic.Dictionary<{type},object> Value {{ get; }}");
+            TestHelper.ExpectNoDiagnosticsErrors(sourceCode);
+        }
+    }
+    [Fact]
+    public void Error_On_Reference_Type()
+    {
+        string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, "[DataMember] public System.Collections.Generic.Dictionary<object,object> Value { get; }");
+        TestHelper.ExpectDiagnosticsError(sourceCode, STRDIAG009InvalidDictionaryKey.DiagnosticId);
+    }
+    [Fact]
+    public void Error_On_Interface_Type()
+    {
+        string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, "[DataMember] public System.Collections.Generic.Dictionary<ICloneable,object> Value { get; }");
+        TestHelper.ExpectDiagnosticsError(sourceCode, STRDIAG009InvalidDictionaryKey.DiagnosticId);
+    }
+}
