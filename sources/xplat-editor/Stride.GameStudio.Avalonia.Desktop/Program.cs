7ac150c0d0e05febc5e5235a151591aa3f203066
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-14 15:45:56 +0200

    [Editor] Display the first level of the solution in the solution explorer

--- sources/xplat-editor/Stride.GameStudio.Avalonia.Desktop/Program.cs
+++ sources/xplat-editor/Stride.GameStudio.Avalonia.Desktop/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Build.Locator;
 
 namespace Stride.GameStudio.Avalonia.Desktop;
 
-class Program
+internal sealed class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
