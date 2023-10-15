3fdb1406d0db0d7c5209ef96914836353ec4566d
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-15 19:35:22 +0200

    [Presentation] Add Stride.Assets.Presentation project

---
+++ sources/editor/Stride.Assets.Presentation/Module.cs
@@ -0,0 +1,18 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using Stride.Assets.Materials;
+using Stride.Core.Reflection;
+
+namespace Stride.Assets.Presentation;
+
+internal class Module
+{
+    [Core.ModuleInitializer]
+    public static void Initialize()
+    {
+        RuntimeHelpers.RunModuleConstructor(typeof(MaterialAsset).Module.ModuleHandle);
+        AssemblyRegistry.Register(typeof(Module).Assembly, AssemblyCommonCategories.Assets);
+    }
+}
