c4541fc2702ffb8b2d711644fd76ce643a46c4cf
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-16 00:01:36 +0200

    [Editor] Basic plugin infrastructure

---
+++ sources/xplat-editor/Stride.Assets.Presentation/StrideDefaultAssetsPlugin.cs
@@ -0,0 +1,15 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Stride.Core.Assets.Presentation;
+using Stride.Core.Diagnostics;
+
+namespace Stride.Assets.Presentation;
+
+public sealed class StrideDefaultAssetsPlugin : AssetsPlugin
+{
+    public override void InitializePlugin(ILogger logger)
+    {
+        // nothing for now
+    }
+}
---
+++ sources/xplat-editor/Stride.Core.Assets.Editor/Services/PluginService.cs
@@ -0,0 +1,39 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Stride.Core.Assets.Editor.ViewModels;
+using Stride.Core.Assets.Presentation;
+using Stride.Core.Assets.Presentation.ViewModels;
+using Stride.Core.Diagnostics;
+using Stride.Core.Extensions;
+
+namespace Stride.Core.Assets.Editor.Services;
+
+public sealed class PluginService
+{
+    internal void RegisterSession(SessionViewModel session, ILogger logger)
+    {
+        foreach (var plugin in AssetsPlugin.RegisteredPlugins)
+        {
+            plugin.InitializePlugin(logger);
+
+            // Asset view models types
+            var assetViewModelsTypes = new Dictionary<Type, Type>();
+            plugin.RegisterAssetViewModelTypes(assetViewModelsTypes);
+            AssertType(typeof(Asset), assetViewModelsTypes.Select(x => x.Key));
+            AssertType(typeof(AssetViewModel), assetViewModelsTypes.Select(x => x.Value));
+            session.AssetViewModelTypes.AddRange(assetViewModelsTypes);
+        }
+    }
+
+    private static void AssertType(Type baseType, Type specificType)
+    {
+        if (!baseType.IsAssignableFrom(specificType))
+            throw new ArgumentException($"Type [{specificType.FullName}] must be assignable to {baseType.FullName}", nameof(specificType));
+    }
+
+    private static void AssertType(Type baseType, IEnumerable<Type> specificTypes)
+    {
+        specificTypes.ForEach(x => AssertType(baseType, x));
+    }
+}
