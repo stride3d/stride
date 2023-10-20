0b2c7f20f83511154dbdb7c1437314074e5032a0
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-20 13:45:05 +0200

    [Presentation] More package or project related view models

--- sources/editor/Stride.Core.Assets.Presentation/ViewModels/DirectoryBaseViewModel.cs
+++ sources/editor/Stride.Core.Assets.Presentation/ViewModels/DirectoryBaseViewModel.cs
@@ -1,18 +1,54 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Collections.ObjectModel;
+using Stride.Core.Presentation.Collections;
+
 namespace Stride.Core.Assets.Presentation.ViewModels;
 
 public abstract class DirectoryBaseViewModel : SessionObjectViewModel
 {
+    private readonly AutoUpdatingSortedObservableCollection<DirectoryViewModel> subDirectories = new(CompareDirectories);
+
     protected DirectoryBaseViewModel(ISessionViewModel session)
         : base(session)
     {
+        SubDirectories = new ReadOnlyObservableCollection<DirectoryViewModel>(subDirectories);
     }
 
+    /// <summary>
+    /// Gets the package containing this directory.
+    /// </summary>
+    public abstract PackageViewModel Package { get; }
+    
+    /// <summary>
+    /// Gets or sets the parent directory of this directory.
+    /// </summary>
     public abstract DirectoryBaseViewModel Parent { get; set; }
 
+    /// <summary>
+    /// Gets the path of this directory in its current package.
+    /// </summary>
     public abstract string Path { get; }
 
+    /// <summary>
+    /// Gets the root directory containing this directory, or this directory itself if it is a root directory.
+    /// </summary>
     public abstract MountPointViewModel Root { get; }
+
+    /// <summary>
+    /// Gets the read-only collection of sub-directories contained in this directory.
+    /// </summary>
+    public ReadOnlyObservableCollection<DirectoryViewModel> SubDirectories { get; }   
+
+    private static int CompareDirectories(DirectoryViewModel? x, DirectoryViewModel? y)
+    {
+        if (x == null && y == null)
+            return 0;
+
+        if (x != null && y != null)
+            return string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+
+        return x == null ? -1 : 1;
+    }
 }
--- sources/editor/Stride.Core.Assets.Presentation/ViewModels/DirectoryViewModel.cs
+++ sources/editor/Stride.Core.Assets.Presentation/ViewModels/DirectoryViewModel.cs
@@ -15,19 +15,34 @@ public sealed class DirectoryViewModel : DirectoryBaseViewModel
         this.parent = parent;
     }
 
+    /// <summary>
+    /// Gets the package containing this directory.
+    /// </summary>
+    public override PackageViewModel Package => Parent.Package;
+
+    /// <summary>
+    /// Gets or sets the parent directory of this directory.
+    /// </summary>
     public override DirectoryBaseViewModel Parent
     {
         get => parent;
         set => SetValue(ref parent, value);
     }
 
+    /// <summary>
+    /// Gets or sets the name of this directory.
+    /// </summary>
     public override string Name
     {
         get => name;
         set => SetValue(ref name, value);
     }
 
+    /// <summary>
+    /// Gets the path of this directory in its current package.
+    /// </summary>
     public override string Path => Parent.Path + Name;
 
+    /// <inheritdoc/>
     public override MountPointViewModel Root => Parent.Root;
 }
