5fb7ff6e22cc46b73580458ee603d7f79c4b9aaf
MaximilianEmel <19846453+MaximilianEmel@users.noreply.github.com>
2023-11-01 18:31:58 -0400

    Silk.NET's SearchPathContainer is now an array

--- sources/engine/Stride.Graphics/OpenGL/GLCoreLibraryNameContainer.cs
+++ sources/engine/Stride.Graphics/OpenGL/GLCoreLibraryNameContainer.cs
@@ -12,22 +12,22 @@ namespace Silk.NET.OpenGL
     internal class GLCoreLibraryNameContainer : SearchPathContainer
     {
         /// <inheritdoc />
-        public override string Linux => "libGL.so.1";
+        public override string[] Linux => new[] { "libGL.so.1" };
 
         /// <inheritdoc />
-        public override string MacOS => "/System/Library/Frameworks/OpenGL.framework/OpenGL";
+        public override string[] MacOS => new[] { "/System/Library/Frameworks/OpenGL.framework/OpenGL" };
 
         /// <inheritdoc />
-        public override string Android => "libGL.so.1";
+        public override string[] Android => new[] { "libGL.so.1" };
 
         /// <inheritdoc />
-        public override string IOS => "/System/Library/Frameworks/OpenGL.framework/OpenGL";
+        public override string[] IOS => new[] { "/System/Library/Frameworks/OpenGL.framework/OpenGL" };
 
         /// <inheritdoc />
-        public override string Windows64 => "opengl32.dll";
+        public override string[] Windows64 => new[] { "opengl32.dll" };
 
         /// <inheritdoc />
-        public override string Windows86 => "opengl32.dll";
+        public override string[] Windows86 => new[] { "opengl32.dll" };
     }
 }
 #endif
