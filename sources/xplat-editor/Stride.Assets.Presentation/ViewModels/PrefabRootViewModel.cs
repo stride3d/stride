faa4e8eb970c6a2c0503fa244b46d7536c2b4d76
Nicolas Musset <nicolas@color-rise.xyz>
2023-11-11 14:12:25 +0100

    [Editor] Support selection service in a scene or prefab editor

--- sources/xplat-editor/Stride.Assets.Presentation/ViewModels/PrefabRootViewModel.cs
+++ sources/xplat-editor/Stride.Assets.Presentation/ViewModels/PrefabRootViewModel.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Stride.Core;
+
 namespace Stride.Assets.Presentation.ViewModels;
 
 public sealed class PrefabRootViewModel : EntityHierarchyRootViewModel
@@ -9,6 +11,9 @@ public sealed class PrefabRootViewModel : EntityHierarchyRootViewModel
         : base(asset)
     {
     }
+    
+    /// <inheritdoc/>
+    public override AbsoluteId Id => new(Asset.Id, Guid.Empty);
 
     public override string? Name { get => "PrefabRoot"; set => throw new NotSupportedException($"Cannot change the name of a {nameof(PrefabRootViewModel)} object."); }
 }
