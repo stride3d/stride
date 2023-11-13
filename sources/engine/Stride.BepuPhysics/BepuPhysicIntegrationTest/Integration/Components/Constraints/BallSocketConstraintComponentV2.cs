bbfd61cf255e919560fba9f1877bdca84c0f4846
Nicogo <->
2023-11-13 22:42:25 +0100

    Test [WIP]

---
+++ sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Constraints/BallSocketConstraintComponentV2.cs
@@ -0,0 +1,18 @@
+﻿using BepuPhysicIntegrationTest.Integration.Processors;
+using BepuPhysics.Constraints;
+using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Engine.Design;
+
+namespace BepuPhysicIntegrationTest.Integration.Components.Constraints
+{
+    [DataContract]
+    [DefaultEntityComponentProcessor(typeof(ConstraintProcessor), ExecutionMode = ExecutionMode.Runtime)]
+    [ComponentCategory("Bepu - Constraint")]
+    public class BallSocketConstraintComponentV2 : ConstraintComponent
+    {
+        public BallSocket parameters { get; set; }
+
+    }
+}
