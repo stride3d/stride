33c30fc2d8769b91f98e552ac795f40a791c8f4e
Virgile Bello <virgile@stride3d.net>
2023-11-16 22:43:50 +0900

    [NuGet] Changed Stride.NuGetResolver to be a proper library (otherwise there was a deadlock with the JIT). Auto-generates NuGetResolverModuleInitializer.cs for each entrypoint assembly/TFM.

--- sources/shared/Stride.NuGetResolver.Targets/NuGetResolverModuleInitializer.cs
+++ sources/shared/Stride.NuGetResolver.Targets/NuGetResolverModuleInitializer.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Stride.Core.Assets
 {
-    class NuGetModuleInitializer
+    class NuGetResolverModuleInitializer
     {
         [ModuleInitializer(-100000)]
         internal static void __Initialize__()
@@ -15,7 +15,7 @@ namespace Stride.Core.Assets
                 || Assembly.GetEntryAssembly() == Assembly.GetCallingAssembly())) // .NET Core: check against calling assembly (note: if using Stride.NuGetLoader, it will be skipped as well which is what we want)
                 return;
 
-            NuGetAssemblyResolver.SetupNuGet(Assembly.GetExecutingAssembly().GetName().Name, StrideVersion.NuGetVersion);
+            NuGetAssemblyResolver.SetupNuGet(STRIDE_NUGET_RESOLVER_TARGET_FRAMEWORK, STRIDE_NUGET_RESOLVER_PACKAGE_NAME, STRIDE_NUGET_RESOLVER_PACKAGE_VERSION);
         }
     }
 }
