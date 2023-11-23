38fa1cbd8cbff054cf134dc1dfdcea2b085656db
Doprez <73259914+Doprez@users.noreply.github.com>
2023-11-23 12:31:53 -0700

    wip

--- sources/tools/Stride.VisualStudio.Package/Commands/StrideCommandsProxy.cs
+++ sources/tools/Stride.VisualStudio.Package/Commands/StrideCommandsProxy.cs
@@ -173,7 +173,7 @@ namespace Stride.VisualStudio.Commands
             {
                 // Try both net8.0 and net472
                 var success = false;
-                foreach (var folder in new[] { "net8.0-windows7.0", "net472" })
+                foreach (var folder in new[] { "net8.0" })
                 {
                     var logger = new Logger();
                     var solutionRoot = Path.GetDirectoryName(solution);
@@ -188,7 +188,7 @@ namespace Stride.VisualStudio.Commands
                 }
                 if (!success)
                 {
-                    throw new InvalidOperationException($"Could not restore {packageName} {packageInfo.ExpectedVersion}, this visual studio extension may fail to work properly without it. To fix this you can either build {packageName} or pull the right version from nugget manually");
+                    throw new InvalidOperationException($"Could not restore {packageName} {packageInfo.ExpectedVersion}, this visual studio extension may fail to work properly without it. To fix this you can either build {packageName} or pull the right version from nuget manually path {Path.GetDirectoryName(solution)}");
                 }
             }
 
