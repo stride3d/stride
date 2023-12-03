bb1041e94a5bc80572a3f0c6601bb8dc6df52b52
Doprez <73259914+Doprez@users.noreply.github.com>
2023-12-03 14:04:46 -0700

    started basics

---
+++ sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Utils/BepuCharacterComponent.cs
@@ -0,0 +1,22 @@
+﻿using BepuPhysicIntegrationTest.Integration.Components.Containers;
+using BepuPhysicIntegrationTest.Integration.Extensions;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+
+namespace BepuPhysicIntegrationTest.Integration.Components.Utils;
+public class BepuCharacterComponent : StartupScript
+{
+	public float Speed { get; set; } = 1f;
+	public float JumpSpeed { get; set; } = 1f;
+
+	public BodyContainerComponent? CharacterBody { get; set; }
+
+	public override void Start()
+	{
+	}
+
+	public void Move(Vector3 direction)
+	{
+		CharacterBody.GetPhysicBody().Value.Velocity.Linear += direction.ToNumericVector() * Speed;
+	}
+}
---
+++ sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Utils/CharacterControllerComponent.cs
@@ -0,0 +1,39 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BepuPhysicIntegrationTest.Integration.Components.Utils;
+public class CharacterControllerComponent : SyncScript
+{
+	public CameraComponent? Camera { get; set; }
+	public BepuCharacterComponent? Character { get; set; }
+
+	public override void Start()
+	{
+	}
+
+	public override void Update()
+	{
+		// Keyboard movement
+		var moveDirection = Vector2.Zero;
+		if (Input.IsKeyDown(Keys.W))
+			moveDirection.Y = 1;
+		if (Input.IsKeyDown(Keys.S))
+			moveDirection.Y -= 1;
+		if (Input.IsKeyDown(Keys.A))
+			moveDirection.X -= 1;
+		if (Input.IsKeyDown(Keys.D))
+			moveDirection.X = 1;
+
+		var velocity = new Vector3(moveDirection.X, 0, -moveDirection.Y);
+		velocity.Normalize();
+
+		velocity = Vector3.Transform(velocity, Camera.Entity.Transform.Rotation);
+		Character.Move(velocity);
+	}
+}
