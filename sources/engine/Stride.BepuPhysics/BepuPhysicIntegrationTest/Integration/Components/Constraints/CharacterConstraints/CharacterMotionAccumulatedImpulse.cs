2fe1b4eed9bdc940cfa7d7968775bfd50c03eddc
Doprez <73259914+Doprez@users.noreply.github.com>
2023-12-05 10:01:16 -0700

    added static character constraint

---
+++ sources/engine/Stride.BepuPhysics/BepuPhysicIntegrationTest/Integration/Components/Constraints/CharacterConstraints/CharacterMotionAccumulatedImpulse.cs
@@ -0,0 +1,9 @@
+﻿using BepuUtilities;
+using System.Numerics;
+
+namespace BepuPhysicIntegrationTest.Integration.Components.Constraints;
+public struct CharacterMotionAccumulatedImpulse
+{
+	public Vector2Wide Horizontal;
+	public Vector<float> Vertical;
+}
