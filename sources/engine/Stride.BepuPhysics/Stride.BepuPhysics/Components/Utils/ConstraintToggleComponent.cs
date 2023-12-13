b20d5ec35cddc5907a58592f59d702850f0ccb36
Nicogo <->
2023-12-13 16:44:42 +0100

    debug tool

---
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Components/Utils/ConstraintToggleComponent.cs
@@ -0,0 +1,34 @@
+﻿using Stride.BepuPhysics.Components.Constraints;
+using Stride.BepuPhysics.Extensions;
+using Stride.Engine;
+using Stride.Input;
+
+#warning This should not be part of the base API, move it to demo/sample
+
+namespace Stride.BepuPhysics.Components.Utils
+{
+    //[DataContract("SpawnerComponent", Inherited = true)]
+    [ComponentCategory("Bepu - Utils")]
+    public class ConstraintToggleComponent : SyncScript
+    {
+        public BaseConstraintComponent? Component { get; set; }
+
+
+        public override void Start()
+        {
+        }
+
+        public override void Update()
+        {
+            if (Component == null )
+                return;
+
+            if (Input.IsKeyPressed(Keys.G))
+            {
+                Component.Enabled = !Component.Enabled;
+            }
+
+            DebugText.Print($"G forr toggle constraint", new(BepuAndStrideExtensions.X_DEBUG_TEXT_POS, 300));
+        }
+    }
+}
