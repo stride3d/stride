bd32d2f60c4c5971f12d2f63534803a94853b281
Nicolas Musset <nicolas@color-rise.xyz>
2023-12-15 22:51:46 +0100

    [Presentation] Move some utils

---
+++ sources/presentation/Stride.Core.Presentation/Core/AnonymousComparer.cs
@@ -0,0 +1,30 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Stride.Core.Presentation.Core;
+
+/// <summary>
+/// This class allows implementation of <see cref="IComparer{T}"/> using an anonymous function.
+/// </summary>
+/// <typeparam name="T">The type of object this comparer can compare.</typeparam>
+public class AnonymousComparer<T> : IComparer<T>
+{
+    private readonly Func<T?, T?, int> compare;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AnonymousComparer{T}"/> class.
+    /// </summary>
+    /// <param name="compare">The comparison function to use for this comparer.</param>
+    public AnonymousComparer(Func<T?, T?, int> compare)
+    {
+        ArgumentNullException.ThrowIfNull(compare);
+
+        this.compare = compare;
+    }
+
+    /// <inheritdoc/>
+    public int Compare(T? x, T? y)
+    {
+        return compare(x, y);
+    }
+}
---
+++ sources/presentation/Stride.Core.Presentation/Core/NaturalStringComparer.cs
@@ -0,0 +1,63 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Text.RegularExpressions;
+
+namespace Stride.Core.Presentation.Core;
+
+public class NaturalStringComparer : IComparer<string>
+{
+    private static readonly Regex NumericRegex = new("([0-9]+)", RegexOptions.Compiled);
+    private readonly StringComparison comparison;
+
+    public NaturalStringComparer(StringComparison comparison = StringComparison.InvariantCultureIgnoreCase)
+    {
+        this.comparison = comparison;
+    }
+
+    public int Compare(string? x, string? y)
+    {
+        if (x is null && y is null)
+            return 0;
+        if (x is null)
+            return -1;
+        if (y is null)
+            return 1;
+        
+        // Split strings by numbers
+        var splitX = NumericRegex.Split(x.Replace(" ", ""));
+        var splitY = NumericRegex.Split(y.Replace(" ", ""));
+
+        var comparer = 0;
+        for (var i = 0; comparer == 0 && i < splitX.Length; i++)
+        {
+            if (splitY.Length <= i)
+            {
+                // x has more numeric patterns: x > y
+                comparer = 1;
+                break;
+            }
+
+            if (int.TryParse(splitX[i], out var numericX))
+            {
+                if (int.TryParse(splitY[i], out var numericY))
+                {
+                    // both numbers
+                    comparer = numericX - numericY;
+                }
+                else
+                {
+                    // x is a number but y is not
+                    comparer = 1;
+                }
+            }
+            else
+            {
+                // both not numbers, fallback to default string comparison
+                comparer = string.Compare(splitX[i], splitY[i], comparison);
+            }
+        }
+
+        return comparer;
+    }
+}
