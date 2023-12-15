94ec78b012fe77fb8fef29e765ce313120b8b1bc
Nicogo1705 <nicogoosse1705@gmail.com>
2023-12-15 14:24:15 +0100

    Fix nugget package

---
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Reflection;
+
+namespace Stride.BepuPhysics
+{
+    internal class Module
+    {
+        [Stride.Core.ModuleInitializer]
+        public static void Initialize()
+        {
+            AssemblyRegistry.Register(typeof(Module).GetTypeInfo().Assembly, AssemblyCommonCategories.Assets);
+        }
+    }
+}
