255d0032df0727a3dc32cf6836f0d6d43e08a78f
Nicolas Musset <nicolas@color-rise.xyz>
2024-01-01 17:29:35 +0100

    [Launcher] Initialize new projects using Avalonia

---
+++ sources/xplat-editor/Stride.Launcher.Desktop/Program.cs
@@ -0,0 +1,23 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Avalonia;
+
+namespace Stride.Launcher.Desktop;
+
+internal sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
