2ffa8fb4276936abbc52957e9def096dadda16b9
Nicolas Musset <nicolas@color-rise.xyz>
2024-01-03 19:58:20 +0100

    [Presentation] Add Stride.Core.Presentation.Avalonia
    So that it can be reused outside editors, e.g. in the Launcher

---
+++ sources/xplat-editor/Stride.Core.Presentation.Avalonia/Converters/AndMulti.cs
@@ -0,0 +1,18 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Globalization;
+using Avalonia;
+using Stride.Core.Presentation.Avalonia.Internal;
+
+namespace Stride.Core.Presentation.Avalonia.Converters;
+
+public class AndMulti : MultiValueConverterBase<AndMulti>
+{
+    public override object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count < 2) return AvaloniaProperty.UnsetValue;
+
+        return values.All(x => x != AvaloniaProperty.UnsetValue && x is bool b && b).Box();
+    }
+}
---
+++ sources/xplat-editor/Stride.Core.Presentation.Avalonia/MarkupExtensions/SizeExtension.cs
@@ -0,0 +1,40 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Metadata;
+
+namespace Stride.Core.Presentation.Avalonia.MarkupExtensions;
+
+public class SizeExtension : MarkupExtension
+{
+    public SizeExtension(double uniformLength)
+    {
+        Value = new Size(uniformLength, uniformLength);
+    }
+
+    public SizeExtension(double width, double height)
+    {
+        Value = new Size(width, height);
+    }
+
+    public SizeExtension(Size value)
+    {
+        Value = value;
+    }
+
+    public SizeExtension()
+    {
+        Value = Size.Infinity;
+    }
+
+    [Content]
+    public Size Value { get; set; }
+    
+    /// <inheritdoc/>
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Value;
+    }
+}
