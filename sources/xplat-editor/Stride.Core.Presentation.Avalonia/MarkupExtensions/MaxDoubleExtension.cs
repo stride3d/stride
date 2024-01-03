e7598ef8bba7f451fdab7f3bf5bd8dbc8a16d122
Nicolas Musset <nicolas@color-rise.xyz>
2024-01-03 19:58:20 +0100

    [Presentation] Add Stride.Core.Presentation.Avalonia
    So that it can be reused outside editors, e.g. in the Launcher

---
+++ sources/xplat-editor/Stride.Core.Presentation.Avalonia/MarkupExtensions/MaxDoubleExtension.cs
@@ -0,0 +1,15 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Core.Presentation.Avalonia.MarkupExtensions;
+
+public sealed class MaxDoubleExtension : MarkupExtension
+{
+    /// <inheritdoc/>
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return double.MaxValue;
+    }
+}
