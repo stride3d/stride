41420552e69611d57bc889fb95ea5996fca2bd62
Nicolas Musset <nicolas@color-rise.xyz>
2024-01-04 17:44:36 +0100

    [Presentation] Re-implement ObservableList and ObservableSet
    To be compatible with both Avalonia and WPF in all cases

---
+++ sources/presentation/Stride.Core.Presentation/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Stride.Core.Presentation.Tests")]
