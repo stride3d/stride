bd6cef5405755288f5e69080f9bc731a6c155f28
Nicolas Musset <nicolas@color-rise.xyz>
2024-01-05 14:50:55 +0100

    [Launcher] Self-update

--- sources/xplat-editor/Stride.Launcher/App.axaml.cs
+++ sources/xplat-editor/Stride.Launcher/App.axaml.cs
@@ -16,6 +16,8 @@ public partial class App : Application
 {
     internal readonly CancellationTokenSource cts = new();
 
+    internal MainWindow? MainWindow { get; private set; }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -29,7 +31,7 @@ public partial class App : Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
+            desktop.MainWindow = MainWindow = new MainWindow
             {
                 DataContext = InitializeMainViewModel()
             };
