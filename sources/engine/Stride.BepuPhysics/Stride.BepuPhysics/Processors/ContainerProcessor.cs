a3b7c1bb1df31ee3f8b22eb7e36e79e181c7153b
Eideren <contact@eideren.com>
2024-01-11 15:54:50 +0100

    [Statics] Start work on rebuilding statics after move

--- sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Processors/ContainerProcessor.cs
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Processors/ContainerProcessor.cs
@@ -1,10 +1,14 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Stride.BepuPhysics.Components;
 using Stride.BepuPhysics.Components.Containers;
 using Stride.BepuPhysics.Configurations;
 using Stride.Core.Annotations;
 using Stride.Engine;
 using Stride.Games;
+using Stride.Rendering;
+using Stride.Core.Mathematics;
+using System.Numerics;
 
 namespace Stride.BepuPhysics.Processors
 {
@@ -12,6 +16,8 @@ namespace Stride.BepuPhysics.Processors
     {
         private IGame? _game = null;
         private BepuConfiguration? _bepuConfiguration = default;
+
+        private readonly UnsortedO1List<StaticContainerComponent, Matrix4x4> _statics = new();
 
         internal Dictionary<ContainerComponent, ContainerComponent>.Enumerator ComponentDatas => base.ComponentDatas.GetEnumerator();
 
@@ -21,6 +27,23 @@ namespace Stride.BepuPhysics.Processors
         public ContainerProcessor()
         {
             Order = BepuOrderHelper.ORDER_OF_CONTAINER_P;
+        }
+
+        public override void Draw(RenderContext context) // While this is not related to drawing, we're doing this in draw as it runs after the TransformProcessor updates WorldMatrix
+        {
+            base.Draw(context);
+
+            #warning should be changed to dispatcher's ForBatch from master when it releases
+            var span = _statics.UnsafeGetSpan();
+            for (int i = 0; i < span.Length; i++)
+            {
+                ref Matrix4x4 entityMatrix = ref Unsafe.As<Matrix, Matrix4x4>(ref span[i].Key.Entity.Transform.WorldMatrix);
+                if (span[i].Value != entityMatrix)
+                {
+                    span[i].Value = entityMatrix;
+                    #warning REBUILD/MOVE STATIC CONTAINER
+                }
+            }
         }
 
         protected override void OnSystemAdd()
@@ -44,12 +67,18 @@ namespace Stride.BepuPhysics.Processors
             if (component is ISimulationUpdate simulationUpdate)
                 component.ContainerData.BepuSimulation.Register(simulationUpdate);
 
+            if (component is StaticContainerComponent staticContainer)
+                _statics.Add(staticContainer, Unsafe.As<Matrix, Matrix4x4>(ref staticContainer.Entity.Transform.WorldMatrix));
+
             OnPostAdd?.Invoke(component);
         }
 
         protected override void OnEntityComponentRemoved(Entity entity, [NotNull] ContainerComponent component, [NotNull] ContainerComponent data)
         {
             OnPreRemove?.Invoke(component);
+
+            if (component is StaticContainerComponent staticContainer)
+                _statics.Remove(staticContainer);
 
             Debug.Assert(component.ContainerData is not null);
 
