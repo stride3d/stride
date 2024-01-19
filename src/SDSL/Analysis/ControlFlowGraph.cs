9723c72afe90a2760aaf3dc40445e2566969c0f1
Youness KAFIA <kafia.youness@gmail.com>
2024-01-19 16:49:23 +0100

    Control flow new design

--- src/SDSL/Analysis/ControlFlowGraph.cs
+++ src/SDSL/Analysis/ControlFlowGraph.cs
@@ -4,10 +4,14 @@ using SoftTouch.Spirv.Core.Buffers;
 namespace SDSL.Analysis;
 
 
-public class CodeNode
+public record struct CFEdge(Expression Condition, CFNode Destination);
+
+public class CFNode()
 {
     public List<Statement> Code = [];
+    public List<CFEdge> Outputs = [];
 }
-public class ControlFlowGraph
+public class ControlFlowGraph()
 {
+    public Dictionary<ShaderMethod, ControlFlowGraph> Graphs = [];
 }
