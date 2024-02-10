7730f3e874344cbecc443bf69e53cff477dfe4f5
Eideren <contact@eideren.com>
2024-02-10 22:16:45 +0100

    [Bepu] Double CollisionMask entries

--- sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/CollisionMask.cs
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/CollisionMask.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 using BepuPhysics.Collidables;
 
 namespace Stride.BepuPhysics;
@@ -16,20 +16,28 @@ namespace Stride.BepuPhysics;
 /// Collision occurs when mask 'a' contains all the layers set in 'b' or 'b' contains all the layers of 'a'.
 /// </summary>
 [Flags]
-public enum CollisionMask : byte
+public enum CollisionMask : ushort
 {
     /// <summary> No contact generated nor collisions reported between any object and this one </summary>
-    NoCollision = 0b0000_0000,
-    Layer0      = 0b0000_0001,
-    Layer1      = 0b0000_0010,
-    Layer2      = 0b0000_0100,
-    Layer3      = 0b0000_1000,
-    Layer4      = 0b0001_0000,
-    Layer5      = 0b0010_0000,
-    Layer6      = 0b0100_0000,
-    Layer7      = 0b1000_0000,
+    NoCollision = 0b0000_0000__0000_0000,
+    Layer0      = 0b0000_0000__0000_0001,
+    Layer1      = 0b0000_0000__0000_0010,
+    Layer2      = 0b0000_0000__0000_0100,
+    Layer3      = 0b0000_0000__0000_1000,
+    Layer4      = 0b0000_0000__0001_0000,
+    Layer5      = 0b0000_0000__0010_0000,
+    Layer6      = 0b0000_0000__0100_0000,
+    Layer7      = 0b0000_0000__1000_0000,
+    Layer8      = 0b0000_0001__0000_0000,
+    Layer9      = 0b0000_0010__0000_0000,
+    Layer10     = 0b0000_0100__0000_0000,
+    Layer11     = 0b0000_1000__0000_0000,
+    Layer12     = 0b0001_0000__0000_0000,
+    Layer13     = 0b0010_0000__0000_0000,
+    Layer14     = 0b0100_0000__0000_0000,
+    Layer15     = 0b1000_0000__0000_0000,
     /// <summary> Collisions will occur between this one and any other colliders unless they have their mask set to <see cref="NoCollision"/> </summary>
-    Everything  = 0b1111_1111,
+    Everything  = 0b1111_1111__1111_1111,
 }
 
 public static class CollisionMaskExtension
@@ -43,6 +51,7 @@ public static class CollisionMaskExtension
         CollisionMask and = a & b;
         return and != 0 && (and == a || and == b);
     }
+
     /// <summary>
     /// Whether a test with this mask against this collidable should be performed or ignored
     /// </summary>
