5f89b917615e612a1bc656705a97a8d45b6fef83
jklawreszuk <jk.lawreszuk@gmail.com>
2024-03-14 14:16:00 +0100

    Update samples/template to top-level statements

--- samples/Input/TouchInputs/TouchInputs.Windows/TouchInputsApp.cs
+++ samples/Input/TouchInputs/TouchInputs.Windows/TouchInputsApp.cs
@@ -1,15 +1,4 @@
 using Stride.Engine;
 
-namespace TouchInputs
-{
-    class TouchInputsApp
-    {
-        static void Main(string[] args)
-        {
-            using (var game = new Game())
-            {
-                game.Run();
-            }
-        }
-    }
-}
+using var game = new Game();
+game.Run();
