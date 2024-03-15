5c847ee852c999625ab2d7e4c7bf3633ec31ab6e
Jakub Ławreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2024-03-15 14:33:34 +0100

    feat: Update samples/template to top-level statements (#2187)

--- samples/Templates/ThirdPersonPlatformer/ThirdPersonPlatformer/ThirdPersonPlatformer.Windows/ThirdPersonPlatformerApp.cs
+++ samples/Templates/ThirdPersonPlatformer/ThirdPersonPlatformer/ThirdPersonPlatformer.Windows/ThirdPersonPlatformerApp.cs
@@ -1,15 +1,5 @@
 using Stride.Engine;
 
-namespace ThirdPersonPlatformer
-{
-    class ThirdPersonPlatformerApp
-    {
-        static void Main(string[] args)
-        {
-            using (var game = new Game())
-            {
-                game.Run();
-            }
-        }
-    }
-}
+using var game = new Game();
+game.Run();
+
