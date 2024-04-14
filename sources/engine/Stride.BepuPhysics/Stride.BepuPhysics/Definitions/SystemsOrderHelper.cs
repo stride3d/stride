cbce81a55cc8627b45a5a78c9c318f18d9497b36
Eideren <contact@eideren.com>
2024-04-14 09:51:49 +0200

    [Bepu] Move simulation to run at the end of update to reduce latency

--- sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Definitions/SystemsOrderHelper.cs
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Definitions/SystemsOrderHelper.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Stride.Engine.Processors;
+
 namespace Stride.BepuPhysics.Definitions;
 
 internal static class SystemsOrderHelper
@@ -8,8 +10,12 @@ internal static class SystemsOrderHelper
 
     //Note : transform processor's Draw() is at -200;
 
-    public const int ORDER_OF_CONSTRAINT_P = -900; //handle the creation of bepu constraints
-    public const int ORDER_OF_GAME_SYSTEM = -810; //Handle the simulation Step(dt) + Transform update
-    public const int ORDER_OF_DEBUG_P = 1000; // Handles drawing debug wireframe, transform processor's Draw operates at -200, as long as we're after it we're working optimally
-    public const int ORDER_OF_COLLIDABLE_P = -100; // Handles the creation of bepu objects and rebuilds static meshes when they are moved, needs to occur after TransformProcessor's draw
+    /// <summary> Creation and management of constraints </summary>
+    public const int ORDER_OF_CONSTRAINT_P = -900;
+    /// <summary> Simulation Step(dt) & Transform Update() </summary>
+    public const int ORDER_OF_GAME_SYSTEM = int.MaxValue;
+    /// <summary> Drawing debug wireframe, must occur after <see cref="TransformProcessor.Draw"/> </summary>
+    public const int ORDER_OF_DEBUG_P = 1000;
+    /// <summary> Creation and management of collidable, rebuilds static meshes when they are moved, must occur after <see cref="TransformProcessor.Draw"/> </summary>
+    public const int ORDER_OF_COLLIDABLE_P = -100;
 }
