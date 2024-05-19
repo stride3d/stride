f4900aa82899636d19c52fba00b2c5b0234bd080
jklawreszuk <jk.lawreszuk@gmail.com>
2024-05-19 15:28:59 +0200

    Remove Stride.Metrics

--- sources/editor/Stride.GameStudio/Program.cs
+++ sources/editor/Stride.GameStudio/Program.cs
@@ -36,7 +36,6 @@ using Stride.Editor.Engine;
 using Stride.Editor.Preview;
 using Stride.GameStudio.View;
 using Stride.Graphics;
-using Stride.Metrics;
 using Stride.PrivacyPolicy;
 using EditorSettings = Stride.Core.Assets.Editor.Settings.EditorSettings;
 using MessageBox = System.Windows.MessageBox;
@@ -82,78 +81,66 @@ public static class Program
         EditorSettings.Initialize();
         Thread.CurrentThread.Name = "Main thread";
 
-        // Install Metrics for the editor
-        using (StrideGameStudio.MetricsClient = EditorSettings.EnableMetrics.GetValue() ? new MetricsClient(CommonApps.StrideEditorAppId) : null)
+        try
         {
-            try
+            // Environment.GetCommandLineArgs correctly process arguments regarding the presence of '\' and '"'
+            var args = Environment.GetCommandLineArgs().Skip(1).ToList();
+            var startupSessionPath = StrideEditorSettings.StartupSession.GetValue();
+            var lastSessionPath = EditorSettings.ReloadLastSession.GetValue() ? mru.MostRecentlyUsedFiles.FirstOrDefault() : null;
+            var initialSessionPath = !UPath.IsNullOrEmpty(startupSessionPath) ? startupSessionPath : lastSessionPath?.FilePath;
+
+            // Handle arguments
+            for (var i = 0; i < args.Count; i++)
             {
-                // Environment.GetCommandLineArgs correctly process arguments regarding the presence of '\' and '"'
-                var args = Environment.GetCommandLineArgs().Skip(1).ToList();
-                var startupSessionPath = StrideEditorSettings.StartupSession.GetValue();
-                var lastSessionPath = EditorSettings.ReloadLastSession.GetValue() ? mru.MostRecentlyUsedFiles.FirstOrDefault() : null;
-                var initialSessionPath = !UPath.IsNullOrEmpty(startupSessionPath) ? startupSessionPath : lastSessionPath?.FilePath;
-
-                // Handle arguments
-                for (var i = 0; i < args.Count; i++)
+                if (args[i] == "/LauncherWindowHandle")
                 {
-                    if (args[i] == "/LauncherWindowHandle")
-                    {
-                        windowHandle = new IntPtr(long.Parse(args[++i]));
-                    }
-                    else if (args[i] == "/NewProject")
-                    {
-                        initialSessionPath = null;
-                    }
-                    else if (args[i] == "/DebugEditorGraphics")
-                    {
-                        EmbeddedGame.DebugMode = true;
-                    }
-                    else if (args[i] == "/RenderDoc")
-                    {
-                        // TODO: RenderDoc is not working here (when not in debug)
-                        GameStudioPreviewService.DisablePreview = true;
-                        renderDocManager = new RenderDocManager();
-                        renderDocManager.Initialize();
-                    }
-                    else if (args[i] == "/RecordEffects")
-                    {
-                        GameStudioBuilderService.GlobalEffectLogPath = args[++i];
-                    }
-                    else
-                    {
-                        initialSessionPath = args[i];
-                    }
+                    windowHandle = new IntPtr(long.Parse(args[++i]));
                 }
-                RuntimeHelpers.RunModuleConstructor(typeof(Asset).Module.ModuleHandle);
-
-                //listen to logger for crash report
-                GlobalLogger.GlobalMessageLogged += GlobalLoggerOnGlobalMessageLogged;
-
-                mainDispatcher = Dispatcher.CurrentDispatcher;
-                mainDispatcher.InvokeAsync(() => Startup(initialSessionPath));
-
-                using (new WindowManager(mainDispatcher))
+                else if (args[i] == "/NewProject")
                 {
-                    app = new App { ShutdownMode = ShutdownMode.OnExplicitShutdown };
-                    app.Activated += (sender, eventArgs) =>
-                    {
-                        StrideGameStudio.MetricsClient?.SetActiveState(true);
-                    };
-                    app.Deactivated += (sender, eventArgs) =>
-                    {
-                        StrideGameStudio.MetricsClient?.SetActiveState(false);
-                    };
-
-                    app.InitializeComponent();
-                    app.Run();
+                    initialSessionPath = null;
                 }
-
-                renderDocManager?.RemoveHooks();
+                else if (args[i] == "/DebugEditorGraphics")
+                {
+                    EmbeddedGame.DebugMode = true;
+                }
+                else if (args[i] == "/RenderDoc")
+                {
+                    // TODO: RenderDoc is not working here (when not in debug)
+                    GameStudioPreviewService.DisablePreview = true;
+                    renderDocManager = new RenderDocManager();
+                    renderDocManager.Initialize();
+                }
+                else if (args[i] == "/RecordEffects")
+                {
+                    GameStudioBuilderService.GlobalEffectLogPath = args[++i];
+                }
+                else
+                {
+                    initialSessionPath = args[i];
+                }
             }
-            catch (Exception e)
+            RuntimeHelpers.RunModuleConstructor(typeof(Asset).Module.ModuleHandle);
+
+            //listen to logger for crash report
+            GlobalLogger.GlobalMessageLogged += GlobalLoggerOnGlobalMessageLogged;
+
+            mainDispatcher = Dispatcher.CurrentDispatcher;
+            mainDispatcher.InvokeAsync(() => Startup(initialSessionPath));
+
+            using (new WindowManager(mainDispatcher))
             {
-                HandleException(e, 0);
+                app = new App { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+
+                app.InitializeComponent();
+                app.Run();
             }
+
+            renderDocManager?.RemoveHooks();
+        }
+        catch (Exception e)
+        {
+            HandleException(e, 0);
         }
     }
 
