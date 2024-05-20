00d696669f469529ce51f01992b730307d5f0d73
jklawreszuk <jk.lawreszuk@gmail.com>
2024-05-20 18:31:26 +0200

    Modernize CrashReport

--- sources/editor/Stride.Editor.CrashReport/CrashReporter.cs
+++ sources/editor/Stride.Editor.CrashReport/CrashReporter.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using System.Diagnostics;
 
-namespace Stride.Editor.CrashReport
+namespace Stride.Editor.CrashReport;
+
+internal static class CrashReporter
 {
-    public static class CrashReporter
+    internal static void OpenGithub()
     {
-        ///Todo: We could send report as issue to Github repo
-        public static Task Report(CrashReportData data)
-        {
-            return Task.CompletedTask;
-        }
+        string url = "https://github.com/stride3d/stride/issues/new?labels=bug&template=bug_report.md&";
+
+        Process browser = new();
+        browser.StartInfo.UseShellExecute = true; 
+        browser.StartInfo.FileName = url;
+        browser.Start();
     }
 }
