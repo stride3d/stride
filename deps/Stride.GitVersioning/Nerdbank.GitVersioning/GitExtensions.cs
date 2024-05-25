a45bc49e9e2168e13f6dfcd5ecaef14270cf22de
Eideren <contact@eideren.com>
2024-05-25 13:26:04 +0200

    [Build] Rollback OperatingSystem.IsXYZ for net472 project (#2277)

--- deps/Stride.GitVersioning/Nerdbank.GitVersioning/GitExtensions.cs
+++ deps/Stride.GitVersioning/Nerdbank.GitVersioning/GitExtensions.cs
@@ -343,15 +343,15 @@ namespace Nerdbank.GitVersioning
         /// <returns>Receives the directory that native binaries are expected.</returns>
         public static string FindLibGit2NativeBinaries(string basePath)
         {
-            if (OperatingSystem.IsWindows())
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 return Path.Combine(basePath, "lib", "win32", IntPtr.Size == 4 ? "x86" : "x64");
             }
-            if (OperatingSystem.IsLinux())
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 return Path.Combine(basePath, "lib", "linux", IntPtr.Size == 4 ? "x86" : "x86_64");
             }
-            if (OperatingSystem.IsMacOS())
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 return Path.Combine(basePath, "lib", "osx");
             }
