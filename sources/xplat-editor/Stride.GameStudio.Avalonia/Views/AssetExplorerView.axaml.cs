Color-Rise/stride-xplat-editor

[Editor] Add placeholders

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/AssetExplorerView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class AssetExplorerView : UserControl
+{
+    public AssetExplorerView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/AssetPreviewView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class AssetPreviewView : UserControl
+{
+    public AssetPreviewView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/MainEditorView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class MainEditorView : UserControl
+{
+    public MainEditorView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/PropertyGridView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class PropertyGridView : UserControl
+{
+    public PropertyGridView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/SolutionExplorerView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class SolutionExplorerView : UserControl
+{
+    public SolutionExplorerView()
+    {
+        InitializeComponent();
+    }
+}

stride3d/stride

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/AssetExplorerView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class AssetExplorerView : UserControl
+{
+    public AssetExplorerView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/AssetPreviewView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class AssetPreviewView : UserControl
+{
+    public AssetPreviewView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/MainEditorView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class MainEditorView : UserControl
+{
+    public MainEditorView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/PropertyGridView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class PropertyGridView : UserControl
+{
+    public PropertyGridView()
+    {
+        InitializeComponent();
+    }
+}

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/SolutionExplorerView.axaml.cs
@@ -1,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class SolutionExplorerView : UserControl
+{
+    public SolutionExplorerView()
+    {
+        InitializeComponent();
+    }
+}
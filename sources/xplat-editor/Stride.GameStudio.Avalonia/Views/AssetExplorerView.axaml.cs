7fded32113c56ed88462795a369dc191a04b07d7
Nicolas Musset <nicolas@color-rise.xyz>
2023-10-09 20:59:53 +0200

    [Editor] Add placeholders

---
+++ sources/xplat-editor/Stride.GameStudio.Avalonia/Views/AssetExplorerView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class AssetExplorerView : UserControl
+{
+    public AssetExplorerView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ sources/xplat-editor/Stride.GameStudio.Avalonia/Views/AssetPreviewView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class AssetPreviewView : UserControl
+{
+    public AssetPreviewView()
+    {
+        InitializeComponent();
+    }
+}
