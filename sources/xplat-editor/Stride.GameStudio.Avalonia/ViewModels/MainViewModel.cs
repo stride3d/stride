Color-Rise/stride-xplat-editor

[Editor] Add basic UI

sources/xplat-editor/Stride.Avalonia.GameStudio/ViewModels/MainViewModel.cs
@@ -1,6 +1,47 @@
-﻿namespace Stride.Avalonia.GameStudio.ViewModels;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+using Stride.Avalonia.GameStudio.Views;
+
+namespace Stride.Avalonia.GameStudio.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string? message;
+
+    public MainViewModel()
+    {
+        AboutCommand = new AsyncRelayCommand(OnAbout, () => Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime);
+        ExitCommand = new RelayCommand(OnExit, () => Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime);
+        OpenCommand = new RelayCommand(OnOpen);
+    }
+
+    public string? Message
+    {
+        get => message;
+        set => SetProperty(ref message, value);
+    }
+
+    public ICommand AboutCommand { get; }
+    public ICommand ExitCommand { get; }
+    public ICommand OpenCommand { get; }
+
+    private async Task OnAbout()
+    {
+        // FIXME: hide implementation details through a dialog service
+        var window = new AboutWindow();
+        await window.ShowDialog(((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!).MainWindow!);
+    }
+
+    private void OnExit()
+    {
+        ((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!).TryShutdown();
+    }
+
+    private void OnOpen()
+    {
+        Message = "Clicked on Open";
+    }
 }

sources/xplat-editor/Stride.Avalonia.GameStudio/Views/AboutWindow.axaml.cs
@@ -1,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stride.Avalonia.GameStudio.Views;
+
+public partial class AboutWindow : Window
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+    }
+}

stride3d/stride

sources/xplat-editor/Stride.GameStudio.Avalonia/ViewModels/MainViewModel.cs
@@ -1,6 +1,47 @@
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.Input;
+using Stride.GameStudio.Avalonia.Views;
+
 namespace Stride.GameStudio.Avalonia.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string? message;
+
+    public MainViewModel()
+    {
+        AboutCommand = new AsyncRelayCommand(OnAbout, () => Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime);
+        ExitCommand = new RelayCommand(OnExit, () => Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime);
+        OpenCommand = new RelayCommand(OnOpen);
+    }
+
+    public string? Message
+    {
+        get => message;
+        set => SetProperty(ref message, value);
+    }
+
+    public ICommand AboutCommand { get; }
+    public ICommand ExitCommand { get; }
+    public ICommand OpenCommand { get; }
+
+    private async Task OnAbout()
+    {
+        // FIXME: hide implementation details through a dialog service
+        var window = new AboutWindow();
+        await window.ShowDialog(((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!).MainWindow!);
+    }
+
+    private void OnExit()
+    {
+        ((IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!).TryShutdown();
+    }
+
+    private void OnOpen()
+    {
+        Message = "Clicked on Open";
+    }
 }

sources/xplat-editor/Stride.GameStudio.Avalonia/Views/AboutWindow.axaml.cs
@@ -1,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stride.GameStudio.Avalonia.Views;
+
+public partial class AboutWindow : Window
+{
+    public AboutWindow()
+    {
+        InitializeComponent();
+    }
+}