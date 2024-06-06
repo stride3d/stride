e550c60a75f19b9a02fb0fec4c126cfa70d98d6a
Nicolae Tugui <NicusorN5@proton.me>
2024-06-06 17:26:15 +0300

    Fixed a NullReferenceException if printing null text
    Just added a null check in the Print() method.

--- sources/engine/Stride.Engine/Profiling/DebugTextSystem.cs
+++ sources/engine/Stride.Engine/Profiling/DebugTextSystem.cs
@@ -43,7 +43,7 @@ namespace Stride.Profiling
         {
             var msg = new DebugOverlayMessage 
             { 
-                Message = message,
+                Message = message ?? "",
                 Position = position,
                 TextColor = color ?? TextColor,
                 RemainingTime = timeOnScreen ?? DefaultOnScreenTime,
