2a3b26b9f1278ca8576806452d217f944f299cdd
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2024-07-14 19:58:45 +0200

    Loading grammar

--- src/Stride.Shaders.AvaloniaViewer/MainWindow.axaml.cs
+++ src/Stride.Shaders.AvaloniaViewer/MainWindow.axaml.cs
@@ -1,4 +1,12 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using AvaloniaEdit;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
+using TextMateSharp.Internal.Grammars;
+using TextMateSharp.Internal.Types;
+using TextMateSharp.Registry;
+using TextMateSharp.Themes;
 
 namespace Stride.Shaders.AvaloniaViewer;
 
@@ -7,5 +15,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        var editor = this.FindControl<TextEditor>("ShaderEditor");
+        var registry = new Registry(new RegistryOptions(ThemeName.Dark));
+        var grammar = registry.LoadGrammarFromPathSync("./sdsl.tmLanguage.json", 0, []);
+
     }
 }
