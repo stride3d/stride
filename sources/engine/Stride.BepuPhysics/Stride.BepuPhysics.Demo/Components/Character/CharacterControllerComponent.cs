a68a197361ecd0ce07d68b429c8651031889396b
Eideren <contact@eideren.com>
2024-10-17 15:07:02 +0200

    Improve camera controls for demo character controller

--- sources/engine/Stride.BepuPhysics/Stride.BepuPhysics.Demo/Components/Character/CharacterControllerComponent.cs
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics.Demo/Components/Character/CharacterControllerComponent.cs
@@ -1,65 +1,60 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Collections.Generic;
 using Stride.Core;
 using Stride.Core.Mathematics;
 using Stride.Engine;
+using Stride.Engine.FlexibleProcessing;
+using Stride.Games;
 using Stride.Input;
 
 
 namespace Stride.BepuPhysics.Demo.Components.Character;
 [ComponentCategory("BepuDemo - Character")]
-public class CharacterControllerComponent : SyncScript
+public class CharacterControllerComponent : CharacterComponent, IComponent<CharacterControllerComponent.UpdateCaller, CharacterControllerComponent>
 {
     public Entity? CameraPivot { get; set; }
-    public required Entity CharacterEntity { get; set; }
-    [DataMemberIgnore]
-    public required CharacterComponent Character { get; set; }
-
     public float MinCameraAngle { get; set; } = -90;
     public float MaxCameraAngle { get; set; } = 90;
 
-    private Vector3 _cameraDirection;
+    private Vector3 _cameraAngle;
+    private UpdateCaller _processor = null!;
 
-    public override void Start()
+    private void Update()
     {
-        Character = CharacterEntity.Get<CharacterComponent>();
-        Input.LockMousePosition(true);
-        Game.IsMouseVisible = false;
-
-        MaxCameraAngle = MathUtil.DegreesToRadians(MaxCameraAngle);
-        MinCameraAngle = MathUtil.DegreesToRadians(MinCameraAngle);
-    }
-
-    public override void Update()
-    {
-        if (Input.IsKeyPressed(Keys.Tab))
+        if (_processor.Inputs.IsKeyPressed(Keys.Tab))
         {
-            Game.IsMouseVisible = !Game.IsMouseVisible;
-            if (Game.IsMouseVisible)
-                Input.UnlockMousePosition();
+            _processor.Game.IsMouseVisible = !_processor.Game.IsMouseVisible;
+            if (_processor.Game.IsMouseVisible)
+                _processor.Inputs.UnlockMousePosition();
             else
-                Input.LockMousePosition(true);
+                _processor.Inputs.LockMousePosition(true);
         }
 
         Move();
         Rotate();
+        if (_processor.Inputs.IsKeyPressed(Keys.Space))
+            TryJump();
+    }
 
-        if (Input.IsKeyPressed(Keys.Space))
-            Character.TryJump();
+    public override void SimulationUpdate(float simTimeStep)
+    {
+        Orientation = Quaternion.RotationY(_cameraAngle.Y); // Do it before physics tick to ensure it is interpolated properly
+        base.SimulationUpdate(simTimeStep);
     }
 
     private void Move()
     {
         // Keyboard movement
         var moveDirection = Vector2.Zero;
-        if (Input.IsKeyDown(Keys.W) || Input.IsKeyDown(Keys.Z))
+        if (_processor.Inputs.IsKeyDown(Keys.W) || _processor.Inputs.IsKeyDown(Keys.Z))
             moveDirection.Y += 1;
-        if (Input.IsKeyDown(Keys.S))
+        if (_processor.Inputs.IsKeyDown(Keys.S))
             moveDirection.Y -= 1;
-        if (Input.IsKeyDown(Keys.A) || Input.IsKeyDown(Keys.Q))
+        if (_processor.Inputs.IsKeyDown(Keys.A) || _processor.Inputs.IsKeyDown(Keys.Q))
             moveDirection.X -= 1;
-        if (Input.IsKeyDown(Keys.D))
+        if (_processor.Inputs.IsKeyDown(Keys.D))
             moveDirection.X += 1;
 
         var velocity = new Vector3(moveDirection.X, 0, -moveDirection.Y);
@@ -67,22 +62,61 @@ public class CharacterControllerComponent : SyncScript
 
         velocity = Vector3.Transform(velocity, Entity.Transform.Rotation);
 
-        if (Input.IsKeyDown(Keys.LeftShift))
+        if (_processor.Inputs.IsKeyDown(Keys.LeftShift))
             velocity *= 2f;
 
-        Character.Move(velocity);
+        Move(velocity);
     }
 
     private void Rotate()
     {
-        var delta = Input.Mouse.Delta;
+        var delta = _processor.Inputs.Mouse.Delta;
 
-        _cameraDirection.X -= delta.Y;
-        _cameraDirection.Y -= delta.X;
-        _cameraDirection.X = MathUtil.Clamp(_cameraDirection.X, MinCameraAngle, MaxCameraAngle);
+        _cameraAngle.X -= delta.Y;
+        _cameraAngle.Y -= delta.X;
+        _cameraAngle.X = MathUtil.Clamp(_cameraAngle.X, MinCameraAngle, MaxCameraAngle);
 
-        Character.Orientation = Quaternion.RotationY(_cameraDirection.Y);
+        Entity.Transform.Rotation = Quaternion.RotationY(_cameraAngle.Y);
         if (CameraPivot != null)
-            CameraPivot.Transform.Rotation = Quaternion.RotationX(_cameraDirection.X);
+        {
+            CameraPivot.Transform.Rotation = Quaternion.RotationX(_cameraAngle.X);
+        }
+    }
+
+    private class UpdateCaller : IComponent<UpdateCaller,CharacterControllerComponent>.IProcessor, IUpdateProcessor
+    {
+        private List<CharacterControllerComponent> Components = new();
+
+        public InputManager Inputs;
+        public IGame Game;
+
+        public int Order { get; }
+
+        public void SystemAdded(IServiceRegistry registryParam)
+        {
+            Inputs = registryParam.GetService<InputManager>();
+            Game = registryParam.GetService<IGame>();
+            Inputs.LockMousePosition(true);
+            Game.IsMouseVisible = false;
+        }
+        public void SystemRemoved() { }
+
+        public void OnComponentAdded(CharacterControllerComponent item)
+        {
+            Components.Add(item);
+            item._processor = this;
+        }
+
+        public void OnComponentRemoved(CharacterControllerComponent item)
+        {
+            Components.Remove(item);
+            item._processor = null!;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var comp in Components)
+                comp.Update();
+        }
     }
 }
