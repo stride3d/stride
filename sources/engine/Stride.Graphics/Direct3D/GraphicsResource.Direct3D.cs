6a200fe3966ae0486e7cd99ffc48d1936f1c763b
Mario Guerra <mgg7007@gmail.com>
2024-11-11 21:39:01 +0100

    Reimplement object lifetimes to fix orphan objects (GraphicsResource)
    * Expands and enhances documentation
    * Minor cleanups

--- sources/engine/Stride.Graphics/Direct3D/GraphicsResource.Direct3D.cs
+++ sources/engine/Stride.Graphics/Direct3D/GraphicsResource.Direct3D.cs
@@ -3,90 +3,109 @@
 
 #if STRIDE_GRAPHICS_API_DIRECT3D11
 
+using Silk.NET.Core.Native;
 using Silk.NET.Direct3D11;
-
-using static Stride.Graphics.DebugHelpers;
+using Stride.Core;
 
 namespace Stride.Graphics
 {
-    /// <summary>
-    /// GraphicsResource class
-    /// </summary>
     public abstract unsafe partial class GraphicsResource
     {
-        private ID3D11ShaderResourceView* shaderResourceView;
-        private ID3D11UnorderedAccessView* unorderedAccessView;
+        private ComPtr<ID3D11ShaderResourceView> shaderResourceView;
+        private ComPtr<ID3D11UnorderedAccessView> unorderedAccessView;
 
-        // Used to internally force a WriteDiscard (to force a rename) with the GraphicsResourceAllocator
+        /// <summary>
+        ///   Used to internally force a <c>WriteDiscard</c> (to force a rename) with the <see cref="GraphicsResourceAllocator"/>.
+        /// </summary>
         internal bool DiscardNextMap;
 
+        /// <summary>
+        ///   Gets a value indicating whether this graphics resource is in "Debug mode".
+        /// </summary>
+        /// <value>
+        ///   <see langword="true"/> if this graphics resource is initialized in "Debug mode"; otherwise, <see langword="false"/>.
+        /// </value>
         protected bool IsDebugMode => GraphicsDevice?.IsDebugMode == true;
 
+        /// <inheritdoc/>
         protected override void OnNameChanged()
         {
             base.OnNameChanged();
 
             if (IsDebugMode)
             {
-                if (shaderResourceView != null)
+                if (shaderResourceView.Handle != null)
                 {
-                    SetDebugName((ID3D11DeviceChild*) shaderResourceView, Name is null ? null : $"{Name} SRV");
+                    using var srv = shaderResourceView.QueryInterface<ID3D11DeviceChild>();
+                    srv.SetDebugName(Name is null ? null : $"{Name} SRV", owningObject: this);
                 }
-                if (unorderedAccessView != null)
+                if (unorderedAccessView.Handle != null)
                 {
-                    SetDebugName((ID3D11DeviceChild*) unorderedAccessView, Name is null ? null : $"{Name} UAV");
+                    using var uav = unorderedAccessView.QueryInterface<ID3D11DeviceChild>();
+                    uav.SetDebugName(Name is null ? null : $"{Name} UAV", owningObject: this);
                 }
             }
         }
 
         /// <summary>
-        /// Gets or sets the ShaderResourceView attached to this GraphicsResource.
-        /// Note that only Texture, Texture3D, RenderTarget2D, RenderTarget3D, DepthStencil are using this ShaderResourceView
+        ///   Gets or sets the <see cref="ID3D11ShaderResourceView"/> attached to this <see cref="GraphicsResource"/>.
         /// </summary>
-        /// <value>The device child.</value>
-        protected internal ID3D11ShaderResourceView* NativeShaderResourceView
+        /// <value>The Shader Resource View associated with this graphics resource.</value>
+        /// <remarks>
+        ///   Only <see cref="Texture"/>s are using this Shader Resource View.
+        /// </remarks>
+        protected internal ComPtr<ID3D11ShaderResourceView> NativeShaderResourceView
         {
             get => shaderResourceView;
-
             set
             {
+                var previousShaderResourceView = shaderResourceView;
+
                 shaderResourceView = value;
 
-                if (IsDebugMode && shaderResourceView != null)
+                if (shaderResourceView.Handle != previousShaderResourceView.Handle)
                 {
-                    SetDebugName((ID3D11DeviceChild*)shaderResourceView, Name is null ? null : $"{Name} SRV");
+                    previousShaderResourceView.RemoveDisposeBy(this);
+                    previousShaderResourceView.Release();
+
+                    shaderResourceView.DisposeBy(this);
+                }
+
+                if (IsDebugMode && shaderResourceView.Handle != null)
+                {
+                    using var srv = shaderResourceView.QueryInterface<ID3D11DeviceChild>();
+                    srv.SetDebugName(Name is null ? null : $"{Name} SRV", owningObject: this);
                 }
             }
         }
 
         /// <summary>
-        /// Gets or sets the UnorderedAccessView attached to this GraphicsResource.
+        ///   Gets or sets the <see cref="ID3D11UnorderedAccessView"/> attached to this <see cref="GraphicsResource"/>.
         /// </summary>
-        /// <value>The device child.</value>
+        /// <value>The Unordered Access View associated with this graphics resource.</value>
         protected internal ID3D11UnorderedAccessView* NativeUnorderedAccessView
         {
             get => unorderedAccessView;
-
             set
             {
+                var previousUnorderedAccessView = unorderedAccessView;
+
                 unorderedAccessView = value;
 
-                if (IsDebugMode && unorderedAccessView != null)
+                if (unorderedAccessView.Handle != previousUnorderedAccessView.Handle)
                 {
-                    SetDebugName((ID3D11DeviceChild*)unorderedAccessView, Name is null ? null : $"{Name} UAV");
+                    previousUnorderedAccessView.RemoveDisposeBy(this);
+                    previousUnorderedAccessView.Release();
+
+                    unorderedAccessView.DisposeBy(this);
+                }
+
+                if (IsDebugMode && unorderedAccessView.Handle != null)
+                {
+                    using var uav = unorderedAccessView.QueryInterface<ID3D11DeviceChild>();
+                    uav.SetDebugName(Name is null ? null : $"{Name} UAV", owningObject: this);
                 }
             }
-        }
-
-        protected internal override void OnDestroyed()
-        {
-            if (shaderResourceView != null)
-                shaderResourceView->Release();
-
-            if (unorderedAccessView != null)
-                unorderedAccessView->Release();
-
-            base.OnDestroyed();
         }
     }
 }
