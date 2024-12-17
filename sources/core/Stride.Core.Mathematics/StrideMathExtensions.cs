66e72c6f967629de54a6a6d2233a50f11efe9637
Doprez <73259914+Doprez@users.noreply.github.com>
2024-12-16 19:11:34 -0700

    forgot that in extensions are simple...

--- sources/core/Stride.Core.Mathematics/StrideMathExtensions.cs
+++ sources/core/Stride.Core.Mathematics/StrideMathExtensions.cs
@@ -12,15 +12,6 @@ namespace Stride.Core.Mathematics;
 /// </summary>
 public static class StrideMathExtensions
 {
-    //NOTE: The "in" extensions are explained here: https://github.com/stride3d/stride/pull/2238#issuecomment-2109071498
-    //The non "in" versions are for user convenience.
-
-    /// <summary>
-    /// Converts a System.Numerics.Vector2 to a Stride.Core.Mathematics.Vector2.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
-    public static Vector2 ToStride(this NVector2 v) => Unsafe.BitCast<NVector2, Vector2>(v);
     /// <summary>
     /// Converts a System.Numerics.Vector2 to a Stride.Core.Mathematics.Vector2.
     /// </summary>
@@ -32,31 +23,13 @@ public static class StrideMathExtensions
     /// </summary>
     /// <param name="v"></param>
     /// <returns></returns>
-    public static Vector3 ToStride(this NVector3 v) => Unsafe.BitCast<NVector3, Vector3>(v);
-    /// <summary>
-    /// Converts a System.Numerics.Vector3 to a Stride.Core.Mathematics.Vector3.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
     public static Vector3 ToStride(in this NVector3 v) => Unsafe.BitCast<NVector3, Vector3>(v);
     /// <summary>
     /// Converts a System.Numerics.Vector4 to a Stride.Core.Mathematics.Vector4.
     /// </summary>
     /// <param name="v"></param>
     /// <returns></returns>
-    public static Vector4 ToStride(this NVector4 v) => Unsafe.BitCast<NVector4, Vector4>(v);
-    /// <summary>
-    /// Converts a System.Numerics.Vector4 to a Stride.Core.Mathematics.Vector4.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
     public static Vector4 ToStride(in this NVector4 v) => Unsafe.BitCast<NVector4, Vector4>(v);
-    /// <summary>
-    /// Converts a System.Numerics.Quaternion to a Stride.Core.Mathematics.Quaternion.
-    /// </summary>
-    /// <param name="q"></param>
-    /// <returns></returns>
-    public static Quaternion ToStride(this NQuaternion q) => Unsafe.BitCast<NQuaternion, Quaternion>(q);
     /// <summary>
     /// Converts a System.Numerics.Quaternion to a Stride.Core.Mathematics.Quaternion.
     /// </summary>
@@ -69,20 +42,7 @@ public static class StrideMathExtensions
     /// </summary>
     /// <param name="v"></param>
     /// <returns></returns>
-    public static NVector2 ToNumeric(this Vector2 v) => Unsafe.BitCast<Vector2, NVector2>(v);
-
-    /// <summary>
-    /// Converts a Stride.Core.Mathematics.Vector2 to a System.Numerics.Vector2.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
     public static NVector2 ToNumeric(in this Vector2 v) => Unsafe.BitCast<Vector2, NVector2>(v);
-    /// <summary>
-    /// Converts a Stride.Core.Mathematics.Vector3 to a System.Numerics.Vector3.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
-    public static NVector3 ToNumeric(this Vector3 v) => Unsafe.BitCast<Vector3, NVector3>(v);
     /// <summary>
     /// Converts a Stride.Core.Mathematics.Vector3 to a System.Numerics.Vector3.
     /// </summary>
@@ -94,24 +54,11 @@ public static class StrideMathExtensions
     /// </summary>
     /// <param name="v"></param>
     /// <returns></returns>
-    public static NVector4 ToNumeric(this Vector4 v) => Unsafe.BitCast<Vector4, NVector4>(v);
-    /// <summary>
-    /// Converts a Stride.Core.Mathematics.Vector4 to a System.Numerics.Vector4.
-    /// </summary>
-    /// <param name="v"></param>
-    /// <returns></returns>
     public static NVector4 ToNumeric(in this Vector4 v) => Unsafe.BitCast<Vector4, NVector4>(v);
     /// <summary>
     /// Converts a Stride.Core.Mathematics.Quaternion to a System.Numerics.Quaternion.
     /// </summary>
     /// <param name="q"></param>
     /// <returns></returns>
-    public static NQuaternion ToNumeric(this Quaternion q) => Unsafe.BitCast<Quaternion, NQuaternion>(q);
-    /// <summary>
-    /// Converts a Stride.Core.Mathematics.Quaternion to a System.Numerics.Quaternion.
-    /// </summary>
-    /// <param name="q"></param>
-    /// <returns></returns>
     public static NQuaternion ToNumeric(in this Quaternion q) => Unsafe.BitCast<Quaternion, NQuaternion>(q);
-
 }
