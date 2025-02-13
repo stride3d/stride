30ce6d6877ac2ed4641d01853f03d38957d0413b
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-13 14:13:12 +0100

    [Core] Review collections

--- sources/core/Stride.Core/Collections/PriorityQueueNode.cs
+++ sources/core/Stride.Core/Collections/PriorityQueueNode.cs
@@ -1,21 +1,21 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-namespace Stride.Core.Collections
+
+namespace Stride.Core.Collections;
+
+/// <summary>
+/// Represents a node in a priority queue, to allow O(n) removal.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class PriorityQueueNode<T>
 {
-    /// <summary>
-    /// Represents a node in a priority queue, to allow O(n) removal.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class PriorityQueueNode<T>
+    public T Value;
+
+    public int Index { get; internal set; }
+
+    public PriorityQueueNode(T value)
     {
-        public T Value;
-
-        public int Index { get; internal set; }
-
-        public PriorityQueueNode(T value)
-        {
-            Value = value;
-            Index = -1;
-        }
+        Value = value;
+        Index = -1;
     }
 }
--- sources/core/Stride.Core/Collections/SafeList.cs
+++ sources/core/Stride.Core/Collections/SafeList.cs
@@ -4,28 +4,27 @@
 using Stride.Core.Serialization;
 using Stride.Core.Serialization.Serializers;
 
-namespace Stride.Core.Collections
+namespace Stride.Core.Collections;
+
+/// <summary>
+/// A list to ensure that all items are not null.
+/// </summary>
+/// <typeparam name="T">Type of the item</typeparam>
+[DataSerializer(typeof(ListAllSerializer<,>), Mode = DataSerializerGenericMode.TypeAndGenericArguments)]
+public class SafeList<T> : ConstrainedList<T> where T : class
 {
+    private const string ExceptionError = "The item added to the list cannot be null";
+
     /// <summary>
-    /// A list to ensure that all items are not null.
+    /// Initializes a new instance of the <see cref="SafeList{T}"/> class.
     /// </summary>
-    /// <typeparam name="T">Type of the item</typeparam>
-    [DataSerializer(typeof(ListAllSerializer<,>), Mode = DataSerializerGenericMode.TypeAndGenericArguments)]
-    public class SafeList<T> : ConstrainedList<T> where T : class
+    public SafeList()
+        : base(NonNullConstraint, true, ExceptionError)
     {
-        private const string ExceptionError = "The item added to the list cannot be null";
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SafeList{T}"/> class.
-        /// </summary>
-        public SafeList()
-            : base(NonNullConstraint, true, ExceptionError)
-        {
-        }
-
-        private static bool NonNullConstraint(ConstrainedList<T> constrainedList, T arg2)
-        {
-            return arg2 != null;
-        }
+    private static bool NonNullConstraint(ConstrainedList<T> constrainedList, T arg2)
+    {
+        return arg2 != null;
     }
 }
