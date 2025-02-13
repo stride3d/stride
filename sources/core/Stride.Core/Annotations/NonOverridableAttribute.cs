b1277e91335e654250e33846254671eae846b64e
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-13 11:53:09 +0100

    [Core] Review annotations

--- sources/core/Stride.Core/Annotations/NonOverridableAttribute.cs
+++ sources/core/Stride.Core/Annotations/NonOverridableAttribute.cs
@@ -1,14 +1,9 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using System;
+namespace Stride.Core.Annotations;
 
-namespace Stride.Core.Annotations
-{
-    /// <summary>
-    /// This attribute notifies that the attached member cannot be overridden.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-    public sealed class NonOverridableAttribute : Attribute
-    {
-    }
-}
+/// <summary>
+/// This attribute notifies that the attached member cannot be overridden.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public sealed class NonOverridableAttribute : Attribute;
