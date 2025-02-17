4cfef48b14e321d0fa00c0eb7339548156e6881b
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-17 12:22:03 +0100

    [Core.CompilerServices.Tests] Modernize code

--- sources/core/Stride.Core.CompilerServices.Tests/AnalyzerTests/STRDIAG010_Test.cs
+++ sources/core/Stride.Core.CompilerServices.Tests/AnalyzerTests/STRDIAG010_Test.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Stride.Core.CompilerServices.Analyzers;
 using Xunit;
 
@@ -10,45 +5,45 @@ namespace Stride.Core.CompilerServices.Tests.AnalyzerTests;
 public class STRDIAG010_Test
 {
     [Fact]
-    public void No_Error_On_Default_Constructor()
+    public async Task No_Error_On_Default_Constructor()
     {
         string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, " ");
-        TestHelper.ExpectNoDiagnosticsErrors(sourceCode);
+        await TestHelper.ExpectNoDiagnosticsErrorsAsync(sourceCode);
     }
     [Fact]
-    public void No_Error_On_Empty_Constructor()
+    public async Task No_Error_On_Empty_Constructor()
     {
         string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, "public ValidCollection() { }");
-        TestHelper.ExpectNoDiagnosticsErrors(sourceCode);
+        await TestHelper.ExpectNoDiagnosticsErrorsAsync(sourceCode);
     }
     [Fact]
-    public void No_Error_On_Empty_And_Other_Constructor()
+    public async Task No_Error_On_Empty_And_Other_Constructor()
     {
         string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, "public ValidCollection() { } public ValidCollection(int x) { }");
-        TestHelper.ExpectNoDiagnosticsErrors(sourceCode);
+        await TestHelper.ExpectNoDiagnosticsErrorsAsync(sourceCode);
     }
     [Fact]
-    public void Error_On_No_Empty_Constructor()
+    public async Task Error_On_No_Empty_Constructor()
     {
         string sourceCode = string.Format(ClassTemplates.BasicClassTemplate, "public ValidCollection(int x) { }");
-        TestHelper.ExpectDiagnosticsError(sourceCode,STRDIAG010InvalidConstructor.DiagnosticId);
+        await TestHelper.ExpectDiagnosticsErrorAsync(sourceCode,STRDIAG010InvalidConstructor.DiagnosticId);
     }
     [Fact]
-    public void Error_On_DataContract_Inherited()
+    public async Task Error_On_DataContract_Inherited()
     {
         string sourceCode = string.Format(ClassTemplates.InheritedDataContract, "public Inherited(int x) { }");
-        TestHelper.ExpectDiagnosticsError(sourceCode, STRDIAG010InvalidConstructor.DiagnosticId);
+        await TestHelper.ExpectDiagnosticsErrorAsync(sourceCode, STRDIAG010InvalidConstructor.DiagnosticId);
     }
     [Fact]
-    public void Error_On_Primary_Constructor()
+    public async Task Error_On_Primary_Constructor()
     {
         string sourceCode = string.Format(ClassTemplates.PrimaryConstructorTemplate, "");
-        TestHelper.ExpectDiagnosticsError(sourceCode, STRDIAG010InvalidConstructor.DiagnosticId);
+        await TestHelper.ExpectDiagnosticsErrorAsync(sourceCode, STRDIAG010InvalidConstructor.DiagnosticId);
     }
     [Fact]
-    public void No_Error_On_Flipped_DataContract_Parameters()
+    public async Task No_Error_On_Flipped_DataContract_Parameters()
     {
         string sourceCode = string.Format(ClassTemplates.DataContractArgumentsTemplate, "");
-        TestHelper.ExpectNoDiagnosticsErrors(sourceCode);
+        await TestHelper.ExpectNoDiagnosticsErrorsAsync(sourceCode);
     }
 }
--- sources/core/Stride.Core.CompilerServices.Tests/CompilerUtils.cs
+++ sources/core/Stride.Core.CompilerServices.Tests/CompilerUtils.cs
@@ -1,65 +1,58 @@
-using System;
 using System.Collections.Immutable;
-using System.IO;
-using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Stride.Core.CompilerServices.Analyzers;
 
-namespace Stride.Core.CompilerServices.Tests
+namespace Stride.Core.CompilerServices.Tests;
+
+public static class CompilerUtils
 {
-    public static class CompilerUtils
+    private static readonly string assembliesDirectory = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location)!;
+
+    /// <summary>
+    /// Runs compilation over <paramref name="source"/> and applies generator <typeparamref name="TGenerator"/> over it.
+    /// </summary>
+    public static (Compilation Compilation, ImmutableArray<Diagnostic> Diagnostics) CompileWithGenerator<TGenerator>(string assemblyName, string source)
+        where TGenerator : ISourceGenerator, new()
     {
-        private static string assembliesDirectory = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
+        var generator = new TGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(CreateCompilation(assemblyName, source), out var compilation, out var diagnostics);
+        return (compilation, diagnostics);
+    }
 
-        /// <summary>
-        /// Runs compilation over <paramref name="source"/> and applies generator <typeparamref name="TGenerator"/> over it.
-        /// </summary>
-        public static (Compilation Compilation, ImmutableArray<Diagnostic> Diagnostics) CompileWithGenerator<TGenerator>(string assemblyName, string source)
-            where TGenerator : ISourceGenerator, new()
-        {
-            var generator = new TGenerator();
-            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
-            driver.RunGeneratorsAndUpdateCompilation(CreateCompilation(assemblyName, source), out var compilation, out var diagnostics);
-            return (compilation, diagnostics);
-        }
+    private static CSharpCompilation CreateCompilation(string assemblyName, string source)
+        => CSharpCompilation.Create(assemblyName,
+            [CSharpSyntaxTree.ParseText(source)],
+            [
+                // System.Runtime.dll
+                MetadataReference.CreateFromFile($"{assembliesDirectory}/System.Runtime.dll"),
+                // System.Private.CoreLib.dll
+                MetadataReference.CreateFromFile($"{assembliesDirectory}/System.Private.CoreLib.dll"),
+                // Stride.Core.dll
+                MetadataReference.CreateFromFile(typeof(DataContractAttribute).GetTypeInfo().Assembly.Location),
+            ],
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+    public static DiagnosticAnalyzer[] AllAnalyzers => typeof(DiagnosticsAnalyzerHelper).Assembly.GetTypes()
+        .Where(t => t.IsSubclassOf(typeof(DiagnosticAnalyzer)) && !t.IsAbstract)
+        .Select(type => (DiagnosticAnalyzer)Activator.CreateInstance(type)!).ToArray();
 
-        private static Compilation CreateCompilation(string assemblyName, string source)
-            => CSharpCompilation.Create(assemblyName,
-                new[] { CSharpSyntaxTree.ParseText(source) },
-                new[]
-                {
-                    // System.Runtime.dll
-                    MetadataReference.CreateFromFile($"{assembliesDirectory}/System.Runtime.dll"),
-                    // System.Private.CoreLib.dll
-                    MetadataReference.CreateFromFile($"{assembliesDirectory}/System.Private.CoreLib.dll"),
-                    // Stride.Core.dll
-                    MetadataReference.CreateFromFile(typeof(Stride.Core.DataContractAttribute).GetTypeInfo().Assembly.Location),
-                },
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-        public static DiagnosticAnalyzer[] AllAnalyzers => typeof(DiagnosticsAnalyzerHelper).Assembly.GetTypes()
-            .Where(t => t.IsSubclassOf(typeof(DiagnosticAnalyzer)) && !t.IsAbstract)
-            .Select(type => (DiagnosticAnalyzer)Activator.CreateInstance(type)).ToArray();
+    public static Compilation CreateCompilation(string sourceCode) => CreateCompilation("TestAssembly", sourceCode);
 
-        public static Compilation CreateCompilation(string sourceCode) => CreateCompilation("TestAssembly", sourceCode);
+    public static async Task<ImmutableArray<Diagnostic>> GetAnalyzerDiagnosticsAsync(
+        this Compilation compilation,
+        params DiagnosticAnalyzer[] analyzers)
+    {
+        // Analyze the compilation and get diagnostics
+        var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create(analyzers));
+        return await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
+    }
 
-        public static ImmutableArray<Diagnostic> GetAnalyzerDiagnostics(
-            this Compilation compilation,
-            params DiagnosticAnalyzer[] analyzers)
-        {
-            // Analyze the compilation and get diagnostics
-            var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create(analyzers));
-            var diagnostics = compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync().Result;
-
-            return diagnostics;
-        }
-
-        public static ImmutableArray<Diagnostic> CompileAndGetAnalyzerDiagnostics(string sourceCode, params DiagnosticAnalyzer[] analyzers)
-        {
-            Compilation compilation = CreateCompilation(sourceCode);
-            return GetAnalyzerDiagnostics(compilation, analyzers);
-        }
+    public static Task<ImmutableArray<Diagnostic>> CompileAndGetAnalyzerDiagnosticsAsync(string sourceCode, params DiagnosticAnalyzer[] analyzers)
+    {
+        Compilation compilation = CreateCompilation(sourceCode);
+        return GetAnalyzerDiagnosticsAsync(compilation, analyzers);
     }
 }
