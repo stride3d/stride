c99057942a9a61d42dd5857252e60a2c8daf5272
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-17 14:17:05 +0100

    [Core.Tests] Modernize code

--- sources/core/Stride.Core.Tests/ObjectIdTests.cs
+++ sources/core/Stride.Core.Tests/ObjectIdTests.cs
@@ -1,17 +1,16 @@
 using Stride.Core.Storage;
 using Xunit;
 
-namespace Stride.Core.Tests
+namespace Stride.Core.Tests;
+
+public class ObjectIdTests
 {
-    public class ObjectIdTests
+    [Fact]
+    public void ToString_ThenTryParse_GivesTheSameResult()
     {
-        [Fact]
-        public void ToString_ThenTryParse_GivesTheSameResult()
-        {
-            var id = ObjectId.New();
-            var str = id.ToString();
-            Assert.True(ObjectId.TryParse(str, out var parsed));
-            Assert.Equal(id, parsed);
-        }
+        var id = ObjectId.New();
+        var str = id.ToString();
+        Assert.True(ObjectId.TryParse(str, out var parsed));
+        Assert.Equal(id, parsed);
     }
 }
