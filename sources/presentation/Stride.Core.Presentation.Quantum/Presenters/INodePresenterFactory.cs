6975a0efdea0df464067457bbeba92ee858d631f
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-21 16:00:51 +0100

    [Core.Presentation.Quantum] Modernize code

--- sources/presentation/Stride.Core.Presentation.Quantum/Presenters/INodePresenterFactory.cs
+++ sources/presentation/Stride.Core.Presentation.Quantum/Presenters/INodePresenterFactory.cs
@@ -1,16 +1,13 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using System;
-using System.Collections.Generic;
-using Stride.Core.Annotations;
+
 using Stride.Core.Quantum;
 
-namespace Stride.Core.Presentation.Quantum.Presenters
-{
-    public interface INodePresenterFactory
-    {
-        INodePresenter CreateNodeHierarchy([NotNull] IObjectNode rootNode, [NotNull] GraphNodePath rootNodePath, [CanBeNull] IPropertyProviderViewModel propertyProvider = null);
+namespace Stride.Core.Presentation.Quantum.Presenters;
 
-        bool IsPrimitiveType(Type type);
-    }
+public interface INodePresenterFactory
+{
+    INodePresenter CreateNodeHierarchy(IObjectNode rootNode, GraphNodePath rootNodePath, IPropertyProviderViewModel? propertyProvider = null);
+
+    bool IsPrimitiveType(Type type);
 }
