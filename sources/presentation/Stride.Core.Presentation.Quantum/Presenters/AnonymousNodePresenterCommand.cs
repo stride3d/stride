391603dd180852f5fe73f9bcdd67d63fc1c0ff28
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-21 16:00:51 +0100

    [Core.Presentation.Quantum] Modernize code

--- sources/presentation/Stride.Core.Presentation.Quantum/Presenters/AnonymousNodePresenterCommand.cs
+++ sources/presentation/Stride.Core.Presentation.Quantum/Presenters/AnonymousNodePresenterCommand.cs
@@ -1,38 +1,34 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using System;
-using System.Threading.Tasks;
-using Stride.Core.Annotations;
 
-namespace Stride.Core.Presentation.Quantum.Presenters
+namespace Stride.Core.Presentation.Quantum.Presenters;
+
+public class AnonymousNodePresenterCommand : NodePresenterCommandBase
 {
-    public class AnonymousNodePresenterCommand : NodePresenterCommandBase
+    private readonly Func<INodePresenter, object?, Task> execute;
+    private readonly Func<INodePresenter, bool>? canAttach;
+
+    public AnonymousNodePresenterCommand(string name, Func<INodePresenter, object?, Task> execute, Func<INodePresenter, bool>? canAttach = null)
     {
-        private readonly Func<INodePresenter, object, Task> execute;
-        private readonly Func<INodePresenter, bool> canAttach;
+        ArgumentNullException.ThrowIfNull(name);
+        ArgumentNullException.ThrowIfNull(execute);
+        this.execute = execute;
+        this.canAttach = canAttach;
+        Name = name;
+    }
 
-        public AnonymousNodePresenterCommand([NotNull] string name, [NotNull] Func<INodePresenter, object, Task> execute, [CanBeNull] Func<INodePresenter, bool> canAttach = null)
-        {
-            if (name == null) throw new ArgumentNullException(nameof(name));
-            if (execute == null) throw new ArgumentNullException(nameof(execute));
-            this.execute = execute;
-            this.canAttach = canAttach;
-            Name = name;
-        }
+    /// <inheritdoc/>
+    public override string Name { get; }
 
-        /// <inheritdoc/>
-        public override string Name { get; }
+    /// <inheritdoc/>
+    public override bool CanAttach(INodePresenter nodePresenter)
+    {
+        return canAttach?.Invoke(nodePresenter) ?? true;
+    }
 
-        /// <inheritdoc/>
-        public override bool CanAttach(INodePresenter nodePresenter)
-        {
-            return canAttach?.Invoke(nodePresenter) ?? true;
-        }
-
-        /// <inheritdoc/>
-        public override Task Execute(INodePresenter nodePresenter, object parameter, object preExecuteResult)
-        {
-            return execute(nodePresenter, parameter);
-        }
+    /// <inheritdoc/>
+    public override Task Execute(INodePresenter nodePresenter, object? parameter, object? preExecuteResult)
+    {
+        return execute(nodePresenter, parameter);
     }
 }
