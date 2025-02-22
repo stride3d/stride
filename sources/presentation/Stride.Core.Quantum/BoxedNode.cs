933f47fdb407e597af71ee646abd95af8df04472
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-22 15:53:21 +0100

    [Core.Quantum] Modernize code

--- sources/presentation/Stride.Core.Quantum/BoxedNode.cs
+++ sources/presentation/Stride.Core.Quantum/BoxedNode.cs
@@ -1,68 +1,65 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using System;
-using Stride.Core.Annotations;
 using Stride.Core.Reflection;
 
-namespace Stride.Core.Quantum
+namespace Stride.Core.Quantum;
+
+public class BoxedNode : ObjectNode
 {
-    public class BoxedNode : ObjectNode
+    private GraphNodeBase? boxedStructureOwner;
+    private NodeIndex boxedStructureOwnerIndex;
+
+    public BoxedNode(INodeBuilder nodeBuilder, object value, Guid guid, ITypeDescriptor descriptor)
+        : base(nodeBuilder, value, guid, descriptor, null)
     {
-        private GraphNodeBase boxedStructureOwner;
-        private NodeIndex boxedStructureOwnerIndex;
+    }
 
-        public BoxedNode([NotNull] INodeBuilder nodeBuilder, object value, Guid guid, [NotNull] ITypeDescriptor descriptor)
-            : base(nodeBuilder, value, guid, descriptor, null)
-        {
-        }
+    protected internal override void UpdateFromMember(object newValue, NodeIndex index)
+    {
+        Update(newValue, index, true);
+    }
 
-        protected internal override void UpdateFromMember(object newValue, NodeIndex index)
-        {
-            Update(newValue, index, true);
-        }
+    internal void UpdateFromOwner(object newValue)
+    {
+        Update(newValue, NodeIndex.Empty, false);
+    }
 
-        internal void UpdateFromOwner(object newValue)
-        {
-            Update(newValue, NodeIndex.Empty, false);
-        }
+    internal void SetOwnerContent(IGraphNode ownerNode, NodeIndex index)
+    {
+        boxedStructureOwner = (GraphNodeBase)ownerNode;
+        boxedStructureOwnerIndex = index;
+    }
 
-        internal void SetOwnerContent(IGraphNode ownerNode, NodeIndex index)
+    private void Update(object newValue, NodeIndex index, bool updateStructureOwner)
+    {
+        if (index.TryGetValue(out var indexValue))
         {
-            boxedStructureOwner = (GraphNodeBase)ownerNode;
-            boxedStructureOwnerIndex = index;
-        }
-
-        private void Update(object newValue, NodeIndex index, bool updateStructureOwner)
-        {
-            if (!index.IsEmpty)
+            if (Descriptor is CollectionDescriptor collectionDescriptor)
             {
-                var collectionDescriptor = Descriptor as CollectionDescriptor;
-                var dictionaryDescriptor = Descriptor as DictionaryDescriptor;
-                if (collectionDescriptor != null)
-                {
-                    collectionDescriptor.SetValue(Value, index.Value, newValue);
-                }
-                else if (dictionaryDescriptor != null)
-                {
-                    dictionaryDescriptor.SetValue(Value, index, newValue);
-                }
-                else
-                    throw new NotSupportedException("Unable to set the node value, the collection is unsupported");
+                collectionDescriptor.SetValue(Value, indexValue, newValue);
+            }
+            else if (Descriptor is DictionaryDescriptor dictionaryDescriptor)
+            {
+                dictionaryDescriptor.SetValue(Value, index, newValue);
             }
             else
             {
-                SetValue(newValue);
-                if (updateStructureOwner)
-                {
-                    boxedStructureOwner?.UpdateFromMember(newValue, boxedStructureOwnerIndex);
-                }
+                throw new NotSupportedException("Unable to set the node value, the collection is unsupported");
             }
         }
-
-        public override string ToString()
+        else
         {
-            return $"{{Node: Boxed {Type.Name} = [{Value}]}}";
+            SetValue(newValue);
+            if (updateStructureOwner)
+            {
+                boxedStructureOwner?.UpdateFromMember(newValue, boxedStructureOwnerIndex);
+            }
         }
+    }
+
+    public override string ToString()
+    {
+        return $"{{Node: Boxed {Type.Name} = [{Value}]}}";
     }
 }
