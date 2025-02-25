e2303c59d42e90c09482387eb54a3cc9e933b1bb
Nicolas Musset <nicolas@color-rise.xyz>
2025-02-25 11:51:36 +0100

    [Core.Presentation.Quantum.Tests] Modernize code

--- sources/presentation/Stride.Core.Presentation.Quantum.Tests/Helpers/TestContainerContext.cs
+++ sources/presentation/Stride.Core.Presentation.Quantum.Tests/Helpers/TestContainerContext.cs
@@ -1,34 +1,33 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using System.Windows.Threading;
-using Stride.Core.Presentation.View;
+
+using Stride.Core.Presentation.Services;
 using Stride.Core.Presentation.ViewModels;
 using Stride.Core.Quantum;
 
-namespace Stride.Core.Presentation.Quantum.Tests.Helpers
+namespace Stride.Core.Presentation.Quantum.Tests.Helpers;
+
+public class TestContainerContext
 {
-    public class TestContainerContext
+    public TestContainerContext()
     {
-        public TestContainerContext()
-        {
-            NodeContainer = new NodeContainer();
-            GraphViewModelService = new GraphViewModelService(NodeContainer);
-            ServiceProvider = new ViewModelServiceProvider();
-            ServiceProvider.RegisterService(new DispatcherService(Dispatcher.CurrentDispatcher));
-            ServiceProvider.RegisterService(GraphViewModelService);
-        }
+        NodeContainer = new NodeContainer();
+        GraphViewModelService = new GraphViewModelService(NodeContainer);
+        ServiceProvider = new ViewModelServiceProvider();
+        ServiceProvider.RegisterService(new NullDispatcherService());
+        ServiceProvider.RegisterService(GraphViewModelService);
+    }
 
-        public ViewModelServiceProvider ServiceProvider { get; }
+    public ViewModelServiceProvider ServiceProvider { get; }
 
-        public GraphViewModelService GraphViewModelService { get; }
+    public GraphViewModelService GraphViewModelService { get; }
 
-        public NodeContainer NodeContainer { get; }
+    public NodeContainer NodeContainer { get; }
 
-        public TestInstanceContext CreateInstanceContext(object instance)
-        {
-            var rootNode = NodeContainer.GetOrCreateNode(instance);
-            var context = new TestInstanceContext(this, rootNode);
-            return context;
-        }
+    public TestInstanceContext CreateInstanceContext(object instance)
+    {
+        var rootNode = NodeContainer.GetOrCreateNode(instance);
+        var context = new TestInstanceContext(this, rootNode);
+        return context;
     }
 }
