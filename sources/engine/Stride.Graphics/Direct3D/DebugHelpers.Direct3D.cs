1ed2722120cdb60f86887246ad13c6d8362dcad3
Mario Guerra <mgg7007@gmail.com>
2025-02-26 15:45:42 +0100

    Improve memory handling for name strings in DebugHelpers
    * Also join all the SetDebugName methods into one

--- sources/engine/Stride.Graphics/Direct3D/DebugHelpers.Direct3D.cs
+++ sources/engine/Stride.Graphics/Direct3D/DebugHelpers.Direct3D.cs
@@ -11,14 +11,14 @@ using Silk.NET.Core.Native;
 using Silk.NET.Direct3D11;
 using Silk.NET.Direct3D12;
 using Silk.NET.DXGI;
-using Stride.Core;
+using Stride.Core.UnsafeExtensions;
 
 namespace Stride.Graphics;
 
 internal static unsafe class DebugHelpers
 {
-    // From d3dcommon.h in Windows SDK
-    public static Guid* WKPDID_D3DDebugObjectName
+    // From d3dcommon.h in Windows SDK (WKPDID_D3DDebugObjectName)
+    public static Guid* DebugObjectName
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
@@ -43,36 +43,40 @@ internal static unsafe class DebugHelpers
         }
     }
 
+
     /// <summary>
-    ///   Associates a debug name to the private data of a device child, useful to see a friendly name
+    ///   Associates a debug name to the private data of a DXGI or Direct3D object, useful to see a friendly name
     ///   in some graphics debuggers.
     /// </summary>
-#if STRIDE_GRAPHICS_API_DIRECT3D11
-    public static void SetDebugName(this ComPtr<ID3D11DeviceChild> deviceChild, string name, ICollectorHolder owningObject)
-    {
-        var nameMemory = SilkMarshal.StringToMemory(name, NativeStringEncoding.LPWStr);
-        nameMemory.DisposeBy(owningObject);
-
-        deviceChild.SetPrivateData(WKPDID_D3DDebugObjectName, (uint) nameMemory.Length, nameMemory.AsPtr<char>());
-    }
-#elif STRIDE_GRAPHICS_API_DIRECT3D12
-    public static unsafe void SetDebugName(this ComPtr<ID3D12DeviceChild> deviceChild, string name)
+    /// <param name="comPtr">A COM pointer to the object to set the debug name for.</param>
+    /// <param name="name">The name to associate with the object.</param>
+    /// <exception cref="NotSupportedException">Thrown when the specified COM pointer type is not supported.</exception>
+    public static void SetDebugName<T>(this ComPtr<T> comPtr, string name)
+        where T : unmanaged, IComVtbl<T>
     {
         using var nameMemory = SilkMarshal.StringToMemory(name, NativeStringEncoding.LPWStr);
-        deviceChild->SetName(nameMemory.AsPtr<char>());
-    }
+
+        switch (comPtr)
+        {
+            case IComVtbl<IDXGIObject>:
+                var dxgiObject = comPtr.BitCast<ComPtr<T>, ComPtr<IDXGIObject>>();
+                dxgiObject.SetPrivateData(DebugObjectName, (uint) nameMemory.Length, nameMemory.AsPtr<char>());
+                break;
+
+#if STRIDE_GRAPHICS_API_DIRECT3D11
+            case IComVtbl<ID3D11DeviceChild>:
+                var d3d11DeviceChild = comPtr.BitCast<ComPtr<T>, ComPtr<ID3D11DeviceChild>>();
+                d3d11DeviceChild.SetPrivateData(DebugObjectName, (uint) nameMemory.Length, nameMemory.AsPtr<char>());
+                break;
+#elif STRIDE_GRAPHICS_API_DIRECT3D12
+            case IComVtbl<ID3D12DeviceChild>:
+                var d3d12DeviceChild = comPtr.BitCast<ComPtr<T>, ComPtr<ID3D12DeviceChild>>();
+                d3d12DeviceChild.SetName(nameMemory.AsPtr<char>());
+                break;
 #endif
-
-    /// <summary>
-    ///   Associates a debug name to the private data of a DXGI object, useful to see a friendly name
-    ///   in some graphics debuggers.
-    /// </summary>
-    public static void SetDebugName(this ComPtr<IDXGIObject> dxgiObject, string name, ICollectorHolder owningObject)
-    {
-        var nameMemory = SilkMarshal.StringToMemory(name, NativeStringEncoding.LPWStr);
-        nameMemory.DisposeBy(owningObject);
-
-        dxgiObject.SetPrivateData(WKPDID_D3DDebugObjectName, (uint)nameMemory.Length, nameMemory.AsPtr<char>());
+            default:
+                throw new NotSupportedException("The specified COM pointer type is not supported.");
+        }
     }
 }
 
