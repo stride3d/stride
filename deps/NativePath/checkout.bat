ea2ffccfe1540dc3b41fb327f85ae2314daa05df
Elias Holzer <elias@vvvv.org>
2025-03-04 13:00:20 +0100

    Updates checkout scripts of freetype and Bullet and NativePath

--- deps/NativePath/checkout.bat
+++ deps/NativePath/checkout.bat
@@ -5,4 +5,8 @@ IF NOT ERRORLEVEL 0 (
 ) 
 %GIT_CMD% clone https://github.com/stride3d/NativePath ../../externals/NativePath
 IF NOT ERRORLEVEL 0 PAUSE
+pushd ..\..\externals\NativePath
+%GIT_CMD% checkout e615840e57ea80ce30b9e9f29f5fd84ca9b4b253
+popd
+IF NOT ERRORLEVEL 0 PAUSE
 
--- deps/freetype/checkout.bat
+++ deps/freetype/checkout.bat
@@ -6,7 +6,10 @@ IF NOT ERRORLEVEL 0 (
 %GIT_CMD% clone https://github.com/stride3d/freetype.git -b 2.6.3 ../../externals/freetype
 if NOT ERRORLEVEL 0 pause
 pushd ..\..\externals\freetype
-%GIT_CMD% remote add upstream http://git.sv.nongnu.org/r/freetype/freetype2.git
-%GIT_CMD% fetch --all
+%GIT_CMD% checkout 84631a11ab76240581f621287b260a6936a9014c
 popd
 if NOT ERRORLEVEL 0 pause
+
+:: Upstream repo can be added via
+:: %GIT_CMD% remote add upstream http://git.sv.nongnu.org/r/freetype/freetype2.git
+:: %GIT_CMD% fetch --all
