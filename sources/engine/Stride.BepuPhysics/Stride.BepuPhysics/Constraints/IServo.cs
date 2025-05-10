63e4207ee11340623448d36aa1f9996a47276d85
Eideren <contact@eideren.com>
2025-05-10 14:45:55 +0200

    fix: Bepu, ctor with valid orientation for constraints (#2777)

--- sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Constraints/IServo.cs
+++ sources/engine/Stride.BepuPhysics/Stride.BepuPhysics/Constraints/IServo.cs
@@ -8,7 +8,32 @@ namespace Stride.BepuPhysics.Constraints;
 /// </summary>
 public interface IServo
 {
+    /// <summary>
+    /// Maximum speed that the constraint can try to use to move towards the target.
+    /// </summary>
+    /// <userdoc>
+    /// Maximum speed that the constraint can try to use to move towards the target.
+    /// </userdoc>
     float ServoMaximumSpeed { get; set; }
+    /// <summary>
+    /// Minimum speed that the constraint will try to use to move towards the target.
+    /// If the speed implied by the spring configuration is higher than this, the servo will attempt to use the higher speed.
+    /// Will be clamped by the MaximumSpeed.
+    /// </summary>
+    /// <userdoc>
+    /// Minimum speed that the constraint will try to use to move towards the target.
+    /// If the speed implied by the spring configuration is higher than this, the servo will attempt to use the higher speed.
+    /// Will be clamped by the MaximumSpeed.
+    /// </userdoc>
     float ServoBaseSpeed { get; set; }
+    /// <summary>
+    /// The maximum force that the constraint can apply to move towards the target.
+    /// </summary>
+    /// <remarks>
+    /// This value is specified in terms of force: a change in momentum over time. It is approximated as a maximum impulse (an instantaneous change in momentum) on a per-substep basis. In other words, for a given velocity iteration, the constraint's impulse can be no larger than <see cref="MaximumForce"/> * dt where dt is the substep duration.
+    /// </remarks>
+    /// <userdoc>
+    /// The maximum force that the constraint can apply to move towards the target.
+    /// </userdoc>
     float ServoMaximumForce { get; set; }
 }
