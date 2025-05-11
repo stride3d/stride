31fb3bd89b63ea18275920e274cbb8c58187ec5f
Vektor19 <viktor.komissarov@knu.ua>
2025-05-12 00:33:44 +0300

    refactor: update GitFile class to use FileStatus instead of GitFileStatus

--- sources/editor/Stride.GameStudio/Git/GitFile.cs
+++ sources/editor/Stride.GameStudio/Git/GitFile.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LibGit2Sharp;
 
 namespace Stride.GameStudio.Git
 {
     public class GitFile
     {
         public string RelativeFilePath { get; set; }
-        public GitFileStatus Status { get; set; }
+        public FileStatus Status { get; set; }
         public bool IsStaged { get; set; }
     }
 }
