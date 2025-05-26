stride3d/stride

commit da86a2ca5e783bb7b11d504bce57a3f53638ccbf

feat: NuGet resolution & start from new Launcher (#2776)

sources/core/Stride.Core/Native/NativeLibraryHelper.cs
@@ -9,7 +9,7 @@ namespace Stride.Core;
 public static class NativeLibraryHelper
 {
     private const string UNIX_LIB_PREFIX = "lib";
-    private static readonly Dictionary<string, IntPtr> LoadedLibraries = new(StringComparer.OrdinalIgnoreCase);
+    private static readonly Dictionary<string, nint> LoadedLibraries = new(StringComparer.OrdinalIgnoreCase);
     private static readonly Dictionary<string, string> NativeDependencies = new(StringComparer.OrdinalIgnoreCase);
 
     /// <summary>
@@ -19,15 +19,15 @@ public static class NativeLibraryHelper
     /// <param name="libraryName">Name of the library, without the extension.</param>
     /// <param name="owner">Type whose assembly location is related to the native library (we can't use GetCallingAssembly as it might be wrong due to optimizations).</param>
     /// <exception cref="System.InvalidOperationException">Library could not be loaded.</exception>
-    public static void PreloadLibrary(string libraryName, Type owner)
+    public static nint PreloadLibrary(string libraryName, Type owner)
     {
 #if STRIDE_PLATFORM_DESKTOP
         lock (LoadedLibraries)
         {
             // If already loaded, just exit as we want to load it just once
-            if (LoadedLibraries.ContainsKey(libraryName))
+            if (LoadedLibraries.TryGetValue(libraryName, out var handle))
             {
-                return;
+                return handle;
             }
 
             // Was the dependency registered beforehand?
@@ -35,7 +35,7 @@ public static class NativeLibraryHelper
                 if (NativeDependencies.TryGetValue(libraryName, out var path) && NativeLibrary.TryLoad(path, out var result))
                 {
                     LoadedLibraries.Add(libraryName, result);
-                    return;
+                    return result;
                 }
             }
 
@@ -44,7 +44,7 @@ public static class NativeLibraryHelper
                 if (NativeLibrary.TryLoad(libraryName, owner.Assembly, searchPath: null, out var result))
                 {
                     LoadedLibraries.Add(libraryName, result);
-                    return;
+                    return result;
                 }
             }
 
@@ -75,13 +75,13 @@ public static class NativeLibraryHelper
                 if (NativeLibrary.TryLoad(libraryNameWithExtension, out var result))
                 {
                     LoadedLibraries.Add(libraryName, result);
-                    return;
+                    return result;
                 }
                 else if (!libraryName.StartsWith(UNIX_LIB_PREFIX, StringComparison.Ordinal)
                     && NativeLibrary.TryLoad(UNIX_LIB_PREFIX + libraryNameWithExtension, out result))
                 {
                     LoadedLibraries.Add(libraryName, result);
-                    return;
+                    return result;
                 }
             }
 
@@ -102,7 +102,7 @@ public static class NativeLibraryHelper
                     if (NativeLibrary.TryLoad(libraryFilename, out var result))
                     {
                         LoadedLibraries.Add(libraryName, result);
-                        return;
+                        return result;
                     }
                 }
             }
@@ -114,7 +114,7 @@ public static class NativeLibraryHelper
                 if (NativeLibrary.TryLoad(libraryFilename, out var result))
                 {
                     LoadedLibraries.Add(libraryName, result);
-                    return;
+                    return result;
                 }
             }
 

build/Stride.sln
@@ -1857,6 +1857,7 @@ Global
 		SolutionGuid = {FF877973-604D-4EA7-B5F5-A129961F9EF2}
 	EndGlobalSection
 	GlobalSection(SharedMSBuildProjectFiles) = preSolution
+		..\sources\shared\Stride.NuGetResolver.Targets\Stride.NuGetResolver.Targets.projitems*{19838bc4-e889-422b-b234-b94a3996a4aa}*SharedItemsImports = 5
 		..\sources\assets\Stride.Core.Assets.Yaml\Stride.Core.Assets.Yaml.projitems*{1e54a9a2-4439-4444-ae57-6d2ed3c0dc47}*SharedItemsImports = 5
 		..\sources\shared\Stride.Core.ShellHelper\Stride.Core.ShellHelper.projitems*{1e54a9a2-4439-4444-ae57-6d2ed3c0dc47}*SharedItemsImports = 5
 		..\sources\shared\Stride.NuGetResolver.Targets\Stride.NuGetResolver.Targets.projitems*{2fc40214-a4aa-45dc-9c93-72ed800c40b0}*SharedItemsImports = 5

sources/assets/Stride.Core.Packages/NugetStore.cs
@@ -143,11 +143,13 @@ public partial class NugetStore : INugetDownloadProgress
     /// List of package Ids under which the main package is known. Usually just one entry, but
     /// we could have several in case there is a product name change.
     /// </summary>
-    public IReadOnlyCollection<string> MainPackageIds { get; } = ["Stride.GameStudio", "Xenko.GameStudio", "Xenko"];
+    // FIXME xplat-editor these names should be parameterized and given to the constructor
+    public IReadOnlyCollection<string> MainPackageIds { get; } = ["Stride.GameStudio.Avalonia.Desktop", "Stride.GameStudio", "Xenko.GameStudio", "Xenko"];
 
     /// <summary>
     /// Package Id of the Visual Studio Integration plugin.
     /// </summary>
+    // FIXME xplat-editor this name should be parameterized and given to the constructor
     public string VsixPackageId { get; } = "Stride.VisualStudio.Package";
 
     /// <summary>
@@ -450,7 +452,7 @@ public partial class NugetStore : INugetDownloadProgress
     /// </summary>
     /// <remarks>It is safe to call it concurrently be cause we operations are done using the FileLock.</remarks>
     /// <param name="package">Package to uninstall.</param>
-    public async Task UninstallPackage(NugetPackage package, ProgressReport progress)
+    public async Task UninstallPackage(NugetPackage package, ProgressReport? progress)
     {
 #if DEBUG
         var installedPackages = GetPackagesInstalled([package.Id]);

sources/assets/Stride.Core.Packages/ProgressReport.cs
@@ -10,7 +10,7 @@ public class ProgressReport : IDisposable
     private ProgressAction action;
     private int progress;
 
-    public ProgressReport(NugetStore store, NugetPackage package)
+    public ProgressReport(NugetStore store, NugetPackage? package)
     {
         ArgumentNullException.ThrowIfNull(store);
         this.store = store;

sources/editor/Stride.GameStudio.Avalonia.Desktop/Module.cs
@@ -1,0 +1,31 @@
+using System.Runtime.InteropServices;
+using Stride.Core;
+
+namespace Stride.GameStudio.Avalonia.Desktop;
+
+internal static class Module
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        // Override import resolution for Avalonia native dependencies
+        OverrideImportResolution<HarfBuzzSharp.ContentType>();
+        OverrideImportResolution<SkiaSharp.GRBackend>();
+        return;
+
+        static void OverrideImportResolution<T>()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(T).Assembly, (name, _, _) =>
+            {
+                try
+                {
+                    return NativeLibraryHelper.PreloadLibrary(name, typeof(T));
+                }
+                catch (Exception)
+                {
+                    return nint.Zero;
+                }
+            });
+        }
+    }
+}

sources/editor/Stride.GameStudio.Avalonia.Desktop/Stride.GameStudio.Avalonia.Desktop.csproj
@@ -1,11 +1,13 @@
-<Project>
+﻿<Project>
   <Import Project="..\..\targets\Stride.props" />
 
   <PropertyGroup>
     <TargetFramework>$(StrideXplatEditorTargetFramework)</TargetFramework>
+    <RuntimeIdentifiers>linux-x64;win-x64</RuntimeIdentifiers>
     <ImplicitUsings>enable</ImplicitUsings>
     <LangVersion>latest</LangVersion>
     <Nullable>enable</Nullable>
+    <StrideNuGetResolverUI>false</StrideNuGetResolverUI>
   </PropertyGroup>
 
   <ItemGroup>
@@ -43,5 +45,6 @@
     <Compile Include="..\..\launcher\Stride.Launcher\Crash\CrashReportWindow.axaml.cs" Link="Crash\CrashReportWindow.axaml.cs" />
   </ItemGroup>
 
+  <Import Project="..\..\shared\Stride.NuGetResolver.Targets\Stride.NuGetResolver.Targets.projitems" Label="Shared" />
   <Import Project="$(StrideSdkTargets)" />
 </Project>

sources/launcher/Stride.Launcher/App.axaml.cs
@@ -31,7 +31,7 @@ public partial class App : Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = MainWindow = new MainWindow
+            desktop.MainWindow = MainWindow = new()
             {
                 DataContext = InitializeMainViewModel()
             };
@@ -50,7 +50,7 @@ public partial class App : Application
 
     private static MainViewModel InitializeMainViewModel()
     {
-        return new MainViewModel(InitializeServiceProvider());
+        return new(InitializeServiceProvider());
     }
 
     private static IViewModelServiceProvider InitializeServiceProvider()

sources/launcher/Stride.Launcher/Constants.cs
@@ -1,0 +1,18 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+namespace Stride.Launcher;
+
+internal struct Names
+{
+    public const string GameStudio = nameof(GameStudio);
+    public const string Stride = nameof(Stride);
+    public const string Xenko = nameof(Xenko);
+}
+
+internal struct GameStudioNames
+{
+    public const string Stride = $"{Names.Stride}.{Names.GameStudio}";
+    public const string StrideAvalonia = $"{Names.Stride}.{Names.GameStudio}.Avalonia.Desktop";
+    public const string Xenko = $"{Names.Xenko}.{Names.GameStudio}";
+}

commit 4d9a1b90e06166b70364177cf21ff96e243558a0

Fix non-desktop build (regression in #2776)

sources/core/Stride.Core/Native/NativeLibraryHelper.cs
@@ -120,6 +120,8 @@ public static class NativeLibraryHelper
 
             throw new InvalidOperationException($"Could not load native library {libraryName} using CPU architecture {cpu}.");
         }
+#else
+        return default;
 #endif
     }
 