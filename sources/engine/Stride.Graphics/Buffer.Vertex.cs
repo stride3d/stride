5ccf8d01d1c777845779d3c3f788ee5f93260da7
Mario Guerra <mgg7007@gmail.com>
2025-06-24 22:55:00 +0200

    Expand documentation for Buffer and associated types

--- sources/engine/Stride.Graphics/Buffer.Vertex.cs
+++ sources/engine/Stride.Graphics/Buffer.Vertex.cs
@@ -28,96 +28,116 @@ namespace Stride.Graphics
     public partial class Buffer
     {
         /// <summary>
-        /// Vertex buffer helper methods.
+        ///   Helper methods for creating <strong>Vertex Buffers</strong>.
         /// </summary>
+        /// <remarks>
+        ///   A <strong>Vertex Buffer</strong> is a <see cref="Buffer"/> that is used as input to the vertex shader stage.
+        ///   They store vertex data for 3D models (positions, normals, UVs, etc.).
+        /// </remarks>
+        /// <seealso cref="Buffer"/>
+        /// <seealso cref="Buffer{T}"/>
         public static class Vertex
         {
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Default"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong> of a given size.
             /// </summary>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="size">The size in bytes.</param>
-            /// <param name="usage">The usage.</param>
-            /// <param name="bindFlags">The bind flags, can be combined with <see cref="BufferFlags.StreamOutput"/> to use the buffer as a stream output target.</param>
-            /// <returns>
-            /// A Vertex buffer
-            /// </returns>
+            /// <param name="bufferSize">Size of the Buffer in bytes.</param>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Default"/>.
+            /// </param>
+            /// <param name="additionalFlags">
+            ///   Additional Buffer flags. For example, you can specify <see cref="BufferFlags.StreamOutput"/> to use the Buffer as a Stream Output target.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer New(GraphicsDevice device, int bufferSize, GraphicsResourceUsage usage = GraphicsResourceUsage.Default, BufferFlags additionalFlags = BufferFlags.None)
             {
                 return Buffer.New(device, bufferSize, BufferFlags.VertexBuffer | additionalFlags, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Default"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong>.
             /// </summary>
-            /// <typeparam name="T">Type of the Vertex buffer to get the sizeof from</typeparam>
+            /// <typeparam name="T">Type of the data stored in the Buffer.</typeparam>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="usage">The usage.</param>
-            /// <returns>A Vertex buffer</returns>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Default"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer<T> New<T>(GraphicsDevice device, GraphicsResourceUsage usage = GraphicsResourceUsage.Default) where T : unmanaged
             {
                 return Buffer.New<T>(device, elementCount: 1, BufferFlags.VertexBuffer, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Immutable"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong> with initial data.
             /// </summary>
-            /// <typeparam name="T">Type of the Vertex buffer to get the sizeof from</typeparam>
+            /// <typeparam name="T">Type of the data stored in the Buffer.</typeparam>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
             /// <param name="value">The value to initialize the Vertex buffer.</param>
-            /// <param name="usage">The usage of this resource.</param>
-            /// <returns>A Vertex buffer</returns>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Immutable"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer<T> New<T>(GraphicsDevice device, ref readonly T value, GraphicsResourceUsage usage = GraphicsResourceUsage.Immutable) where T : unmanaged
             {
                 return Buffer.New(device, in value, BufferFlags.VertexBuffer, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Immutable"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong> with initial data.
             /// </summary>
-            /// <typeparam name="T">Type of the Vertex buffer to get the sizeof from</typeparam>
+            /// <typeparam name="T">Type of the data stored in the Buffer.</typeparam>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="value">The value to initialize the Vertex buffer.</param>
-            /// <param name="usage">The usage of this resource.</param>
-            /// <returns>A Vertex buffer</returns>
+            /// <param name="data">The data to initialize the Vertex buffer.</param>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Immutable"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer<T> New<T>(GraphicsDevice device, T[] data, GraphicsResourceUsage usage = GraphicsResourceUsage.Immutable) where T : unmanaged
             {
                 return Buffer.New(device, (ReadOnlySpan<T>) data, BufferFlags.VertexBuffer, PixelFormat.None, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Default"/> usage by default.
+            ///   Creates a new <strong>Vertex Buffer</strong>.
             /// </summary>
-            /// <typeparam name="T">Type of the Vertex buffer to get the sizeof from</typeparam>
+            /// <typeparam name="T">Type of the data stored in the Buffer.</typeparam>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="vertexBufferCount">Number of vertex in this buffer with the sizeof(T).</param>
-            /// <param name="usage">The usage.</param>
-            /// <returns>A Vertex buffer</returns>
+            /// <param name="vertexCount">The number of vertices in this Buffer.</param>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Default"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer<T> New<T>(GraphicsDevice device, int vertexCount, GraphicsResourceUsage usage = GraphicsResourceUsage.Default) where T : unmanaged
             {
                 return Buffer.New<T>(device, vertexCount, BufferFlags.VertexBuffer, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Immutable"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong> with initial data.
             /// </summary>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="value">The value to initialize the Vertex buffer.</param>
-            /// <param name="usage">The usage of this resource.</param>
-            /// <returns>A Vertex buffer</returns>
+            /// <param name="data">The data to initialize the Vertex buffer.</param>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Immutable"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
             public static Buffer New(GraphicsDevice device, ReadOnlySpan<byte> data, GraphicsResourceUsage usage = GraphicsResourceUsage.Immutable)
             {
                 return Buffer.New(device, data, elementSize: 0, BufferFlags.VertexBuffer, PixelFormat.None, usage);
             }
 
             /// <summary>
-            /// Creates a new Vertex buffer with <see cref="GraphicsResourceUsage.Immutable"/> uasge by default.
+            ///   Creates a new <strong>Vertex Buffer</strong> with initial data.
             /// </summary>
             /// <param name="device">The <see cref="GraphicsDevice"/>.</param>
-            /// <param name="value">The value to initialize the Vertex buffer.</param>
-            /// <param name="usage">The usage of this resource.</param>
-            /// <returns>A Vertex buffer</returns>
-            [Obsolete("Use span instead")]
+            /// <param name="dataPointer">The data pointer to the data to initialize the Vertex buffer.</param>
+            /// <param name="usage">
+            ///   The usage for the Buffer, which determines who can read/write data. By default, it is <see cref="GraphicsResourceUsage.Immutable"/>.
+            /// </param>
+            /// <returns>A new instance of <see cref="Buffer"/>.</returns>
+            [Obsolete("This method is obsolete. Use the span-based methods instead")]
             public static Buffer New(GraphicsDevice device, DataPointer dataPointer, GraphicsResourceUsage usage = GraphicsResourceUsage.Immutable)
             {
                 return Buffer.New(device, dataPointer, elementSize: 0, BufferFlags.VertexBuffer, usage);
--- sources/engine/Stride.Graphics/BufferDescription.cs
+++ sources/engine/Stride.Graphics/BufferDescription.cs
@@ -6,17 +6,24 @@ using System;
 namespace Stride.Graphics
 {
     /// <summary>
-    /// Describes a buffer.
+    ///   Describes a GPU <see cref="Buffer"/>.
     /// </summary>
     public struct BufferDescription : IEquatable<BufferDescription>
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="BufferDescription"/> struct.
+        ///   Initializes a new instance of <see cref="BufferDescription"/> struct.
         /// </summary>
-        /// <param name="sizeInBytes">Size of the buffer in bytes.</param>
-        /// <param name="bufferFlags">Buffer flags describing the type of buffer.</param>
-        /// <param name="usage">Usage of this buffer.</param>
-        /// <param name="structureByteStride">The size of the structure (in bytes) when it represents a structured/typed buffer. Default = 0.</param>
+        /// <param name="sizeInBytes">Size of the Buffer in bytes.</param>
+        /// <param name="bufferFlags">Buffer flags describing the type of Buffer.</param>
+        /// <param name="usage">The usage for the Buffer, which determines who can read/write data.</param>
+        /// <param name="structureByteStride">
+        ///   <para>
+        ///     If the Buffer is a <strong>Structured Buffer</strong> or a <strong>Typed Buffer</strong>, this parameter indicates
+        ///     the <strong>stride</strong> of each element of the Buffer (the structure). The stride is not only the size of the
+        ///     structure, but also any padding in between two consecutive elements.
+        ///   </para>
+        ///   <para>For any other kind of Buffer, this parameter can be 0.</para>
+        /// </param>
         public BufferDescription(int sizeInBytes, BufferFlags bufferFlags, GraphicsResourceUsage usage, int structureByteStride = 0)
         {
             SizeInBytes = sizeInBytes;
@@ -25,27 +32,30 @@ namespace Stride.Graphics
             StructureByteStride = structureByteStride;
         }
 
+
         /// <summary>
-        /// Size of the buffer in bytes.
+        ///   Size of the <see cref="Buffer"/> in bytes.
         /// </summary>
         public int SizeInBytes;
 
         /// <summary>
-        /// Buffer flags describing the type of buffer.
+        ///   Flags describing the type of <see cref="Buffer"/>.
         /// </summary>
         public BufferFlags BufferFlags;
 
         /// <summary>
-        /// Usage of this buffer.
+        ///   Usage for the <see cref="Buffer"/>, which determines who can read / write data.
         /// </summary>
         public GraphicsResourceUsage Usage;
 
         /// <summary>
-        /// The size of the structure (in bytes) when it represents a structured/typed buffer.
+        ///   The size in bytes of the structure (each element in the <see cref="Buffer"/>) when it represents a
+        ///   <strong>Structured Buffer</strong> or a <strong>Typed Buffer</strong>.
         /// </summary>
         public int StructureByteStride;
 
 
+        /// <inheritdoc/>
         public readonly bool Equals(BufferDescription other)
         {
             return SizeInBytes == other.SizeInBytes
@@ -63,6 +73,7 @@ namespace Stride.Graphics
             return obj is BufferDescription description && Equals(description);
         }
 
+        /// <inheritdoc/>
         public override readonly int GetHashCode()
         {
             return HashCode.Combine(SizeInBytes, BufferFlags, Usage, StructureByteStride);
