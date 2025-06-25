2c0e3caa32a325ee62a75b7bcd571010a6ee25ad
Mario Guerra <mgg7007@gmail.com>
2025-06-25 20:40:59 +0200

    Refactor GraphicsResource to use ComPtrs

--- sources/engine/Stride.Graphics/GraphicsResourceLink.cs
+++ sources/engine/Stride.Graphics/GraphicsResourceLink.cs
@@ -3,49 +3,49 @@
 
 using System;
 
-namespace Stride.Graphics
+namespace Stride.Graphics;
+
+public sealed class GraphicsResourceLink
 {
     /// <summary>
     /// A resource allocated by <see cref="GraphicsResourceAllocator"/> providing allocation informations.
     /// </summary>
-    public sealed class GraphicsResourceLink
+    internal GraphicsResourceLink(GraphicsResourceBase resource)
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="GraphicsResourceLink"/> class.
         /// </summary>
         /// <param name="resource">The graphics resource.</param>
         /// <exception cref="System.ArgumentNullException">resource</exception>
-        internal GraphicsResourceLink(GraphicsResourceBase resource)
-        {
-            Resource = resource ?? throw new ArgumentNullException(nameof(resource));
-        }
-
         /// <summary>
         /// The graphics resource.
         /// </summary>
-        public GraphicsResourceBase Resource { get; }
-
         /// <summary>
         /// Gets the last time this resource was accessed.
         /// </summary>
         /// <value>The last access time.</value>
-        public DateTime LastAccessTime { get; internal set; }
-
         /// <summary>
         /// Gets the total count of access to this resource (include Increment and Decrement)
         /// </summary>
         /// <value>The access total count.</value>
-        public long AccessTotalCount { get; internal set; }
-
         /// <summary>
         /// Gets the access count since last recycle policy was run.
         /// </summary>
         /// <value>The access count since last recycle.</value>
-        public long AccessCountSinceLastRecycle { get; internal set; }
-
         /// <summary>
         /// The number of active reference to this resource.
         /// </summary>
-        public int ReferenceCount { get; internal set; }
+        Resource = resource ?? throw new ArgumentNullException(nameof(resource));
     }
+
+
+    public GraphicsResourceBase Resource { get; }
+
+    public DateTime LastAccessTime { get; internal set; }
+
+    public long AccessTotalCount { get; internal set; }
+
+    public long AccessCountSinceLastRecycle { get; internal set; }
+
+    public int ReferenceCount { get; internal set; }
 }
