952e5afc2a86a7678670985ac8bb0487a3bbb73e
Mario Guerra <mgg7007@gmail.com>
2025-06-25 21:02:00 +0200

    Refactor render state objects to use ComPtrs

--- sources/engine/Stride.Graphics/RasterizerStates.cs
+++ sources/engine/Stride.Graphics/RasterizerStates.cs
@@ -1,42 +1,50 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using Stride.Core;
 
-namespace Stride.Graphics
+namespace Stride.Graphics;
+
+public static class RasterizerStates
 {
     /// <summary>
     /// Known values for <see cref="RasterizerStateDescription"/>.
     /// </summary>
-    public static class RasterizerStates
+    static RasterizerStates()
     {
         /// <summary>
         /// Built-in rasterizer state object with settings for culling primitives with clockwise winding order.
         /// </summary>
-        public static readonly RasterizerStateDescription CullFront = new RasterizerStateDescription(CullMode.Front);
-
         /// <summary>
         /// Built-in rasterizer state object with settings for culling primitives with counter-clockwise winding order.
         /// </summary>
-        public static readonly RasterizerStateDescription CullBack = new RasterizerStateDescription(CullMode.Back);
-
         /// <summary>
         /// Built-in rasterizer state object with settings for not culling any primitives.
         /// </summary>
-        public static readonly RasterizerStateDescription CullNone = new RasterizerStateDescription(CullMode.None);
-
         /// <summary>
         /// Built-in rasterizer state object for wireframe rendering with settings for culling primitives with clockwise winding order.
         /// </summary>
-        public static readonly RasterizerStateDescription WireframeCullFront = new RasterizerStateDescription(CullMode.Front) { FillMode = FillMode.Wireframe };
-
         /// <summary>
         /// Built-in rasterizer state object for wireframe with settings for culling primitives with counter-clockwise winding order.
         /// </summary>
-        public static readonly RasterizerStateDescription WireframeCullBack = new RasterizerStateDescription(CullMode.Back) { FillMode = FillMode.Wireframe };
-
         /// <summary>
         /// Built-in rasterizer state object for wireframe with settings for not culling any primitives.
         /// </summary>
-        public static readonly RasterizerStateDescription Wireframe = new RasterizerStateDescription(CullMode.None) { FillMode = FillMode.Wireframe };
+        var defaultState = new RasterizerStateDescription();
+        defaultState.SetDefaults();
+        Default = defaultState;
     }
+
+
+    public static readonly RasterizerStateDescription Default;
+
+    public static readonly RasterizerStateDescription CullFront = new(CullMode.Front);
+
+    public static readonly RasterizerStateDescription CullBack = new(CullMode.Back);
+
+    public static readonly RasterizerStateDescription CullNone = new(CullMode.None);
+
+    public static readonly RasterizerStateDescription WireframeCullFront = new(CullMode.Front) { FillMode = FillMode.Wireframe };
+
+    public static readonly RasterizerStateDescription WireframeCullBack = new(CullMode.Back) { FillMode = FillMode.Wireframe };
+
+    public static readonly RasterizerStateDescription Wireframe = new(CullMode.None) { FillMode = FillMode.Wireframe };
 }
