f15fd6282042a99f1845be0feb8a2520fd74e36e
Mario Guerra <mgg7007@gmail.com>
2025-07-04 19:58:47 +0200

    Documentation pass for GraphicsResource (D3D12)

--- sources/engine/Stride.Graphics/Direct3D12/GraphicsResource.Direct3D12.cs
+++ sources/engine/Stride.Graphics/Direct3D12/GraphicsResource.Direct3D12.cs
@@ -7,29 +7,53 @@ using Silk.NET.Direct3D12;
 
 namespace Stride.Graphics
 {
-    /// <summary>
-    /// GraphicsResource class
-    /// </summary>
     public abstract partial class GraphicsResource
     {
+        /// <summary>
+        ///   A reference to the parent <see cref="GraphicsResource"/> that owns this resource, if any.
+        /// </summary>
         internal GraphicsResource ParentResource;
 
+        /// <summary>
+        ///   An optional fence value used to track the staging of this Graphics Resource.
+        /// </summary>
         internal ulong? StagingFenceValue;
+        /// <summary>
+        ///   The Command List being used to record commands for staging data into this Graphics Resource.
+        /// </summary>
         internal CommandList StagingBuilder;
 
+        /// <summary>
+        ///   A handle to the CPU-accessible Shader Resource View (SRV) Descriptor.
+        /// </summary>
         internal CpuDescriptorHandle NativeShaderResourceView;
+        /// <summary>
+        ///   A handle to the CPU-accessible Unordered Access View (UAV) Descriptor.
+        /// </summary>
         internal CpuDescriptorHandle NativeUnorderedAccessView;
 
+        /// <summary>
+        ///   The current Direct3D 12 Resource State of the Graphics Resource.
+        /// </summary>
         internal ResourceStates NativeResourceState;
 
+        /// <summary>
+        ///   Gets a value indicating whether the Graphics Resource is in "Debug mode".
+        /// </summary>
+        /// <value>
+        ///   <see langword="true"/> if the Graphics Resource is initialized in "Debug mode"; otherwise, <see langword="false"/>.
+        /// </value>
         protected bool IsDebugMode => GraphicsDevice?.IsDebugMode == true;
 
 
         /// <summary>
-        /// Returns true if resource state transition is needed in order to use resource in given state
+        ///   Determines if the Graphics Resource needs to perform a state transition in order to reach the target state.
         /// </summary>
-        /// <param name="targeState">Destination resource state</param>
-        /// <returns>True if need to perform a transition, otherwsie false</returns>
+        /// <param name="targeState">The destination Graphics Resource state.</param>
+        /// <returns>
+        ///   <see langword="true"/> if a transition is needed to reach the target state;
+        ///   otherwise, <see langword="false"/>.
+        /// </returns>
         internal bool IsTransitionNeeded(ResourceStates targeState)
         {
             // If 'targeState' is a subset of 'before', then there's no need for a transition
