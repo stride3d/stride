8078e65fdc5a635a8aacacec5ecfe91ce99ae417
Nicolas Musset <nicolas@color-rise.xyz>
2025-07-08 10:29:46 +0200

    feat: Toolbar overflow (#2835)

---
+++ sources/presentation/Stride.Core.Presentation.Avalonia/Internal/DoubleUtil.cs
@@ -0,0 +1,40 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+// This source file is adapted from the Windows Presentation Foundation project. 
+// (https://github.com/dotnet/wpf/)
+// Licensed under MIT license, courtesy of The .NET Foundation.
+
+namespace Stride.Core.Presentation.Avalonia.Internal;
+
+// ReSharper disable CompareOfFloatsByEqualityOperator
+
+/// <summary>
+/// This class provides "fuzzy" comparison functionalities for doubles.
+/// </summary>
+internal static class DoubleUtil
+{
+    internal const double Epsilon = 1e-6;
+
+    public static bool AreClose(double a, double b)
+    {
+        if (a == b) return true;
+
+        return (a - b) is < Epsilon and > -Epsilon;
+    }
+
+    public static bool GreaterThan(double value1, double value2)
+    {
+        return (value1 > value2) && !AreClose(value1, value2);
+    }
+
+    public static bool GreaterThanOrClose(double value1, double value2)
+    {
+        return (value1 > value2) || AreClose(value1, value2);
+    }
+
+    public static bool LessThan(double a, double b)
+    {
+        return (a < b) && !AreClose(a, b);
+    }
+}
