c7990175a47d66f47499e9387cb25d8ab6898b01
Mario Guerra <mgg7007@gmail.com>
2025-07-12 19:59:34 +0200

    Refactor ShaderCompiler to use ComPtrs

--- sources/engine/Stride.Shaders.Compiler/ShaderBytecodeResult.cs
+++ sources/engine/Stride.Shaders.Compiler/ShaderBytecodeResult.cs
@@ -1,11 +1,13 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using Stride.Core.Diagnostics;
+
 namespace Stride.Shaders.Compiler;
 
-internal interface IShaderCompiler
+internal class ShaderBytecodeResult : LoggerResult
 {
-    ShaderBytecodeResult Compile(string shaderSource, string entryPoint, ShaderStage stage,
-                                 EffectCompilerParameters effectParameters, EffectReflection reflection,
-                                 string? sourceFilename = null);
+    public ShaderBytecode? Bytecode { get; set; }
+
+    public string? DisassembleText { get; set; }
 }
