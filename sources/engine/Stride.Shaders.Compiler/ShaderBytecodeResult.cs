67271deaf8aebe1e1a73647dd76bb064ef185f7f
Mario Guerra <mgg7007@gmail.com>
2025-07-12 20:00:33 +0200

    Documentation pass for ShaderCompiler

--- sources/engine/Stride.Shaders.Compiler/ShaderBytecodeResult.cs
+++ sources/engine/Stride.Shaders.Compiler/ShaderBytecodeResult.cs
@@ -5,9 +5,22 @@ using Stride.Core.Diagnostics;
 
 namespace Stride.Shaders.Compiler;
 
+/// <summary>
+///   A result class for storing the output of a Shader compilation process, serving also
+///   as a logger for any messages generated during the compilation.
+/// </summary>
 internal class ShaderBytecodeResult : LoggerResult
 {
+    /// <summary>
+    ///   Gets or sets the compiled Shader byte-code if the compilation is succesful.
+    /// </summary>
+    /// <value>The compiled Shader byte-code, or <see langword="null"/> if the compilation failed.</value>
     public ShaderBytecode? Bytecode { get; set; }
 
+    /// <summary>
+    ///   Gets or sets the decompiled Shader HLSL code from the compiled Shader byte-code if the compilation is succesful,
+    ///   useful for debugging or analysis purposes.
+    /// </summary>
+    /// <value>The decompiled Shader HLSL code, or <see langword="null"/> if the compilation failed.</value>
     public string? DisassembleText { get; set; }
 }
