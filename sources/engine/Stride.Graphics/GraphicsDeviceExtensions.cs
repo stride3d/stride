7fe5f95c994256db40d9978bfbc7f9ba57090cd1
Mario Guerra <mgg7007@gmail.com>
2025-07-17 22:54:50 +0200

    Minor refactor of PrimitiveQuad

--- sources/engine/Stride.Graphics/GraphicsDeviceExtensions.cs
+++ sources/engine/Stride.Graphics/GraphicsDeviceExtensions.cs
@@ -6,12 +6,18 @@ using System;
 using Stride.Core.Mathematics;
 using Stride.Rendering;
 
-namespace Stride.Graphics
+namespace Stride.Graphics;
+
+public static class GraphicsDeviceExtensions
 {
+    #region DrawQuad / DrawTexture Helpers
+
+    // TODO: Should this be named DrawFullScreen or DrawFullScreenTriangle instead?
+
     /// <summary>
     /// Extensions for the <see cref="GraphicsDevice"/>
     /// </summary>
-    public static class GraphicsDeviceExtensions
+    public static void DrawQuad(this GraphicsContext graphicsContext, EffectInstance effectInstance)
     {
         /// <summary>
         /// Draws a fullscreen quad with the specified effect and parameters.
@@ -19,32 +25,25 @@ namespace Stride.Graphics
         /// <param name="graphicsContext">The graphics context used for drawing.</param>
         /// <param name="effectInstance">The effect instance to apply when drawing the quad.</param>
         /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="effectInstance"/> is <c>null</c>.</exception>
-        public static void DrawQuad(this GraphicsContext graphicsContext, EffectInstance effectInstance)
-        {
-            if (effectInstance == null) throw new ArgumentNullException("effectInstance");
+        ArgumentNullException.ThrowIfNull(effectInstance);
 
-            // Draw a full screen quad
-            graphicsContext.CommandList.GraphicsDevice.PrimitiveQuad.Draw(graphicsContext, effectInstance);
-        }
+        // Draw a full screen triangle using the provided effect instance.
+        graphicsContext.CommandList.GraphicsDevice.PrimitiveQuad.Draw(graphicsContext, effectInstance);
+    }
 
-        #region DrawQuad/DrawTexture Helpers
         /// <summary>
         /// Draws a full screen quad. An <see cref="Effect"/> must be applied before calling this method.
         /// </summary>
-        public static void DrawQuad(this CommandList commandList)
-        {
-            commandList.GraphicsDevice.PrimitiveQuad.Draw(commandList);
-        }
+    public static void DrawQuad(this CommandList commandList)
+    {
+        commandList.GraphicsDevice.PrimitiveQuad.Draw(commandList);
+    }
 
         /// <summary>
         /// Draws a fullscreen texture using a <see cref="SamplerStateFactory.LinearClamp"/> sampler. See <see cref="Draw+a+texture"/> to learn how to use it.
         /// </summary>
         /// <param name="texture">The texture. Expecting an instance of <see cref="Texture"/>.</param>
         /// <param name="applyEffectStates">The flag to apply effect states.</param>
-        public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, BlendStateDescription? blendState = null)
-        {
-            graphicsContext.DrawTexture(texture, null, Color4.White, blendState);
-        }
 
         /// <summary>
         /// Draws a fullscreen texture using the specified sampler. See <see cref="Draw+a+texture"/> to learn how to use it.
@@ -52,10 +51,6 @@ namespace Stride.Graphics
         /// <param name="texture">The texture. Expecting an instance of <see cref="Texture"/>.</param>
         /// <param name="sampler">The sampler.</param>
         /// <param name="applyEffectStates">The flag to apply effect states.</param>
-        public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, SamplerState sampler, BlendStateDescription? blendState = null)
-        {
-            graphicsContext.DrawTexture(texture, sampler, Color4.White, blendState);
-        }
 
         /// <summary>
         /// Draws a fullscreen texture using a <see cref="SamplerStateFactory.LinearClamp"/> sampler
@@ -64,10 +59,6 @@ namespace Stride.Graphics
         /// <param name="texture">The texture. Expecting an instance of <see cref="Texture"/>.</param>
         /// <param name="color">The color.</param>
         /// <param name="applyEffectStates">The flag to apply effect states.</param>
-        public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, Color4 color, BlendStateDescription? blendState = null)
-        {
-            graphicsContext.DrawTexture(texture, null, color, blendState);
-        }
 
         /// <summary>
         /// Draws a fullscreen texture using the specified sampler
@@ -77,25 +68,43 @@ namespace Stride.Graphics
         /// <param name="sampler">The sampler.</param>
         /// <param name="color">The color.</param>
         /// <param name="applyEffectStates">The flag to apply effect states.</param>
-        public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, SamplerState sampler, Color4 color, BlendStateDescription? blendState = null)
-        {
-            graphicsContext.CommandList.GraphicsDevice.PrimitiveQuad.Draw(graphicsContext, texture, sampler, color, blendState);
-        }
-        #endregion
 
-        public static Texture GetSharedWhiteTexture(this GraphicsDevice device)
-        {
-            return device.GetOrCreateSharedData("WhiteTexture", CreateWhiteTexture);
-        }
+    public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, BlendStateDescription? blendState = null)
+    {
+        graphicsContext.DrawTexture(texture, samplerState: null, color: Color4.White, blendState);
+    }
 
-        private static Texture CreateWhiteTexture(GraphicsDevice device)
+    public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, SamplerState samplerState, BlendStateDescription? blendState = null)
+    {
+        graphicsContext.DrawTexture(texture, samplerState, Color4.White, blendState);
+    }
+
+    public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, Color4 color, BlendStateDescription? blendState = null)
+    {
+        graphicsContext.DrawTexture(texture, samplerState: null, color, blendState);
+    }
+
+    public static void DrawTexture(this GraphicsContext graphicsContext, Texture texture, SamplerState samplerState, Color4 color, BlendStateDescription? blendState = null)
+    {
+        graphicsContext.CommandList.GraphicsDevice.PrimitiveQuad.Draw(graphicsContext, texture, samplerState, color, blendState);
+    }
+
+    #endregion
+
+    public static Texture GetSharedWhiteTexture(this GraphicsDevice device)
+    {
+        return device.GetOrCreateSharedData("WhiteTexture", CreateWhiteTexture);
+
+        static Texture CreateWhiteTexture(GraphicsDevice device)
         {
             const int Size = 2;
-            var whiteData = new Color[Size * Size];
-            for (int i = 0; i < Size * Size; i++)
-                whiteData[i] = Color.White;
 
-            return Texture.New2D(device, Size, Size, PixelFormat.R8G8B8A8_UNorm, whiteData);
+            Span<Color> textureData = stackalloc Color[Size * Size];
+            textureData.Fill(Color.White);
+
+            var texture = Texture.New2D(device, Size, Size, PixelFormat.R8G8B8A8_UNorm, textureData.ToArray());  // TODO: Use the Span<T> overload when available
+            texture.Name = "WhiteTexture";
+            return texture;
         }
     }
 }
