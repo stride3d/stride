20ed6df4796f34f63813074f112ab33d23f496ab
Mario Guerra <mgg7007@gmail.com>
2025-07-17 22:54:50 +0200

    Minor refactor of PrimitiveQuad

--- sources/engine/Stride.Graphics/PrimitiveQuad.cs
+++ sources/engine/Stride.Graphics/PrimitiveQuad.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using System;
 using Stride.Core;
 using Stride.Core.Mathematics;
 using Stride.Rendering;
@@ -13,17 +12,14 @@ namespace Stride.Graphics
     /// </summary>
     public class PrimitiveQuad : ComponentBase
     {
-        /// <summary>
-        /// The pipeline state.
-        /// </summary>
         private readonly MutablePipelineState pipelineState;
 
         private readonly EffectInstance simpleEffect;
         private readonly SharedData sharedData;
-        private const int QuadCount = 3;
 
         public static readonly VertexDeclaration VertexDeclaration = VertexPositionNormalTexture.Layout;
         public static readonly PrimitiveType PrimitiveType = PrimitiveType.TriangleList;
+
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PrimitiveQuad" /> class.
@@ -33,9 +29,10 @@ namespace Stride.Graphics
         public PrimitiveQuad(GraphicsDevice graphicsDevice)
         {
             GraphicsDevice = graphicsDevice;
-            sharedData = GraphicsDevice.GetOrCreateSharedData("PrimitiveQuad::VertexBuffer", d => new SharedData(GraphicsDevice));
 
-            simpleEffect = new EffectInstance(new Effect(GraphicsDevice, SpriteEffect.Bytecode));
+            sharedData = graphicsDevice.GetOrCreateSharedData(SharedData.SharedDataKey, static device => new SharedData(device));
+
+            simpleEffect = new EffectInstance(new Effect(graphicsDevice, SpriteEffect.Bytecode));
             simpleEffect.Parameters.Set(SpriteBaseKeys.MatrixTransform, Matrix.Identity);
             simpleEffect.UpdateEffect(graphicsDevice);
 
@@ -45,11 +42,12 @@ namespace Stride.Graphics
             pipelineState.State.PrimitiveType = PrimitiveType;
         }
 
+
         /// <summary>
         /// Gets the graphics device.
         /// </summary>
         /// <value>The graphics device.</value>
-        public GraphicsDevice GraphicsDevice { get; private set; }
+        public GraphicsDevice GraphicsDevice { get; }
 
         /// <summary>
         /// Gets the parameters used.
@@ -57,14 +55,15 @@ namespace Stride.Graphics
         /// <value>The parameters.</value>
         public ParameterCollection Parameters => simpleEffect.Parameters;
 
+
         /// <summary>
         /// Draws a quad. The effect must have been applied before calling this method with pixel shader having the signature float2:TEXCOORD.
         /// </summary>
         /// <param name="texture"></param>
         public void Draw(CommandList commandList)
         {
-            commandList.SetVertexBuffer(0, sharedData.VertexBuffer.Buffer, sharedData.VertexBuffer.Offset, sharedData.VertexBuffer.Stride);
-            commandList.Draw(QuadCount);
+            commandList.SetVertexBuffer(index: 0, sharedData.VertexBuffer.Buffer, sharedData.VertexBuffer.Offset, sharedData.VertexBuffer.Stride);
+            commandList.Draw(SharedData.VertexCount);
         }
 
         /// <summary>
@@ -83,7 +82,6 @@ namespace Stride.Graphics
 
             graphicsContext.CommandList.SetPipelineState(pipelineState.CurrentState);
 
-            // Apply the effect
             effectInstance.Apply(graphicsContext);
 
             Draw(graphicsContext.CommandList);
@@ -96,7 +94,7 @@ namespace Stride.Graphics
         /// <param name="applyEffectStates">The flag to apply effect states.</param>
         public void Draw(GraphicsContext graphicsContext, Texture texture, BlendStateDescription? blendState = null)
         {
-            Draw(graphicsContext, texture, null, Color.White, blendState);
+            Draw(graphicsContext, texture, samplerState: null, Color.White, blendState);
         }
 
         /// <summary>
@@ -114,9 +112,10 @@ namespace Stride.Graphics
             pipelineState.State.BlendState = blendState ?? BlendStates.Default;
             pipelineState.State.Output.CaptureState(graphicsContext.CommandList);
             pipelineState.Update();
+
             graphicsContext.CommandList.SetPipelineState(pipelineState.CurrentState);
 
-            // Make sure that we are using our vertex shader
+            // Make sure that we are using our Vertex Shader
             simpleEffect.Parameters.Set(SpriteEffectKeys.Color, color);
             simpleEffect.Parameters.Set(TexturingKeys.Texture0, texture);
             simpleEffect.Parameters.Set(TexturingKeys.Sampler, samplerState ?? GraphicsDevice.SamplerStates.LinearClamp);
@@ -124,36 +123,45 @@ namespace Stride.Graphics
 
             Draw(graphicsContext.CommandList);
 
-            // TODO ADD QUICK UNBIND FOR SRV
+            // TODO: ADD QUICK UNBIND FOR SRV
             //GraphicsDevice.Context.PixelShader.SetShaderResource(0, null);
         }
+
 
         /// <summary>
         /// Internal structure used to store VertexBuffer and VertexInputLayout.
         /// </summary>
         private class SharedData : ComponentBase
         {
+            public const string SharedDataKey = "PrimitiveQuad::VertexBuffer";
             /// <summary>
             /// The vertex buffer
             /// </summary>
             public readonly VertexBufferBinding VertexBuffer;
 
+            public const int VertexCount = 3;
+
             // TODO: This is not a quad, but a fullscreen triangle! Maybe this class should be renamed?
-            private static readonly VertexPositionNormalTexture[] QuadsVertices =
-            {
+            private static readonly VertexPositionNormalTexture[] TriangleVertices =
+            [
+                //                              Position                Normal                Texture Coordinates
                 new VertexPositionNormalTexture(new Vector3(-1,  1, 0), new Vector3(0, 0, 1), new Vector2(0, 0)),
                 new VertexPositionNormalTexture(new Vector3(+3,  1, 0), new Vector3(0, 0, 1), new Vector2(2, 0)),
                 new VertexPositionNormalTexture(new Vector3(-1, -3, 0), new Vector3(0, 0, 1), new Vector2(0, 2)),
-            };
+            ];
 
-            public SharedData(GraphicsDevice device)
+
+            public SharedData(GraphicsDevice device) : base(name: SharedDataKey)
             {
-                var vertexBuffer = Buffer.Vertex.New(device, QuadsVertices).DisposeBy(this);
+                var vertexBuffer = Buffer.Vertex.New(device, TriangleVertices).DisposeBy(this);
+                vertexBuffer.Name = device.IsDebugMode
+                    ? $"{SharedDataKey} ({vertexBuffer.Name})"
+                    : SharedDataKey;
 
                 // Register reload
-                vertexBuffer.Reload = (graphicsResource, services) => ((Buffer)graphicsResource).Recreate(QuadsVertices);
+                vertexBuffer.Reload = (graphicsResource, services) => ((Buffer) graphicsResource).Recreate(TriangleVertices);
 
-                VertexBuffer = new VertexBufferBinding(vertexBuffer, VertexDeclaration, QuadsVertices.Length, VertexPositionNormalTexture.Size);
+                VertexBuffer = new VertexBufferBinding(vertexBuffer, VertexDeclaration, TriangleVertices.Length, VertexPositionNormalTexture.Size);
             }
         }
     }
