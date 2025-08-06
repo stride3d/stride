52289f5a1274ea9fe8b0950e5e00fbb77f215274
Eideren <contact@eideren.com>
2025-08-06 22:35:41 +0200

    feat: BufferHelper API

---
+++ sources/engine/Stride.Graphics/MeshExtension.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+#nullable enable
+
+using System;
+using Stride.Core;
+using Stride.Core.IO;
+using Stride.Core.Serialization;
+using Stride.Core.Serialization.Contents;
+using Stride.Graphics.Data;
+
+namespace Stride.Graphics;
+
+public static class MeshExtension
+{
+    /// <summary>
+    /// Fetch this buffer and create a helper to read from it.
+    /// </summary>
+    /// <remarks>
+    /// This operation loads the buffer from disk, or directly from the gpu. It is <b>very slow</b>, avoid calling this too often if at all possible.
+    /// </remarks>
+    /// <param name="binding"> The bindings for this buffer </param>
+    /// <param name="services"> The service used to retrieve the buffer from disk/GPU </param>
+    /// <param name="helper"> The helper class to interact with the loaded buffer </param>
+    /// <param name="count"> The amount of vertices this buffer holds </param>
+    /// <inheritdoc cref="VertexBufferHelper"/>
+    public static void AsReadable(this VertexBufferBinding binding, IServiceRegistry services, out VertexBufferHelper helper, out int count)
+    {
+        helper = new VertexBufferHelper(binding, services, out count);
+    }
+
+    /// <summary>
+    /// Fetch this buffer and create a helper to read from it.
+    /// </summary>
+    /// <remarks>
+    /// This operation loads the buffer from disk, or directly from the gpu. It is <b>very slow</b>, avoid calling this too often if at all possible.
+    /// </remarks>
+    /// <param name="binding"> The bindings for this buffer </param>
+    /// <param name="services"> The service used to retrieve the buffer from disk/GPU </param>
+    /// <param name="helper"> The helper class to interact with the loaded buffer </param>
+    /// <param name="count"> The amount of indices this buffer holds </param>
+    /// <inheritdoc cref="IndexBufferHelper"/>
+    public static void AsReadable(this IndexBufferBinding binding, IServiceRegistry services, out IndexBufferHelper helper, out int count)
+    {
+        helper = new IndexBufferHelper(binding, services, out count);
+    }
+
+    /// <summary>
+    /// Given a semantic and its index, returns its offset and size in the given vertex buffer. Similar to <see cref="VertexDeclaration.EnumerateWithOffsets"/>
+    /// </summary>
+    public static bool TryGetElement(this VertexBufferBinding binding, string vertexElementUsage, int semanticIndex, out VertexElementWithOffset result)
+    {
+        int offset = 0;
+        foreach (var element in binding.Declaration.VertexElements)
+        {
+            // Get new offset (if specified)
+            var currentElementOffset = element.AlignedByteOffset;
+            if (currentElementOffset != VertexElement.AppendAligned)
+                offset = currentElementOffset;
+
+            var elementSize = element.Format.SizeInBytes();
+            if (vertexElementUsage == element.SemanticName && semanticIndex == element.SemanticIndex)
+            {
+                result = new VertexElementWithOffset(element, offset, elementSize);
+                return true;
+            }
+
+            // Compute next offset (if automatic)
+            offset += elementSize;
+        }
+
+        result = default;
+        return false;
+    }
+
+    /// <inheritdoc cref="TryFetchBufferContent"/>
+    /// <remarks> Same as <see cref="TryFetchBufferContent"/> but throws on failure </remarks>
+    internal static byte[] FetchBufferContentOrThrow(Buffer buffer, IServiceRegistry services)
+    {
+        var data = TryFetchBufferContent(buffer, services);
+        if (data is null || data.Length == 0)
+        {
+            throw new InvalidOperationException(
+                $"Failed to find mesh buffers while attempting to {nameof(FetchBufferContentOrThrow)}. " +
+                $"Make sure that the mesh is either an asset on disk, or has its buffer data attached to the buffer through '{nameof(AttachedReference)}'\n");
+        }
+
+        return data;
+    }
+
+    /// <summary> Get buffer content from GPU, shared memory or disk </summary>
+    internal static byte[]? TryFetchBufferContent(Buffer buffer, IServiceRegistry services)
+    {
+        var bufRef = AttachedReferenceManager.GetAttachedReference(buffer);
+        if (bufRef?.Data != null && ((BufferData)bufRef.Data).Content is { } output)
+            return output;
+
+        // Try to load it from disk, a file provider is required, editor does not provide one
+        if (bufRef?.Url != null && services.GetService<IDatabaseFileProviderService>() is {} provider && provider.FileProvider is not null)
+        {
+            // We have to create a new one without providing services to ensure that it dumps the graphics buffer data to the attached reference below
+            var cleanManager = new ContentManager(provider);
+            var bufferCopy = cleanManager.Load<Buffer>(bufRef.Url);
+            try
+            {
+                return bufferCopy.GetSerializationData().Content;
+            }
+            finally
+            {
+                cleanManager.Unload(bufRef.Url);
+            }
+        }
+
+        // When the mesh is created at runtime, or when the file provider is null as can be the case in editor, fetch from GPU
+        // will most likely break on non-dx11 APIs
+        if (services.GetService<GraphicsContext>() is { } context)
+        {
+            output = new byte[buffer.SizeInBytes];
+            if (buffer.Description.Usage == GraphicsResourceUsage.Staging) // Directly if this is a staging resource
+            {
+                buffer.GetData(context.CommandList, buffer, output);
+            }
+            else // inefficient way to use the Copy method using dynamic staging texture
+            {
+                using var throughStaging = buffer.ToStaging();
+                buffer.GetData(context.CommandList, throughStaging, output);
+            }
+
+            return output;
+        }
+
+        return null;
+    }
+}
---
+++ sources/engine/Stride.Graphics/VertexBufferHelper.cs
@@ -0,0 +1,235 @@
+﻿// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+#nullable enable
+using System;
+using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Graphics.Semantic;
+
+namespace Stride.Graphics;
+
+/// <example>
+/// Reading the vertex positions of a mesh:
+/// <code>
+/// <![CDATA[
+/// Model.Meshes[0].Draw.VertexBuffers[0].AsReadable(Services, out VertexBufferHelper helper, out int count);
+/// var vertexPositions = new Vector3[count];
+/// helper.Copy<PositionSemantic, Vector3>(vertexPositions);
+/// ]]>
+/// </code>
+/// </example>
+public class VertexBufferHelper
+{
+    /// <summary>
+    /// Full vertex buffer, does not account for the binding offset or length
+    /// </summary>
+    public readonly byte[] DataOuter;
+    public readonly VertexBufferBinding Binding;
+
+    /// <summary>
+    /// Effective vertex buffer, accounts for the binding offset and length
+    /// </summary>
+    public Span<byte> DataInner => DataOuter.AsSpan(Binding.Offset, Binding.Count * Binding.Stride);
+
+    /// <inheritdoc cref="MeshExtension.AsReadable(VertexBufferBinding, IServiceRegistry, out VertexBufferHelper, out int)"/>
+    public VertexBufferHelper(VertexBufferBinding binding, IServiceRegistry services, out int count)
+    {
+        var data = MeshExtension.FetchBufferContentOrThrow(binding.Buffer, services);
+        DataOuter = data;
+        Binding = binding;
+        count = Binding.Count;
+    }
+
+    /// <summary>
+    /// Extract individual element from each vertex contained in this vertex buffer and copies them into <paramref name="buffer"/>
+    /// </summary>
+    /// <param name="buffer">
+    /// The buffer which will be written to, must have exactly the same amount of items as there are <b>vertices</b> in the buffer
+    /// </param>
+    /// <param name="semanticIndex">
+    /// The semantic to read with that index, starts at zero.<br/>
+    /// For example, to sample the second TextureCoordinate, you would use
+    /// <code>
+    /// <![CDATA[
+    /// helper.Copy<TextureCoordinateSemantic, Vector2>(myUvs, 1);
+    /// ]]>
+    /// </code>
+    /// </param>
+    /// <typeparam name="TSemantic">The semantic to read, for example <see cref="PositionSemantic"/></typeparam>
+    /// <typeparam name="TValue">The value type to read, depends entirely on the <typeparamref name="TSemantic"/> used</typeparam>
+    /// <returns>True when this semantic exists in the vertex buffer, false otherwise</returns>
+    /// <exception cref="NotImplementedException">
+    /// When the data format for this semantic is too arcane - no conversion logic is implemented for that type
+    /// </exception>
+    /// <inheritdoc cref="VertexBufferHelper"/>
+    public bool Copy<TSemantic, TValue>(Span<TValue> buffer, int semanticIndex = 0) where TSemantic : ISemantic<TValue> where TValue : unmanaged
+    {
+        return Read<TSemantic, TValue, CopyToDest<TValue>>(buffer, new CopyToDest<TValue>(), semanticIndex);
+    }
+
+    /// <summary>
+    /// Provides custom access to the vertex buffer while having access to the auto-conversion
+    /// </summary>
+    /// <param name="reader">
+    /// An implementation of <see cref="IReader{TDestValue}"/>, implement this interface to read directly from the vertex buffer
+    /// while making use of the auto-conversion of the <typeparamref name="TSemantic"/> provided <br/>
+    /// Preferably as a struct to ensure it is inlined by the JIT
+    /// </param>
+    /// <param name="semanticIndex">
+    /// The semantic to read with that index, starts at zero.<br/>
+    /// For example, to sample the second TextureCoordinate, you would use
+    /// <code>
+    /// <![CDATA[
+    /// helper.Read<TextureCoordinateSemantic, Vector2>(yourReader, 1);
+    /// ]]>
+    /// </code>
+    /// </param>
+    /// <typeparam name="TSemantic">The semantic to read, <see cref="PositionSemantic"/> for example</typeparam>
+    /// <typeparam name="TValue">The value type to read, depends on the <typeparamref name="TSemantic"/> used, <see cref="Vector3"/> when your <typeparamref name="TSemantic"/> <see cref="PositionSemantic"/> for example</typeparam>
+    /// <typeparam name="TReader">The type of the reader you're providing</typeparam>
+    /// <returns>True when this semantic exists in the vertex buffer, false otherwise</returns>
+    /// <exception cref="NotImplementedException">
+    /// When the data format for this semantic is too arcane - no conversion logic is implemented for that type
+    /// </exception>
+    /// <inheritdoc cref="IReader{TDest}"/>
+    public bool Read<TSemantic, TValue, TReader>(Span<TValue> destination, TReader reader, int semanticIndex = 0)
+        where TSemantic : ISemantic<TValue> where TValue : unmanaged
+        where TReader : IReader<TValue>
+    {
+        if (Binding.TryGetElement(TSemantic.Name, semanticIndex, out var elementData))
+        {
+            switch (elementData.VertexElement.Format)
+            {
+                case PixelFormat.R32G32_Float: Inner<TSemantic, Vector2>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R32G32B32_Float: Inner<TSemantic, Vector3>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R32G32B32A32_Float: Inner<TSemantic, Vector4>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R16G16_Float: Inner<TSemantic, Half2>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R16G16B16A16_Float: Inner<TSemantic, Half4>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R16G16B16A16_UInt: Inner<TSemantic, UShort4>(DataInner, destination, reader, Binding, elementData); break;
+                case PixelFormat.R8G8B8A8_UInt: Inner<TSemantic, Byte4>(DataInner, destination, reader, Binding, elementData); break;
+                default: throw new NotImplementedException($"Unsupported format when converting vertex element ({elementData.VertexElement.Format})");
+            }
+
+            return true;
+        }
+
+        return false;
+
+        static unsafe void Inner<TConversion, TSource>(Span<byte> vertexBuffer, Span<TValue> destination, IReader<TValue> reader, VertexBufferBinding binding, VertexElementWithOffset element) 
+            where TConversion : IConversion<TSource, TValue> where TSource : unmanaged
+        {
+            var stride = binding.Declaration.VertexStride;
+            var offset = element.Offset;
+            var count = vertexBuffer.Length / element.Size;
+
+            if (sizeof(TSource) != element.Size)
+                throw new ArgumentException($"{typeof(TSource)} does not match element size ({sizeof(TSource)} != {element.Size})");
+            
+            fixed (byte* ptrSr = vertexBuffer)
+            {
+                byte* src = ptrSr + offset;
+                reader.Read<TConversion, TSource>(src, count, stride, destination);
+            }
+        }
+    }
+
+    public struct CopyAsTriangleList : IReader<Vector3>
+    {
+        public required IndexBufferHelper IndexBufferHelper;
+        
+        public unsafe void Read<TConversion, TSource>(byte* sourcePointer, int elementCount, int stride, Span<Vector3> destination)
+            where TConversion : IConversion<TSource, Vector3> where TSource : unmanaged
+        {
+            if (destination.Length != IndexBufferHelper.Binding.Count)
+                throw new ArgumentException($"{nameof(destination)} length does not match the amount of indices contained within the index buffer buffer ({destination.Length} / {IndexBufferHelper.Binding.Count})");
+
+            fixed (Vector3* destPtr = destination)
+            {
+                Vector3* dest = destPtr;
+                if (IndexBufferHelper.Is32Bit(out var indices32, out var indices16))
+                {
+                    foreach (var index in indices32)
+                    {
+                        TConversion.Convert(*(TSource*)(sourcePointer + index * stride), out *dest);
+                        dest++;
+                    }
+                }
+                else
+                {
+                    foreach (var index in indices16)
+                    {
+                        TConversion.Convert(*(TSource*)(sourcePointer + index * stride), out *dest);
+                        dest++;
+                    }
+                }
+            }
+        }
+    }
+
+    private struct CopyToDest<T> : IReader<T> where T : unmanaged
+    {
+        public unsafe void Read<TConversion, TSource>(byte* sourcePointer, int elementCount, int stride, Span<T> destination) 
+            where TConversion : IConversion<TSource, T> where TSource : unmanaged
+        {
+            if (destination.Length != elementCount)
+                throw new ArgumentException($"{nameof(destination)} length does not match the amount of vertices contained within this vertex buffer ({destination.Length} / {elementCount})");
+            
+            fixed (T* ptrDest = destination)
+            {
+                byte* end = sourcePointer + elementCount * stride;
+                T* dest = ptrDest;
+                for (; sourcePointer < end; sourcePointer += stride, dest++)
+                    TConversion.Convert(*(TSource*)sourcePointer, out *dest);
+            }
+        }
+    }
+
+    /// <example>
+    /// Implementing <see cref="Copy{TSemantic,TValue}"/> manually:
+    /// <code>
+    /// <![CDATA[
+    /// Model.Meshes[0].Draw.VertexBuffers[0].AsReadable(Services, out VertexBufferHelper helper, out int count);
+    /// var vertexPositions = new Vector3[count];
+    /// var myReader = new CopyTo();
+    /// helper.Read<PositionSemantic, Vector3, CopyTo>(vertexPositions, myReader);
+    /// 
+    /// struct CopyTo : IReader<Vector3>
+    /// {
+    ///    public unsafe void Read<TConversion, TSource>(byte* sourcePointer, int elementCount, int stride, Span<T> destination) 
+    ///     where TConversion : IConversion<TSource, T> where TSource : unmanaged
+    ///    {
+    ///        if (destination.Length != elementCount)
+    ///            throw new ArgumentException($"{nameof(destination)} length does not match the amount of vertices contained within this vertex buffer ({destination.Length} / {elementCount})");
+    ///            
+    ///        fixed (T* ptrDest = destination)
+    ///        {
+    ///            byte* end = sourcePointer + elementCount * stride;
+    ///            T* dest = ptrDest;
+    ///            for (; sourcePointer < end; sourcePointer += stride, dest++)
+    ///                TConversion.Convert(*(TSource*)sourcePointer, out *dest);
+    ///        }
+    ///    }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    public interface IReader<TDest>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sourcePointer">The pointer already offset by the buffer and element offset</param>
+        /// <param name="elementCount">The amount of vertices, this is not the same as the size of the vertex buffer, or the size in bytes taken by individual vertices</param>
+        /// <param name="stride">The size in bytes taken by individual vertices</param>
+        /// <param name="destination">The span passed into the <see cref="Read{TConversion,TSource}"/> method call</param>
+        /// <typeparam name="TConversion">A helper to convert between <typeparamref name="TSource"/> and <typeparamref name="TDest"/> properly</typeparam>
+        /// <typeparam name="TSource">
+        /// The source type this vertex buffer was built with, for example <see cref="Vector2"/> or <see cref="Byte4"/>,
+        /// use <typeparamref name="TConversion"/> to convert it into a <typeparamref name="TDest"/>.
+        /// </typeparam>
+        /// <inheritdoc cref="IReader{TDest}"/>
+        unsafe void Read<TConversion, TSource>(byte* sourcePointer, int elementCount, int stride, Span<TDest> destination)
+            where TConversion : IConversion<TSource, TDest> where TSource : unmanaged;
+    }
+}
