4fdc17aa9ccae2ec11964fef7b7300e95c444528
Doprez <73259914+Doprez@users.noreply.github.com>
2025-08-23 15:07:06 -0600

    removed obsolete

--- sources/core/Stride.Core/Collections/ITrackingCollectionChanged.cs
+++ sources/core/Stride.Core/Collections/ITrackingCollectionChanged.cs
@@ -9,6 +9,5 @@ public interface ITrackingCollectionChanged<TKey, TValue>
     /// Occurs when [collection changed].
     /// </summary>
     /// Called as is when adding an item, and in reverse-order when removing an item.
-    [Obsolete]
     event EventHandler<TrackingCollectionChangedEventArgs<TKey, TValue>> CollectionChanged;
 }
