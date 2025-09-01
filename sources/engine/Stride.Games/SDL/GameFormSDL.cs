8244615d6f8d2174bdc095492fc8ecbebca09fd9
DockFrankenstein <ferafiks@proton.me>
2025-09-01 19:09:59 +0200

    OpenGL: refactored how resize events are handled

--- sources/engine/Stride.Games/SDL/GameFormSDL.cs
+++ sources/engine/Stride.Games/SDL/GameFormSDL.cs
@@ -30,8 +30,7 @@ namespace Stride.Games
         public GameFormSDL(string text) : base(text)
         {
             Size = new Size2(800, 600);
-            ResizeBeginActions += GameForm_ResizeBeginActions;
-            ResizeEndActions += GameForm_ResizeEndActions;
+            SizeChangedActions += GameForm_SizeChangedActions;
             ActivateActions += GameForm_ActivateActions;
             DeActivateActions += GameForm_DeActivateActions;
             previousWindowState = FormWindowState.Normal;
@@ -66,7 +65,7 @@ namespace Stride.Games
         /// <summary>
         /// Occurs when [user resized].
         /// </summary>
-        public event EventHandler<EventArgs> UserResized;
+        public event EventHandler<EventArgs> SizeChanged;
 
         /// <summary>
         /// Occurs when alt-enter key combination has been pressed.
@@ -77,10 +76,7 @@ namespace Stride.Games
 
         #region Implementation
         // TODO: The code below is taken from GameForm.cs of the Windows Desktop implementation. This needs reviewing
-        private Size2 cachedSize;
         private FormWindowState previousWindowState;
-        //private DisplayMonitor monitor;
-        private bool isUserResizing;
 
         private void GameForm_MinimizedActions(WindowEvent e)
         {
@@ -95,9 +91,7 @@ namespace Stride.Games
 
             previousWindowState = FormWindowState.Maximized;
 
-            UserResized?.Invoke(this, EventArgs.Empty);
-            //UpdateScreen();
-            cachedSize = Size;
+            SizeChanged?.Invoke(this, EventArgs.Empty);
         }
 
         private void GameForm_RestoredActions(WindowEvent e)
@@ -106,6 +100,7 @@ namespace Stride.Games
             {
                 ResumeRendering?.Invoke(this, EventArgs.Empty);
             }
+
             previousWindowState = FormWindowState.Normal;
         }
 
@@ -119,37 +114,15 @@ namespace Stride.Games
             AppDeactivated?.Invoke(this, EventArgs.Empty);
         }
 
-        private void GameForm_ResizeBeginActions(WindowEvent e)
+        private void GameForm_SizeChangedActions(WindowEvent e)
         {
-            isUserResizing = true;
-            cachedSize = Size;
-            PauseRendering?.Invoke(this, EventArgs.Empty);
-
-            //This is a temporary workaround
-            //How this should work, is that after resizing, SDL invokes
-            //ResizeBeginActions and ResizeEndActions. Currently on linux
-            //these methods are called in the opposite order every frame,
-            //so we just call the second method here to fix that
-            if (OperatingSystem.IsLinux())
-                GameForm_ResizeEndActions(new WindowEvent());
-        }
-
-        private void GameForm_ResizeEndActions(WindowEvent e)
-        {
-            if (isUserResizing && cachedSize.Equals(Size))
-            {
-                UserResized?.Invoke(this, EventArgs.Empty);
-                // UpdateScreen();
-            }
-
-            isUserResizing = false;
-            ResumeRendering?.Invoke(this, EventArgs.Empty);
+            SizeChanged?.Invoke(this, EventArgs.Empty);
         }
 
         private void GameFormSDL_KeyDownActions(KeyboardEvent e)
         {
-            var altReturn = ((KeyCode)e.Keysym.Sym == KeyCode.KReturn) && (((Keymod)e.Keysym.Mod & Keymod.KmodAlt) != 0);
-            var altEnter = ((KeyCode)e.Keysym.Sym == KeyCode.KKPEnter) && (((Keymod)e.Keysym.Mod & Keymod.KmodAlt) != 0);
+            var altReturn = ((KeyCode)e.Keysym.Sym == KeyCode.KReturn) && (((Keymod)e.Keysym.Mod & Keymod.Alt) != 0);
+            var altEnter = ((KeyCode)e.Keysym.Sym == KeyCode.KKPEnter) && (((Keymod)e.Keysym.Mod & Keymod.Alt) != 0);
             if (altReturn || altEnter)
             {
                 FullscreenToggle?.Invoke(this, EventArgs.Empty);
