cfc48928f555c7664ed88b0965302f30bf9b68be
Doprez <73259914+Doprez@users.noreply.github.com>
2025-09-05 10:28:01 -0600

    Forgot how C# works

--- sources/tools/Stride.Importer.3D/MeshConverter.cs
+++ sources/tools/Stride.Importer.3D/MeshConverter.cs
@@ -1170,10 +1170,8 @@ namespace Stride.Importer.ThreeD
             for (uint i = 0; i < scene->MNumMaterials; i++)
             {
                 var lMaterial = scene->MMaterials[i];
-                var materialName = materialNames[(IntPtr)lMaterial];
-
                 // Replace slashes with an underscore to indicate it is not absolute to Strides asset system.
-                materialName.Replace('/', '_');
+                var materialName = materialNames[(IntPtr)lMaterial].Replace('/', '_'); ;
 
                 materials.Add(materialName, ProcessMeshMaterial(scene, lMaterial));
             }
