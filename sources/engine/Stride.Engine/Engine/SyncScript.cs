158f2002962edd691c2ca2517432ba201251e671
Eideren <contact@eideren.com>
2025-12-07 11:52:47 +0100

    fix: ScriptSystem performance (#2986)

--- sources/engine/Stride.Engine/Engine/SyncScript.cs
+++ sources/engine/Stride.Engine/Engine/SyncScript.cs
@@ -1,8 +1,8 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using Stride.Core.Collections;
-using Stride.Core.MicroThreading;
+using Stride.Core.Annotations;
+using Stride.Engine.Processors;
 
 namespace Stride.Engine
 {
@@ -11,11 +11,18 @@ namespace Stride.Engine
     /// </summary>
     public abstract class SyncScript : StartupScript
     {
-        internal PriorityQueueNode<SchedulerEntry> UpdateSchedulerNode;
+        [CanBeNull] internal ScriptSystem ScriptSystem;
+        internal long ScheduledPriorityForUpdate;
 
         /// <summary>
         /// Called every frame.
         /// </summary>
         public abstract void Update();
+
+        protected internal override void PriorityUpdated()
+        {
+            base.PriorityUpdated();
+            ScriptSystem?.MarkAsPriorityChanged(this);
+        }
     }
 }
