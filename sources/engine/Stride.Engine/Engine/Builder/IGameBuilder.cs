36a6c2ac310046f1374098aa727c4a0df1a3bda0
Doprez <73259914+Doprez@users.noreply.github.com>
2025-12-26 06:51:14 -0700

    clean up

--- sources/engine/Stride.Engine/Engine/Builder/IGameBuilder.cs
+++ sources/engine/Stride.Engine/Engine/Builder/IGameBuilder.cs
@@ -1,10 +1,12 @@
+// Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+
 using System.Collections.Generic;
 using Stride.Core.Diagnostics;
 using Stride.Games;
 using System;
 using Microsoft.Extensions.DependencyInjection;
 using Stride.Input;
-using Stride.Core.IO;
 
 namespace Stride.Engine.Builder;
 public interface IGameBuilder
