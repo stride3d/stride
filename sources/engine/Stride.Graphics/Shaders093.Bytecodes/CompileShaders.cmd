7930259558edfc164749201bef6a102bcbcc3ff4
Virgile Bello <virgile@stride3d.net>
2026-01-23 15:16:39 +0900

    Graphics: fix (again) prebuilt shader batch file

--- sources/engine/Stride.Graphics/Shaders093.Bytecodes/CompileShaders.cmd
+++ sources/engine/Stride.Graphics/Shaders093.Bytecodes/CompileShaders.cmd
@@ -2,7 +2,8 @@
 setlocal
 set StrideSdkDir=%~dp0..\..\..\..\
 set StrideAssetCompiler=%StrideSdkDir%sources\assets\Stride.Core.Assets.CompilerApp\bin\Debug\net10.0\Stride.Core.Assets.CompilerApp.exe
-%StrideAssetCompiler% --platform=Windows --property:RuntimeIdentifier=win --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
-%StrideAssetCompiler% --platform=Windows --property:RuntimeIdentifier=win-opengl --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
-%StrideAssetCompiler% --platform=Windows --property:RuntimeIdentifier=win-opengles --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
-%StrideAssetCompiler% --platform=Windows --property:RuntimeIdentifier=win-vulkan --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
+rmdir /s %~dp0obj\
+%StrideAssetCompiler% --platform=Windows --property:StrideGraphicsApi=Direct3D11 --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
+%StrideAssetCompiler% --platform=Windows --property:StrideGraphicsApi=OpenGL --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
+%StrideAssetCompiler% --platform=Windows --property:StrideGraphicsApi=OpenGLES --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
+%StrideAssetCompiler% --platform=Windows --property:StrideGraphicsApi=Vulkan --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.sdpkg
