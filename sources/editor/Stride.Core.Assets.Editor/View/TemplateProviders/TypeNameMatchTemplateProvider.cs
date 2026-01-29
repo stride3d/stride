3acfd994ad0ee9200c5db644c082ff40921181ce
Peter Laske <37439758+laske185@users.noreply.github.com>
2026-01-29 19:48:57 +0100

    refactor: Replace specialized rotation property template provider by general template provider
    Replaced the custom RotationFloatPropertyTemplateProvider with a new, reusable TypeNameMatchTemplateProvider that matches both type and property name. Updated XAML to use this provider for "Rotation" float properties, simplifying and generalizing the approach for property-specific editors. This makes it easier to add similar editors in the future without extra provider classes.

--- sources/editor/Stride.Core.Assets.Editor/View/TemplateProviders/TypeNameMatchTemplateProvider.cs
+++ sources/editor/Stride.Core.Assets.Editor/View/TemplateProviders/TypeNameMatchTemplateProvider.cs
@@ -1,23 +1,25 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
-using Stride.Core.Assets.Editor.View;
 using Stride.Core.Presentation.Quantum.ViewModels;
 
 namespace Stride.Core.Assets.Editor.View.TemplateProviders
 {
     /// <summary>
-    /// Template provider that matches float properties named "Rotation" to display them in degrees using AngleEditor.
+    /// A template provider that matches nodes based on both their type and name.
     /// </summary>
-    public class RotationFloatPropertyTemplateProvider : NodeViewModelTemplateProvider
+    public class TypeNameMatchTemplateProvider : TypeMatchTemplateProvider
     {
         /// <inheritdoc/>
-        public override string Name => "RotationFloatProperty";
+        public override string Name => $"{base.Name}_{PropertyName}";
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets or sets the name of the property to match.
+        /// </summary>
+        public string PropertyName { get; set; }
+
         public override bool MatchNode(NodeViewModel node)
         {
-            // Match float properties named "Rotation"
-            return node.Type == typeof(float) && node.Name == "Rotation";
+            return base.MatchNode(node) && node.Name == PropertyName;
         }
     }
 }
