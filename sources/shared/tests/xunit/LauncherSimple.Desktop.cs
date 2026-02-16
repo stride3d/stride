172043382ae536bdc04ed0b56a8fbd33f438faf9
Virgile Bello <virgile@stride3d.net>
2026-02-16 17:50:12 +0900

    Shader tests: fix project so that they can run

--- sources/shared/tests/xunit/LauncherSimple.Desktop.cs
+++ sources/shared/tests/xunit/LauncherSimple.Desktop.cs
@@ -1,9 +1,7 @@
-using Stride.Graphics.Tests;
-
 namespace xunit.runner.stride
 {
     class Program
     {
-        public static void Main(string[] args) => new TestImageEffect().RunImageEffect();
+        public static void Main(string[] args) => StrideXunitRunner.Main(args);
     }
 }
