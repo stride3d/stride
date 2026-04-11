080248603f3500d0fb3380599f970931db095125
Virgile Bello <virgile@stride3d.net>
2026-04-11 11:35:50 +0900

    refactor: remove SharpDX.DirectWrite dependency from font providers and SDF importer

--- sources/engine/Stride.Assets/SpriteFont/FontProviderBase.cs
+++ sources/engine/Stride.Assets/SpriteFont/FontProviderBase.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation and Contributors (https://dotnetfoundation.org/ & https://stride3d.net) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
-using SharpDX.DirectWrite;
 using Stride.Core.Assets.Compiler;
 using Stride.Core;
 
@@ -12,12 +11,6 @@ namespace Stride.Assets.SpriteFont
     {
         [DataMemberIgnore]
         public virtual Graphics.Font.FontStyle Style { get; set; } = Graphics.Font.FontStyle.Regular;
-
-        /// <summary>
-        /// Gets the associated <see cref="FontFace"/>
-        /// </summary>
-        /// <returns><see cref="FontFace"/> from the specified source or <c>null</c> if not found</returns>
-        public abstract FontFace GetFontFace();
 
         /// <summary>
         /// Gets the actual file path to the font file
