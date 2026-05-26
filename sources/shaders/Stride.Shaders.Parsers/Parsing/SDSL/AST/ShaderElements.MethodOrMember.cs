7e30d6b1b634b28ea322241858642b5b89deacfd
Virgile Bello <virgile@stride3d.net>
2026-05-26 18:52:45 +0900

    Stride.Shaders.Parsers: drop unused System.Runtime.InteropServices.JavaScript using

--- sources/shaders/Stride.Shaders.Parsers/Parsing/SDSL/AST/ShaderElements.MethodOrMember.cs
+++ sources/shaders/Stride.Shaders.Parsers/Parsing/SDSL/AST/ShaderElements.MethodOrMember.cs
@@ -9,7 +9,6 @@ using Stride.Shaders.Spirv.Tools;
 using System.Collections.Immutable;
 using System.Diagnostics.Metrics;
 using CommunityToolkit.HighPerformance;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Stride.Shaders.Parsing.SDSL.AST;
 
