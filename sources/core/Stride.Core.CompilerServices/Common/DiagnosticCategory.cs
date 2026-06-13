4d66ed5c60a63d866846e6f1e92c4af72911f417
Virgile Bello <virgile@stride3d.net>
2026-06-14 01:40:38 +0900

    assets: STRDIAG011 warns on undeclared IProjectAsset extensions
    An IProjectAsset type whose [AssetDescription] extension is missing from StrideProjectAssetExtensions would silently drop its assets from the build; warn at compile time. Skips .cs and generator assets.

--- sources/core/Stride.Core.CompilerServices/Common/DiagnosticCategory.cs
+++ sources/core/Stride.Core.CompilerServices/Common/DiagnosticCategory.cs
@@ -2,5 +2,6 @@ namespace Stride.Core.CompilerServices.Common;
 public static class DiagnosticCategory
 {
     public const string Serialization = "Serialization";
+    public const string Build = "Build";
     public const string LinkFormat = "https://doc.stride3d.net/latest/en/diagnostics/{0}.html";
 }
