1b63c54e35f73b1e91451035359ab72a0d1bb380
Virgile Bello <virgile@stride3d.net>
2026-06-25 15:58:54 +0900

    engine: rename Stride foundation assembly to Stride.Foundation
    The bare Stride assembly held the API-agnostic graphics/rendering
    foundation (Image, PixelFormat, ParameterCollection, media interfaces),
    not the engine, so the name was misleading. Renaming it frees the
    Stride id and clarifies the layering. Bumps the binary serialization
    format version since the assembly identity changed.

--- sources/core/Stride.Core/Serialization/DataSerializer.BinaryFormatVersion.cs
+++ sources/core/Stride.Core/Serialization/DataSerializer.BinaryFormatVersion.cs
@@ -13,7 +13,7 @@ partial class DataSerializer
 {
     // Binary format version, needs to be bumped in case of big changes in serialization formats (i.e. primitive types).
     public const int BinaryFormatVersion = 4 * 1000000 // Major version: any number is ok
-                                         + 0 * 10000   // Minor version: supported range: 0-99
+                                         + 4 * 10000   // Minor version: supported range: 0-99
                                          + 0 * 100     // Patch version: supported range: 0-99
                                          + 1;          // Bump ID: supported range: 0-99
 }
